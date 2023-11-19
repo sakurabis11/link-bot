8c7321442b4bca0418f50de60d1591a4cf54a12e
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-19 21:42:02 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,85 +1,50 @@
 import os, asyncio
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery, ReplyKeyboardMarkup
 from telegraph import upload_file
+from pyrogram.errors import TelegramError
 from utils import get_file_id
 from info import API_ID, API_HASH, BOT_TOKEN, PORT
+import telegraph
 
-@Client.on_message(filters.command("telegraph") & filters.private & filters.group)
-async def telegraph_upload(client, update):
-    replied = update.reply_to_message
+telegraph = telegraph.api.Telegraph()
 
-    if not replied:
-        return await update.reply_text("Please reply to a photo or video under 20MB")
+@Client.on_message(pyrogram.filters.command('telegraph'))
+def handle_upload_command(message):
+    if message.reply_to_message:
+        reply_message = message.reply_to_message
 
-    file_info = get_file_id(replied)
-
-    if not file_info:
-        return await update.reply_text("Unsupported media type")
-
-    file_size = file_info.file_size
-    file_name = file_info.file_name
-
-    if file_size > 20971520:  # 20 MB in bytes
-        await update.reply_text("File size exceeds 20 MB. Uploading in chunks...")
-
-        # Initiate chunking and uploading
-        chunk_size = 5242880  # 5 MB in bytes
-        chunks = []
-
-        with open(file_name, "rb") as f:
-            while True:
-                chunk = f.read(chunk_size)
-                if not chunk:
-                    break
-
-                chunks.append(chunk)
-
-        # Upload each chunk to Telegraph
-        telegraph_links = []
-
-        for chunk in chunks:
-            try:
-                response = upload_file(chunk)
-            except Exception as error:
-                await update.reply_text(f"Error: {error}")
+        if reply_message.media:
+            if reply_message.media.photo:
+                photo_link = upload_photo(reply_message.media.photo)
+            elif reply_message.media.video:
+                video_link = upload_video(reply_message.media.video)
+            elif reply_message.media.document:
+                document_link = upload_document(reply_message.media.document)
+            else:
+                message.reply_text('Unsupported media type')
                 return
 
-            telegraph_links.append(f"https://graph.org{response[0]}")
+            keyboard = InlineKeyboardMarkup([
+                InlineKeyboardButton('Open in Telegraph', url=photo_link or video_link or document_link)
+            ])
 
-        # Combine Telegraph links into a single link
-        telegraph_link = "\n".join(telegraph_links)
+            try:
+                bot.send_message(message.chat.id, 'Uploaded to Telegraph:', reply_markup=keyboard)
+            except TelegramError:
+                message.reply_text('Failed to send message')
+    else:
+        message.reply_text('Please reply to a media message with /telegraph')
 
-    else:  # File size is under 20 MB
-        # Upload the entire file to Telegraph
-        download_message = await update.reply_text("Downloading to my server...")
-        media = await replied.download()
+def upload_photo(photo):
+    result = telegraph.upload_photo(photo)
+    return result['url']
 
-        await download_message.edit_text("Download completed. Uploading to telegra.ph...")
+def upload_video(video):
+    result = telegraph.upload_file(video.file_id)
+    return result['url']
 
-        try:
-            response = upload_file(media)
-        except Exception as error:
-            await download_message.edit_text(f"Error: {error}")
-            return
+def upload_document(document):
+    result = telegraph.upload_file(document.file_id)
+    return result['url']
 
-        try:
-            os.remove(media)
-        except Exception:
-            pass
-
-        telegraph_link = f"https://graph.org{response[0]}"
-
-    # Generate and send response message
-    markup = InlineKeyboardMarkup([[
-        InlineKeyboardButton("Open Link", url=telegraph_link),
-        InlineKeyboardButton("Share Link", url=f"https://telegram.me/share/url?url={telegraph_link}")
-    ], [
-        InlineKeyboardButton("Close", callback_data="close")
-    ]])
-
-    await download_message.edit_text(
-        f"<b>Link:</b>\n\n`{telegraph_link}`",
-        disable_web_page_preview=True,
-        reply_markup=markup
-    )
