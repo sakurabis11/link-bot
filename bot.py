2343f2d1e6d487773a05cdfe62aa4013616fb326
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-20 19:44:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,9 +21,9 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=200,
+            workers=50,
             plugins={"root": "plugins"},
-            sleep_threshold=10,
+            sleep_threshold=5,
         )
     async def start(self):
         await super().start()
