6438951d99174f6eddb66284fff34fc251eb36a0
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-20 19:35:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,6 @@ import re
 import os
 from os import environ
 from pyrogram import enums
-from Script import script
 
 import asyncio
 import json
@@ -10,5 +9,5 @@ from pyrogram import Client
 
 API_ID = int(os.environ.get('API_ID', '8914119'))
 API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '6866077251:AAGu3Cegq2h1tar3pNCCQFceh5QsstvdVrY')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '6866077251:AAGMOU-l84Qe_PIZFQ_pH5M7BALyhqbqK_E')
 PORT = os.environ.get("PORT", "8080")
