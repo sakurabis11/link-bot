3e983045c74e2c5fded968a0dfc7690b2a05ebbd
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-20 19:57:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
-class app(Client):
+class Bot(Client):
 
     def __init__(self):
         super().__init__(
@@ -54,4 +54,5 @@ async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int 
 
 
 if __name__ == "__main__":
+   app = Bot()
    app.run()
