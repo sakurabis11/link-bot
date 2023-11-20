4723615ccd4268214088b207352096d114945afd
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-20 21:12:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,3 +11,10 @@ API_ID = int(os.environ.get('API_ID', '8914119'))
 API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '6866077251:AAGMOU-l84Qe_PIZFQ_pH5M7BALyhqbqK_E')
 PORT = os.environ.get("PORT", "8080")
+
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1342641151').split()]
+
+# MongoDB information
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://Gojo:gojo@cluster0.cn6dtat.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
+DATABASE_NAME = environ.get('DATABASE_NAME', "gojo")
