9d6f2baef4d740764fea474ccab273d8a91e2f42
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-20 08:47:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,24 +7,6 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait
 from info import API_ID, API_HASH, BOT_TOKEN, PORT
 
-@Client.on_message(filters.command("start"))
-aysnc def start_command(client, message):
-    # Welcome message
-    welcome_message = "Welcome to my Telegram bot! 🎉"
-
-    # Inline keyboard buttons
-    keyboard = pyrogram.types.InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Support Group", url="https://t.me/amal_nath_05"),
-                InlineKeyboardButton("Dᴇᴠᴇʟᴏᴩᴇʀ", url="https://t.me/MrTG_Coder"),
-            ],
-        ]
-    )
-
-    # Send the welcome message with the inline keyboard
-    client.send_message(
-        message.chat_id,
-        text=welcome_message,
-        reply_markup=keyboard,
-    )
+@Client.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+    await message.reply_text(START_TXT)
