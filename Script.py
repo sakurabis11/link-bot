59c2a55b9b50f96a4623fc55daada7afec0da572
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-20 21:17:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,5 +2,14 @@ class script(object):
     START_TXT = """<b>✨ Hᴇʟʟᴏ {user}.
 
 Mʏ Nᴀᴍᴇ Is {bot}."""
+    LOG_TEXT_G = """<b>#ɴᴇᴡ_ɢʀᴏᴜᴩ
+
+◉ ɢʀᴏᴜᴩ: {}(<code>{}</code>)
+◉ ᴍᴇᴍʙᴇʀꜱ: <code>{d}</code>
+◉ ᴀᴅᴅᴇᴅ ʙʏ: {}"""
     
+    LOG_TEXT_P = """#ɴᴇᴡ_ᴜꜱᴇʀ
+    
+◉ ᴜꜱᴇʀ-ɪᴅ: <code>{}</code>
+◉ ᴀᴄᴄ-ɴᴀᴍᴇ: {}"""
     
