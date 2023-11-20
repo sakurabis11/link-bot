3b48f399b6985b3b29fb97721f6317d262ac31de
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-20 19:18:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,6 +7,8 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait
 from info import API_ID, API_HASH, BOT_TOKEN, PORT
 
-@Client.on_message(filters.command("start") & filters.private)
-async def start(client, message):
-    await message.reply_text(START_TXT)
+# Define a function to handle the /start command
+@Client.on_message(filters.command("start"))
+def start_message(client, message):
+    client.send_message("Hello! Welcome to my Telegram bot.")
+
