39fb3145ac4c53e70df7f13efe760a1de0a7b858
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-21 19:28:33 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 import asyncio
 from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY, PORT
 
-
+# Define GPT-3 command handler
 @Client.on_message(filters.command("gpt3"))
 async def gpt3_handler(client, message):
     try:
@@ -13,9 +13,9 @@ async def gpt3_handler(client, message):
         # Send prompt to OpenAI API and get response
         response = openai.Completion.create(
             prompt=prompt,
-            model="text-davinci-003", 
+            model="text-davinci-003",
             temperature=0.7,
-            max_tokens=1024,
+            max_tokens=1024
         )
 
         # Truncate response to a reasonable length
@@ -26,6 +26,13 @@ async def gpt3_handler(client, message):
 
     except IndexError:
         await message.reply("Please provide a prompt after the command, e.g., /gpt3 Tell me a joke.")
+
     except Exception as e:
-        print(f"Error occurred: {e}")
-        await message.reply("An error occurred while processing your request. Please try again later.")
+        if isinstance(e, openai.errors.OpenAIError) and e.status_code == 400:
+            await message.reply("Invalid prompt or request format. Please try again.")
+        elif isinstance(e, openai.errors.OpenAIError):
+            await message.reply(f"OpenAI API error: {e.status_code} - {e.reason}. Please try again later.")
+        else:
+            print(f"Unexpected error occurred: {e}")
+            await message.reply("An unexpected error occurred while processing your request. Please try again later.")
+
