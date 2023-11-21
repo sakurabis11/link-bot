04f1b72af9adaebc57ce2d118ed5939657f09d32
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-21 21:33:59 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -1,41 +1,19 @@
-import openai
 from pyrogram import Client, filters
-import asyncio
-from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY, PORT
+from pyrogram.types import Message
+import openai
+from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY
 
-# Define GPT-3 command handler
-@Client.on_message(filters.command("openai"))
-async def openai_handler(client, message):
-    try:
-        # Get the prompt from the message
-        prompt = message.text.split(" ")[1]
+@Client.on_message(filters.text)
+async def generate_response(_, message: Message):
+    # Get the user's message
+    user_input = message.text
 
-        # Create a completion request
-        completion_request = {
-            "prompt": prompt,
-            "model": "text-davinci-003",
-            "temperature": 0.5,
-            "max_tokens": 1024,
-            "top_p": 1,
-            "frequency_penalty": 0.0,
-            "presence_penalty": 0.0
-        }
+    # Use OpenAI to generate a response
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=user_input,
+        max_tokens=100
+    )
 
-        # Send the completion request to OpenAI
-        response = await openai.Completion.create(**completion_request)
-
-        # Extract the response text
-        response_text = response["choices"][0]["text"][:2048]
-
-        # Send the OpenAI response to the Telegram chat
-        await message.reply(response_text)
-    except IndexError:
-        await message.reply("Please provide a prompt after the command, e.g., /openai Tell me a joke.")
-    except openai.errors.OpenAIError as e:
-        if e.status_code == 400:
-            await message.reply("Invalid prompt or request format. Please try again.")
-        else:
-            await message.reply(f"OpenAI API error: {e.status_code} - {e.reason}. Please try again later.")
-    except Exception as e:
-        print(f"Unexpected error occurred: {e}")
-        await message.reply("An unexpected error occurred while processing your request. Please try again later.")
+    # Send the generated response to the user
+    message.reply_text(response['choices'][0]['text'])
