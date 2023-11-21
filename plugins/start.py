2ab60d7991d27141dca6d8645f7e4016e9a41f8b
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-21 22:19:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -13,8 +13,13 @@ from utils import get_size, temp, extract_user, get_file_id, humanbytes
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-@Client.on_message(filters.command(["start"]))
-async def home(client, message):
+@Client.on_message(filters.command(['start']))
+
+async
+ 
+def
+ 
+home(client, message):
     buttons = [[
         InlineKeyboardButton('Help', callback_data='help'),
         InlineKeyboardButton('Close', callback_data='close')
@@ -23,7 +28,7 @@ async def home(client, message):
         InlineKeyboardButton('Our Channel', url='https://t.me/amal_nath_05')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
-    await Tgraph.send_message(
+    await client.send_message(
         chat_id=message.chat.id,
         text="""hello""",
         reply_markup=reply_markup,
@@ -31,8 +36,13 @@ async def home(client, message):
         reply_to_message_id=message.message_id
     )
 
-@Client.on_message(filters.command(["help"]))
-async def help(client, message):
+@Client.on_message(filters.command(['help']))
+
+async
+ 
+def
+ 
+help(client, message):
     buttons = [[
         InlineKeyboardButton('Home', callback_data='home'),
         InlineKeyboardButton('Close', callback_data='close')
@@ -41,7 +51,7 @@ async def help(client, message):
         InlineKeyboardButton('Our Channel', url='https://t.me/amal_nath_05')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
-    await Tgraph.send_message(
+    await client.send_message(
         chat_id=message.chat.id,
         text="""wait""",
         reply_markup=reply_markup,
@@ -55,15 +65,14 @@ async def button(client, update):
         return
 
     cb_data = update.data
-    if "help" in cb_data:
+    if cb_data == 'help':
         await update.message.delete()
         await help(client, update.message)
-    elif "close" in cb_data:
+    elif cb_data == 'close':
         await update.message.delete()
-    elif "home" in cb_data:
+    elif cb_data == 'home':
         await update.message.delete()
         await home(client, update.message)
-
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
 async def restart_bot(client, msg):
     await msg.reply("Rᴇꜱᴛᴀᴛɪɴɢ........")
