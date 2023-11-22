d2e777cb8c2c07e6536b89d1b319d21334ef6886
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-22 20:47:19 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -3,16 +3,19 @@ from pyrogram.types import Message
 import openai
 from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY
 
+openai.api_key = OPENAI_API_KEY 
+
 @Client.on_message(filters.command("gpt"))
 async def gpt(client, message):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        temperature=0.5,
-        max_tokens=1024,
-        top_p=1,
-        frequency_penalty=0.0,
-        persence_penalty=0.0
-    )
-    await message.reply_text(response.choices[0].text)
-
-
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            temperature=0.5,
+            max_tokens=1024,
+            top_p=1,
+            frequency_penalty=0.0,
+            persence_penalty=0.0
+        )
+        await message.reply_text(response.choices[0].text)
+    except Exception as e:
+        await message.reply_text(f"Error: {e}")
