ed54c45314f6381ba80719f7ebfe9ad42a6141e1
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-22 08:10:29 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -1,6 +1,6 @@
 import pyrogram
 from pyrogram import types, Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.utils import get_file_id, download_media
 from info import API_ID, API_HASH, BOT_TOKEN
 from yt_dlp import YoutubeDL
