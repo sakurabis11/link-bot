1d77820d2313b6038327d58976b8ac5798bfcb07
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-22 21:01:54 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 import openai
 from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY
 
-openai.api_key = OPENAI_API_KEY 
+openai.api_key = OPENAI_API_KEY  # Set the OpenAI API key
 
 @Client.on_message(filters.command("gpt"))
 async def gpt(client, message):
@@ -14,8 +14,8 @@ async def gpt(client, message):
             max_tokens=1024,
             top_p=1,
             frequency_penalty=0.0,
-            persence_penalty=0.0
         )
         await message.reply_text(response.choices[0].text)
     except Exception as e:
         await message.reply_text(f"Error: {e}")
+
