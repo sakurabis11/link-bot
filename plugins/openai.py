3ac091d85d5f8351c74a3d1aa730e5c260b12ffd
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 17:14:51 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -7,26 +7,27 @@ openai.api_key = OPENAI_API_KEY
 
 @Client.on_message(filters.command("openai"))
 async def openai_command(client, message):
-    user_input = message.text
+    try:
+        user_input = message.text
 
-    if "when user" in user_input:
-      await message.reply_text("I am ready to assist. Please enter a prompt or question.eg:- /openai {ur_question}")
-    else:
-      user_message = {
-        "role": "user",
-        "content": user_input
-      }
+        if "when user" in user_input:
+            await message.reply_text("I am ready to assist. Please enter a prompt or question.eg:- /openai {ur_question}")
+        else:
+            user_message = {
+                "role": "user",
+                "content": user_input
+            }
 
-      response = openai.Completion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "I am ready to assist."},
-            user_message
-          ]
-      )
+            response = openai.Completion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "I am ready to assist."},
+                    user_message
+                ]
+            )
 
-      await message.reply_text(response.choices[0].text)
+            await message.reply_text(response.choices[0].text)
 
-  except Exception as e:
-    error_message = f"Sorry, an error occurred: {str(e)}"
-    await message.reply_text(error_message)
+    except Exception as e:
+        error_message = f"Sorry, an error occurred: {str(e)}"
+        await message.reply_text(error_message)
