90f9a3258ad49efc2b92179665d253e6310749c5
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 17:20:53 +0530

    Add files via upload

---
+++ plugins/openai (1).py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import openai
+from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY, PORT
+
+openai.api_key = OPENAI_API_KEY
+
+@Client.on_message(filters.command("openai"))
+async def openai_command(client, message):
+    try:
+        user_input = message.text
+
+        response = openai.Completion.create(
+            engine="gpt-3.5-turbo",
+            prompt=user_input,
+            max_tokens=1024
+        )
+
+        await message.reply_text(`response.choices[0].text`)
+
+    except Exception as e:
+        error_message = f"Sorry, an error occurred: {str(e)}"
+        await message.reply_text(error_message)
