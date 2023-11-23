5466d19d4647418ce2276891587004be82e2c3f2
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 21:14:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,24 +7,25 @@ from Script import script
 async def start_command(client, message):
     button = [[
         InlineKeyboardButton("Developer", url="https://t.me/MrTG_Coder")
-    ],[
+    ], [
         InlineKeyboardButton("Support Group", url="https://t.me/+1YR5aYuCdr40N2M1"),
         InlineKeyboardButton("Support Channel", url="https://t.me/amal_nath_05"),
     ]]
     reply_markup = InlineKeyboardMarkup(button)
 
-    await message.reply_text("**Welcome to my bot**\n /help to get the help message\n /about just click this\n", reply_markup=reply_markup)
+    await message.reply_text("**Welcome to my bot**\n/help to get the help message\n/about just click this\n", reply_markup=reply_markup)
 
 @Client.on_message(filters.command("help"))
-async def start_command(client, message):
+async def help_command(client, message):
     button = [[
         InlineKeyboardButton("Support Group", url="https://t.me/+1YR5aYuCdr40N2M1"),
         InlineKeyboardButton("Support Channel", url="https://t.me/amal_nath_05"),
     ]]
     reply_markup = InlineKeyboardMarkup(button)
 
-    await message.reply_text("**HELP COMMANDS ARE:\n /openai {works in both group and private chat}\n**", reply_markup=reply_markup)
+    await message.reply_text("**HELP COMMANDS ARE:\n/openai {works in both group and private chat}\n**", reply_markup=reply_markup)
 
 @Client.on_message(filters.command("about"))
-async def start_command(client, message):
+async def about_command(client, message):
     await message.reply_text(ABOUT_TXT)
+
