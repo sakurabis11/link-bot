e2f6f7a15b147bc85dc0457b4d786948c61841b0
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 11:52:43 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -3,25 +3,21 @@ from pyrogram.types import Message
 import openai
 from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY, PORT
 
-openai.api_key = OPENAI_API_KEY  # Set the OpenAI API key
+openai.api_key = OPENAI_API_KEY
 
 @Client.on_message(filters.command("openai"))
 async def openai_command(client, message):
     try:
-        # Get the text from the user's message
         user_input = message.text
 
-        # Use OpenAI API to generate a response
-        response = openai.Completion.create(
-            engine="text-davinci-003",  # Choose the appropriate engine
+        response = await openai.Completion.create(
+            engine="text-davinci-003",
             prompt=user_input,
-            max_tokens=100  # Adjust as needed
+            max_tokens=100
         )
 
-        # Send the generated response back to the user
-    await message.reply_text(response.choices[0].text)
+        await message.reply_text(response.choices[0].text)
 
     except Exception as e:
-        # Handle any errors and send an error message to the user
         error_message = f"Sorry, an error occurred: {str(e)}"
-  await message.reply_text(error_message)
+        await message.reply_text(error_message)
