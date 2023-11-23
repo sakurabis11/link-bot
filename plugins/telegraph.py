e130300b62845db4b72f9c9bce8c522703b6a929
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 23:33:31 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,32 +1,55 @@
 import os, asyncio
+import aiohttp
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from telegraph import upload_file
 from utils import get_file_id
 
 @Client.on_message(filters.command("telegraph"))
 async def telegraph_upload(bot, update):
+    # Check if the user has replied to a message
     replied = update.reply_to_message
     if not replied:
-        return await update.reply_text("Rᴇᴘʟʏ Tᴏ A Pʜᴏᴛᴏ Oʀ Vɪᴅᴇᴏ Uɴᴅᴇʀ 15ᴍʙ")
+        await update.reply_text("Reply To A Photo Or Video Under 15mb")
+        return
+
+    # Get the file information from the replied message
     file_info = get_file_id(replied)
     if not file_info:
-        return await update.reply_text("Not Supported!")
-    text = await update.reply_text(text="<code>Downloading To My Server ...</code>", disable_web_page_preview=True)
+        await update.reply_text("Not Supported!")
+        return
+
+    # Send a message indicating that the file is being downloaded
+    download_text = await update.reply_text(text="<code>Downloading To My Server ...</code>", disable_web_page_preview=True)
+
+    # Download the media from the replied message
     media = await update.reply_to_message.download()
-    await text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)
+
+    # Update the message to indicate that the file has been downloaded
+    await download_text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)
+
+    # Get the upload speed
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://speed.cloudflare.com/api/v4/get') as response:
+            if response.status == 200:
+                upload_speed = await response.json()
+                upload_speed = upload_speed['result']['upload']['bps'] / 1000000
+
+    # Upload the media to Telegraph
     try:
         response = upload_file(media)
     except Exception as error:
         print(error)
-        await text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)
+        await download_text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)
         return
+
+    # Remove the downloaded media file if possible
     try:
         os.remove(media)
     except Exception as error:
         print(error)
-        return
-    await text.edit_text(
-        text=f"https://telegra.ph{response[0]}",
+
+    # Send the Telegraph link and upload speed information to the user
+    await download_text.edit_text(
+        text=f"https://telegra.ph{response[0]}\n\nUpload Speed: {upload_speed:.2f} Mbps",
         disable_web_page_preview=True,
-        )
+    )
