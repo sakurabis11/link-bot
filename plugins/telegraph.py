d6d7e155f8a010616747dc7489e883545da08fb3
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-24 00:13:29 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -4,6 +4,7 @@ from pyrogram import Client, filters
 from telegraph import upload_file
 from utils import get_file_id
 
+
 @Client.on_message(filters.command("telegraph"))
 async def telegraph_upload(bot, update):
     # Check if the user has replied to a message
@@ -19,27 +20,34 @@ async def telegraph_upload(bot, update):
         return
 
     # Send a message indicating that the file is being downloaded
-    download_text = await update.reply_text(text="<code>Downloading To My Server ...</code>", disable_web_page_preview=True)
+    download_text = await update.reply_text(
+        text="<code>Downloading To My Server ...</code>", disable_web_page_preview=True
+    )
 
     # Download the media from the replied message
     media = await update.reply_to_message.download()
 
     # Update the message to indicate that the file has been downloaded
-    await download_text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)
+    await download_text.edit_text(
+        text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>",
+        disable_web_page_preview=True,
+    )
 
     # Get the upload speed
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://speed.cloudflare.com/api/v4/get') as response:
+        async with session.get("https://speed.cloudflare.com/api/v4/get") as response:
             if response.status == 200:
-                upload_speed = await response.json()
-                upload_speed = upload_speed['result']['upload']['bps'] / 1000000
+                upload_speed_data = await response.json()
+                upload_speed = upload_speed_data["result"]["upload"]["bps"] / 1000000
 
     # Upload the media to Telegraph
     try:
         response = upload_file(media)
     except Exception as error:
         print(error)
-        await download_text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)
+        await download_text.edit_text(
+            text=f"Error :- {error}", disable_web_page_preview=True
+        )
         return
 
     # Remove the downloaded media file if possible
