b9211367dd4affa6a7a829212e62b9a697946210
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 21:01:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,7 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram import enums, filters, Client
 from info import API_ID, API_HASH, BOT_TOKEN, PORT
-import pyrogram
+from Script import script
 
 @Client.on_message(filters.command("start"))
 async def start_command(client, message):
@@ -13,4 +13,18 @@ async def start_command(client, message):
     ]]
     reply_markup = InlineKeyboardMarkup(button)
 
-    await message.reply_text("**Welcome to my bot**", reply_markup=reply_markup)
+    await message.reply_text("**Welcome to my bot**\n /help to get the help message\n /about just click this\n", reply_markup=reply_markup)
+
+@Client.on_message(filters.command("help"))
+async def start_command(client, message):
+    button = [[
+        InlineKeyboardButton("Support Group", url="https://t.me/+1YR5aYuCdr40N2M1"),
+        InlineKeyboardButton("Support Channel", url="https://t.me/amal_nath_05"),
+    ]]
+    reply_markup = InlineKeyboardMarkup(button)
+
+    await message.reply_text("**HELP COMMANDS ARE:\n /openai {works in both group and private chat}\n**", reply_markup=reply_markup)
+
+@Client.on_message(filters.command("about"))
+async def start_command(client, message):
+    await message.reply_text(ABOUT_TXT)
