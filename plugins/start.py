b48a9b06cdb7af234bbfdc2b54f3513389468835
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 06:23:32 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,44 +1,62 @@
-    elif query.data == "start":
-        buttons = [[
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram import enums, filters, Client
+from Script import script
+from info import API_ID, API_HASH, BOT_TOKEN, PORT
+import asyncio
+import re
+
+MSG_ALRT = "HI"
+
+async def handle_callback_query(query):
+    if query.data == "start":
+        await show_start_menu(query)
+    elif query.data == "help":
+        await show_help_menu(query)
+    elif query.data == "about":
+        await show_about_menu(query)
+
+async def show_start_menu(query):
+    buttons = [
+        [
             InlineKeyboardButton("Hᴇʟᴩ 🕸️", callback_data="help"),
             InlineKeyboardButton("Aʙᴏᴜᴛ ✨", callback_data="about")
-         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
-        await query.answer(MSG_ALRT)
-    elif query.data == "help":
-        buttons = [[
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    message_text = script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME)
+
+    await edit_message_with_processing(query.message, message_text, reply_markup)
+
+async def show_help_menu(query):
+    buttons = [
+        [
             InlineKeyboardButton('🏠 ʜᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('🔒 ᴄʟᴏꜱᴇ', callback_data="close_data")
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text="Pʀᴏᴄᴇꜱꜱɪɴɢ•"
-        )
-        await query.message.edit_text(
-            text="Pʀᴏᴄᴇꜱꜱɪɴɢ••"
-        )
-        await query.message.edit_text(
-            text="Pʀᴏᴄᴇꜱꜱɪɴɢ•••"
-        )       
-        await query.message.edit_text(                     
-            text=script.HELP_TXT.format(query.from_user.mention),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
-        
-    elif query.data == "about":
-        buttons = [[
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    processing_messages = ["Pʀᴏᴄᴇꜱꜱɪɴɢ•", "Pʀᴏᴄᴇꜱꜱɪɴɢ••", "Pʀᴏᴄᴇꜱꜱɪɴɢ•••"]
+    for text in processing_messages:
+        await query.message.edit_text(text=text)
+    
+    help_text = script.HELP_TXT.format(query.from_user.mention)
+    await edit_message_with_processing(query.message, help_text, reply_markup)
+
+async def show_about_menu(query):
+    buttons = [
+        [
             InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.ABOUT_TXT.format(temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    about_text = script.ABOUT_TXT.format(temp.B_NAME)
+    await edit_message_with_processing(query.message, about_text, reply_markup)
+
+async def edit_message_with_processing(message, text, reply_markup):
+    await message.edit_text(text="Pʀᴏᴄᴇꜱꜱɪɴɢ•")
+    await message.edit_text(text="Pʀᴏᴄᴇꜱꜱɪɴɢ••")
+    await message.edit_text(text="Pʀᴏᴄᴇꜱꜱɪɴɢ•••")
+    await message.edit_text(text=text, reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
