7989fe70b54926f787b3831ee080239b9572597c
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 08:13:28 +0530

    Create gpt.py

---
+++ plugins/gpt.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import openai
+
+# Define a message handler
+@Client.on_message(filters.text)
+async def generate_response(_, message: Message):
+    try:
+        # Get the text from the user's message
+        user_input = message.text
+
+        # Use OpenAI API to generate a response
+        response = openai.Completion.create(
+            engine="text-davinci-003",  # Choose the appropriate engine
+            prompt=user_input,
+            max_tokens=1024  # Adjust as needed
+        )
+
+        # Send the generated response back to the user
+        message.reply_text(response.choices[0].text)
+
+    except Exception as e:
+        # Handle any errors and send an error message to the user
+        error_message = f"Sorry, an error occurred: {str(e)}"
+        message.reply_text(error_message)
