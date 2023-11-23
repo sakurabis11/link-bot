cd3c4bf2347552043720302a4117aef4a61b79e4
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 20:08:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,10 @@ import pyrogram
 @Client.on_message(filters.command("start"))
 async def start_command(client, message):
     button = [[
-        InlineKeyboardButton("Developer", url="https://t.me/MrTG_Coder"),
+        InlineKeyboardButton("Developer", url="https://t.me/MrTG_Coder")
+    ],[
+        InlineKeyboardButton("Support Group", url="https://t.me/+1YR5aYuCdr40N2M1"),
+        InlineKeyboardButton("Support Channel", url="https://t.me/amal_nath_05"),
     ]]
     reply_markup = InlineKeyboardMarkup(button)
 
