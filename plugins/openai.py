dbde6928d926b368fe8cfd2ceb5c99ce0cbc16bc
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 09:21:25 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -5,17 +5,23 @@ from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY, PORT
 
 openai.api_key = OPENAI_API_KEY  # Set the OpenAI API key
 
-@Client.on_message(filters.command("gpt"))
-async def gpt(client, message):
+@Client.on_message(filters.command("openai"))
+async def openai_command(client, message):
     try:
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            temperature=0.5,
-            max_tokens=1024,
-            top_p=1,
-            frequency_penalty=0.0,
-        )
-        await message.reply_text(response.choices[0].text)
-    except Exception as e:
-        await message.reply_text(f"Error: {e}")
+        # Get the text from the user's message
+        user_input = message.text
 
+        # Use OpenAI API to generate a response
+        response = openai.Completion.create(
+            engine="text-davinci-003",  # Choose the appropriate engine
+            prompt=user_input,
+            max_tokens=100  # Adjust as needed
+        )
+
+        # Send the generated response back to the user
+        message.reply_text(response.choices[0].text)
+
+    except Exception as e:
+        # Handle any errors and send an error message to the user
+        error_message = f"Sorry, an error occurred: {str(e)}"
+        message.reply_text(error_message)
