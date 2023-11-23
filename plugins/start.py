626fce51215b2f0f398c45b902e9807e6e2999cb
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 09:14:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,14 +1,7 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram import enums, filters, Client
 from info import API_ID, API_HASH, BOT_TOKEN, PORT
 import pyrogram
 
 @Client.on_message(filters.command("start"))
-def start_command(client, message):
-    keyboard = [[
-        InlineKeyboardButton("Developer", url="https://t.me/Unni0240"),
-    ]]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    message.reply_text("Welcome to my bot!", reply_markup=reply_markup)
+async def start_command(client, message):
+      message.reply_text("Welcome to my bot!")
