086b7ae52e0f4875057a6020b916f758ee27cbd9
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-23 23:30:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -32,9 +32,11 @@ async def start_command(client, message):
 
 @Client.on_message(filters.command("help"))
 async def help_command(client, message):
-    button = [[
-        InlineKeyboardButton("Back to Start", callback_data="start"),
-        InlineKeyboardButton("Close", callback_data="close_data")
+    buttons = [[
+         InlineKeyboardButton('Telegraph', callback_data='telegraph'),
+         InlineKeyboardButton('Openai', callback_data='openai')
+         ],[
+         InlineKeyboardButton('Back', callback_data='start')
     ]]
     reply_markup = InlineKeyboardMarkup(button)
     await message.reply_text("**HELPS**", reply_markup=reply_markup)
@@ -42,8 +44,7 @@ async def help_command(client, message):
 @Client.on_message(filters.command("about"))
 async def about_command(client, message):
     button = [[
-        InlineKeyboardButton("Back to Start", callback_data="start"),
-        InlineKeyboardButton("Close", callback_data="close_data")
+        InlineKeyboardButton("Back to Start", callback_data="start")
     ]]
     reply_markup = InlineKeyboardMarkup(button)
     await message.reply_text(ABOUT_TXT, reply_markup=reply_markup)
@@ -62,16 +63,31 @@ async def callback_handler(client, callback_query):
 
     if query.data == "help":
         buttons = [[
-            InlineKeyboardButton('Back', callback_data='start'), 
-            InlineKeyboardButton('Close', callback_data='close_data')
+            InlineKeyboardButton('Telegraph', callback_data='telegraph'),
+            InlineKeyboardButton('Openai', callback_data='openai')
+            ],[
+            InlineKeyboardButton('Back', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text("**HELPS**", reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
 
+    if query.data == "telegraph"
+        buttons = [[
+            InlineKeyboardButton('Back', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text("**/telegraph Rᴇᴘʟʏ Tᴏ A Pʜᴏᴛᴏ Oʀ Vɪᴅᴇᴏ**", reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
+
+    if query.data == "openai"
+        buttons = [[
+            InlineKeyboardButton('Back', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text("**/openai {ur question}\n Sometimes it will not work has very well**", reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
+    
     if query.data == "about":
         buttons = [[
-            InlineKeyboardButton('Back', callback_data='start'),
-            InlineKeyboardButton('Close', callback_data='close_data')
+            InlineKeyboardButton('Back', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(ABOUT_TXT, reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
