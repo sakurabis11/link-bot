14ef2305893a89e0c59cc96be2481e32243a3ce8
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-24 19:31:19 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -10,7 +10,6 @@ def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
-
 @Client.on_message(filters.command('song'))
 def a(client, message):
     query = ''
@@ -27,18 +26,16 @@ def a(client, message):
                 time.sleep(1)
             results = YoutubeSearch(query, max_results=1).to_dict()
             count += 1
-        # results = YoutubeSearch(query, max_results=1).to_dict()
+
         try:
             link = f"https://youtube.com{results[0]['url_suffix']}"
-            # print(results)
             title = results[0]["title"]
             thumbnail = results[0]["thumbnails"][0]
             duration = results[0]["duration"]
 
-            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
-            # if time_to_seconds(duration) >= 1800:  # duration limit
-            #     m.edit("Exceeded 30mins cap")
-            #     return
+            if time_to_seconds(duration) >= 1800:  # duration limit
+                m.edit("Exceeded 30mins cap")
+                return
 
             views = results[0]["views"]
             thumb_name = f'thumb{message.message_id}.jpg'
@@ -51,7 +48,7 @@ def a(client, message):
             return
     except Exception as e:
         m.edit(
-            "✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.\n\n𝐓𝐫𝐲 𝐀𝐧𝐨𝐭𝐡𝐞𝐫 𝐊𝐞𝐲𝐰𝐨𝐫𝐝 𝐎𝐫 𝐌𝐚𝐲𝐛𝐞 𝐒𝐩𝐞𝐥𝐥 𝐈𝐭 𝐏𝐫𝐨𝐩𝐞𝐫𝐥𝐲.\n\nEg.`/song wanna be yours`"
+            "✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.\n\n𝐓𝐫𝐲 𝐀𝐧𝐨𝐭𝐡𝐞𝐫 𝐊𝐞𝐲𝐰𝐨𝐫𝐤 𝐎𝐫 𝐌𝐚𝐲𝐛𝐞 𝐒𝐩𝐞𝐥𝐥 𝐈𝐭 𝐏𝐫𝐨𝐩𝐞𝐫𝐥𝐲.\n\nEg.`/song wanna be yours`"
         )
         print(str(e))
         return
@@ -60,4 +57,19 @@ def a(client, message):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_
+            ydl.process_info(info_dict)
+        rep = f'🎧 𝐓𝐢𝐭𝐥𝐞 : [{title[:35]}]({link})\n⏳ 𝐃𝐮𝐫𝐚𝐭𝐢𝐨𝐧 : `{duration}`\n👁‍🗨 𝐕𝐢𝐞𝐰𝐬 : `{views}'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, parse_mode='md', quote=False, title=title, duration=dur, thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit('❌ 𝐄𝐫𝐫𝐨𝐫\n\n Report This Error To Fix @mrtgbot_support ❤️')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
