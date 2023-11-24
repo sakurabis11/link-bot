d5c0ee6c9d22f277450060aba71b9b257d76646d
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-24 22:16:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ API_ID = int(os.environ.get('API_ID', '8914119'))
 API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '6645084082:AAFz1M-IliiL3EEqPVQans-JjXJLVlHiWpc')
 PORT = os.environ.get("PORT", "8080")
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1342641151').split()]
