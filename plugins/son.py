cad07b4bd47d0935ec34c7f82f399c03d21b14dc
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-24 23:04:36 +0530

    Create son.py

---
+++ plugins/son.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+import requests
+import os
+
+bot_token = "YOUR_BOT_TOKEN"
+app_id = "YOUR_APP_ID"
+app_hash = "YOUR_APP_HASH"
+
+bot = Client(
+    "SaavnDownloaderBot",
+    bot_token=bot_token,
+    api_id=app_id,
+    api_hash=app_hash
+)
+
+@bot.on_message(filters.command("song"))
+def download_song(client, message):
+    query = ' '.join(message.command[1:])
+    response = requests.get(f"https://www.jiosaavn.com/api.php?query={query}&__call=autocomplete.get&_format=json&_marker=0")
+    data = response.json()
+    song_id = data['songs']['data'][0]['id']
+    song_response = requests.get(f"https://www.jiosaavn.com/api.php?__call=song.getDetails&pids={song_id}&_format=json&_marker=0&model=SM-G930F")
+    song_data = song_response.json()
+    song_url = song_data[song_id]['media_preview_url'].replace("preview", "aac")
+    song_name = song_data[song_id]['song']
+    r = requests.get(song_url)
+    with open(f"{song_name}.mp3", 'wb') as f:
+        f.write(r.content)
+    audio = open(f"{song_name}.mp3", 'rb')
+    bot.send_audio(chat_id=message.chat.id, audio=audio, caption=song_name)
+    os.remove(f"{song_name}.mp3")
+
+bot.run()
