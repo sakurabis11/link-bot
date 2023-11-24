9998deb903fe5dd8340e0ac747d23e4798cb9737
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-24 13:58:49 +0530

    Create uptime.py

---
+++ plugins/uptime.py
@@ -0,0 +1,21 @@
+import psutil
+
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("uptime"))
+def uptime_handler(client, message):
+    uptime = psutil.boot_time()
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/")
+
+    message.reply_text(
+        f"Uᴩᴛɪᴍᴇ: {psutil.elapsed_time(uptime)}\\n"
+        f"CPU Uꜱᴀɢᴇ: {cpu_usage:.1f}%\\n"
+        f"RAM Uꜱᴀɢᴇ: {ram_usage:.1f}%\\n"
+        f"Tᴏᴛᴀʟ Dɪꜱᴋ: {disk_usage.total:,} GB\\n"
+        f"Uꜱᴇᴅ Dɪꜱᴋ: {disk_usage.used:,} GB ({disk_usage.percent:.1f}%)\\n"
+        f"Fʀᴇᴇ Dɪꜱᴋ: {disk_usage.free:,} GB"
+    )
+  
+
