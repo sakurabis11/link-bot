81bee4ca0c4f5edea8bf5e5f007df7d788fee877
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-24 19:14:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,9 +32,6 @@ class Bot(Client):
             sleep_threshold=5,
         )
     async def start(self):
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats
         await super().start()
         me = await self.get_me()
         temp.ME = me.id
