85d79af81deb5e56f91de555808325bc32235bf0
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-24 15:45:12 +0530

    Update nothing.py

--- plugins/nothing.py
+++ plugins/nothing.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
-from info import ADMINS, LOG_CHANNEL,
+from info import ADMINS, LOG_CHANNEL, MELCOW_NEW_USERS, MELCOW_VID
 from database.users_chats_db import db
 from utils import get_size, temp, get_settings
 from Script import script
