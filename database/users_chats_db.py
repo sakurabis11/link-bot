f7883f56a5a458cdfd8c14a11451c456f46b018f
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-24 15:05:15 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,7 +1,7 @@
 
 # https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
 import motor.motor_asyncio
-from info import DATABASE_NAME, DATABASE_URI
+from info import DATABASE_URI, DATABASE_NAME
 
 class Database:
     
