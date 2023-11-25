0c01fbf460aa36e5f36e156c242266fd649f4237
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-25 22:47:54 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -1,71 +1,47 @@
 import os
 import shutil
-from random import randint
+from pyrogram import filters, enums
 import random
-import youtube_dl
-import ffmpeg
-from pyrogram import Client, filters, enums
+from random import randint
+from pyrogram import errors
 
-async def download_song(query, randomdir):
-    try:
-        print(f"Downloading song: {query}")
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': f"{randomdir}/{query}.%(ext)s",
-        }
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([f"ytsearch1:{query}"])
-
-        music_file_path = f"{randomdir}/{query}.mp3"
-        print(f"Song downloaded: {music_file_path}")
-        return music_file_path
-    except Exception as e:
-        print(f"Failed to download song: {e}")
-        raise e
-
-@Client.on_message(filters.command('music') & filters.text & filters.incoming)
-async def song(client, message):
+@Client.on_message(filters.command('music') & filters.text)
+async def song(_, message):
     try:
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        k = await message.reply("⌛")
+        k = await message.reply("⌛️")
+        print('⌛️')
 
         try:
-            randomdir = f"/tmp/{str(random.randint(1, 100000000))}"
-            os.mkdir(randomdir)
+            random_dir = f"/tmp/{str(random.randint(1, 100000000))}"
+            os.mkdir(random_dir)
         except Exception as e:
-            await message.reply_text(f"Failed to send song. Retry after some time. Reason: {e}")
+            await message.reply_text(f"Failed to send song, retry after sometime 😥 Reason: {e}")
             return await k.delete()
 
-        query = message.text.split(None, 1)[1].lower()
-        await k.edit("Downloading...")
-        print("Downloading...")
+        query = message.text.split(None, 1)[1]
+        await k.edit("Downloading ⬇️")
+        print('Downloading ⬇️')
+
         await message.reply_chat_action(enums.ChatAction.RECORD_AUDIO)
-
-        path = await download_song(query, randomdir)
-
-        # Check if file exists before sending
-        if not os.path.exists(path):
-            await message.reply_text("Song not found.")
-            return await k.delete()
+        audio_path = await download_songs(query, random_dir)
 
         await message.reply_chat_action(enums.ChatAction.UPLOAD_AUDIO)
-        await k.edit("Uploading...")
-        await message.reply_audio(path)
+        await k.edit('Uploading ⬆️')
+        await message.reply_audio(audio_path)
 
     except IndexError:
-        await message.reply("Please provide a song name after the `/song` command.")
+        await message.reply("Song requires an argument, e.g., /song faded")
+        return await k.delete()
+
     except Exception as e:
-        await message.reply_text(f"Failed to send song. Reason: {e}")
+        await message.reply_text(f"Failed to send song 😥 Reason: {e}")
+
     finally:
         try:
-            # Use a lock to prevent a race condition
-            lock = client._p_lock
-            with lock:
-                shutil.rmtree(randomdir)
-            await message.reply_text("Check out @Spotify_downloa(music) @Spotifynewss(Updates Group)")
-        except Exception as e:
-            print(f"Failed to cleanup temporary directory: {e}")
-
-        # Delete the initial "Please wait" message
-        await k.delete()
+            shutil.rmtree(random_dir)
+            await message.reply_text("Check out new update")
+            return await k.delete()
+        except:
+            pass
