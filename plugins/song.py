a7fcb6c30134104b6fd6295a0f8d98c481a0f28f
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-25 08:58:55 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -1,32 +1,48 @@
-### This download from saavn.me an unofficial api
-from pyrogram import Client,filters
-import requests,os,wget 
+from pyrogram import Client, filters
+import requests, os, wget
 
 @Client.on_message(filters.command('song') & filters.text)
 async def song(client, message):
     try:
-       args = message.text.split(None, 1)[1]
+        args = message.text.split(None, 1)[1]
     except:
         return await message.reply("/song requires an argument.")
-    if args.startswith(" "):
+
+    if not args:
         await message.reply("/song requires an argument.")
         return ""
+
     pak = await client.send_sticker(message.chat.id, 'CAACAgUAAxkBAAJyMWVhaXgwvctsfT0fApCGniRz20upAAKfAwACgSNIVG3KGaDGncrFHgQ')
+
     try:
-        r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
+        response = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
     except Exception as e:
         await pak.edit(str(e))
         return
-    sname = r['data']['results'][0]['name']
-    slink = r['data']['results'][0]['downloadUrl'][4]['link']
-    ssingers = r['data']['results'][0]['primaryArtists']
-  #  album_id = r.json()[0]["albumid"]
-    img = r['data']['results'][0]['image'][2]['link']
+
+    sname = response['data']['results'][0]['name']
+    slink = response['data']['results'][0]['downloadUrl'][4]['link']
+    ssingers = response['data']['results'][0]['primaryArtists']
+    # album_id = r.json()[0]["albumid"]
+    img = response['data']['results'][0]['image'][2]['link']
+
     thumbnail = wget.download(img)
     file = wget.download(slink)
+
+    # Replace "mp4" with "mp3" in the filename
     ffile = file.replace("mp4", "mp3")
+
+    # Rename the file to the final filename
     os.rename(file, ffile)
-    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']})",thumb=thumbnail)
+
+    # Send the audio file with metadata
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,
+                             caption=f"[{sname}]({response['data']['results'][0]['url']})",
+                             thumb=thumbnail)
+
+    # Remove temporary files
     os.remove(ffile)
     os.remove(thumbnail)
+
+    # Delete the sticker message
     await pak.delete()
