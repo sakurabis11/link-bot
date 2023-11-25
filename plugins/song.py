77f6e8a30bd6c68fe60fdaa90fcf5f738158e26f
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-25 08:57:17 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -2,7 +2,7 @@
 from pyrogram import Client,filters
 import requests,os,wget 
 
-@Client.on_message(filters.command('saavn') & filters.text)
+@Client.on_message(filters.command('song') & filters.text)
 async def song(client, message):
     try:
        args = message.text.split(None, 1)[1]
@@ -11,7 +11,7 @@ async def song(client, message):
     if args.startswith(" "):
         await message.reply("/song requires an argument.")
         return ""
-    pak = await message.reply('Downloading...')
+    pak = await client.send_sticker(message.chat.id, 'CAACAgUAAxkBAAJyMWVhaXgwvctsfT0fApCGniRz20upAAKfAwACgSNIVG3KGaDGncrFHgQ')
     try:
         r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
     except Exception as e:
@@ -26,7 +26,6 @@ async def song(client, message):
     file = wget.download(slink)
     ffile = file.replace("mp4", "mp3")
     os.rename(file, ffile)
-    await pak.edit('Uploading...')
     await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']})",thumb=thumbnail)
     os.remove(ffile)
     os.remove(thumbnail)
