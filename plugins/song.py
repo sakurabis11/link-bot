2a8061474ed0058be8187775f5aceb752c18590b
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-25 10:08:15 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -1,58 +1,52 @@
-from pyrogram import Client, filters
+import asyncio
+import pyrogram
 import requests, os, wget
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command('song') & filters.text)
-async def song(client, message):
+async def download_song(song_name, chat_id):
     try:
-        args = message.text.split(None, 1)[1]
+        song_name = message.text.split(None, 1)[1]
     except:
-        return await message.reply("send the the song name.")
-
-    if not args:
-        await message.reply("send the the song name..")
+        await message.reply("Please send the song name.")
         return ""
 
-    pak = await client.send_sticker(message.chat.id, 'CAACAgUAAxkBAAJyMWVhaXgwvctsfT0fApCGniRz20upAAKfAwACgSNIVG3KGaDGncrFHgQ')
+    if not song_name:
+        await message.reply("Please send the song name.")
+        return ""
 
-    try:
-        response = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
+    await message.reply("Searching for the song...")
+
+    # Download the song in a separate task to prevent blocking the main thread
+    asyncio.create_task(download_song(song_name, message.chat.id))
+
+        # Get song information
+        response = requests.get(f"https://saavn.me/search/songs?query={song_name}&page=1&limit=1").json()
+        song_data = response['data']['results'][0]
+
+        # Extract song details
+        song_title = song_data['name']
+        song_artists = song_data['primaryArtists']
+        song_download_url = song_data['downloadUrl'][4]['link']
+
+        # Download thumbnail image
+        thumbnail_url = song_data['image'][2]['link']
+        thumbnail_file = wget.download(thumbnail_url)
+
+        # Download audio file asynchronously
+        async with aiofiles.open(f"song_{song_title}.mp3", mode='wb') as f:
+            async for chunk in requests.get(song_download_url, stream=True).iter_content(chunk_size=1024):
+                await f.write(chunk)
+
+        audio_file = f"song_{song_title}.mp3"
+
+        # Send audio file with metadata and inline buttons
+        await bot.send_audio(chat_id, audio=audio_file, title=song_title, performer=song_artists,
+                             thumb=thumbnail_file)
+
+        # Remove temporary files
+        os.remove(audio_file)
+        os.remove(thumbnail_file)
+
     except Exception as e:
-        await pak.edit(str(e))
-        return
-
-    sname = response['data']['results'][0]['name']
-    slink = response['data']['results'][0]['downloadUrl'][4]['link']
-    ssingers = response['data']['results'][0]['primaryArtists']
-
-    # Download the thumbnail image
-    img = response['data']['results'][0]['image'][2]['link']
-    thumbnail = wget.download(img)
-
-    # Download the audio file
-    file = wget.download(slink)
-
-    # Replace "mp4" with "mp3" in the filename
-    ffile = file.replace("mp4", "mp3")
-
-    # Rename the file to the final filename
-    os.rename(file, ffile)
-
-    # Generate inline buttons for music streaming services
-    spotify_button = InlineKeyboardButton("Spotify", url=f"https://open.spotify.com/search?q={sname}")
-    youtube_button = InlineKeyboardButton("YouTube", url=f"https://www.youtube.com/results?search_query={sname}")
-    saavn_button = InlineKeyboardButton("Saavn", url=response['data']['results'][0]['url'])
-
-    # Create an inline keyboard markup and add the buttons
-    keyboard = InlineKeyboardMarkup([[spotify_button], [youtube_button], [saavn_button]])
-
-    # Send the audio file with metadata and inline buttons
-    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,
-                             thumb=thumbnail, reply_markup=keyboard)
-
-    # Remove temporary files
-    os.remove(ffile)
-    os.remove(thumbnail)
-
-    # Delete the sticker message
-    await pak.delete()
+        await bot.send_message(chat_id, f"An error occurred while downloading the song: {e}")
