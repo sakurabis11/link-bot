33fa5c00b5a3e0ab05728646652f0781aa195c7f
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-26 06:58:40 +0530

    Update image.py

--- plugins/image.py
+++ plugins/image.py
@@ -1,10 +1,10 @@
 import pyrogram
 from pyrogram import Client, filters, enums
 import requests
-
+from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY PORT
 
 @Client.on_message(filters.command("generate"))
-def on_message(client, message):
+async def on_message(client, message):
         response = requests.get("https://api.openai.com/v1/images/generate", params={
             "model": "dall-e-3",
             "prompt": "a white siamese cat",
@@ -12,7 +12,7 @@ def on_message(client, message):
             "quality": "standard",
             "n": 1,
         }, headers={
-            "Authorization": "Bearer YOUR_OPENAI_API_KEY",
+            "Authorization": "Bearer {OPENAI_API_KEY}",
         })
 
         image_url = response.json()["data"][0]["url"]
