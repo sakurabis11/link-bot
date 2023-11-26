fbb27666ee4c3a9f92bc63cd948c1463be605594
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-26 16:42:12 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -2,36 +2,42 @@ import os
 import logging
 from pyrogram import Client, filters
 import requests
-import json
 
 logging.basicConfig(level=logging.INFO)
 
-GROUP_CHAT_ID = "-1001568397419"
-
-@Client.on_message(filters.command('ringtune') & filters.text)
+@Client.on_message(filters.text)
 async def song(client, message):
     query = message.text
 
     # Send a request to the Deezer API with the search query
     response = requests.get(f"https://api.deezer.com/search?q={query}")
 
-    # Convert the response to JSON format
-    result = response.json()
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Convert the response to JSON format
+        result = response.json()
 
-    # Get the first result (most relevant result)
-    song = result["data"][0]
+        # Check if there are any search results
+        if "data" in result and result["data"]:
+            # Get the first result (most relevant result)
+            song = result["data"][0]
 
-    # Get the song details
-    artist = song["artist"]["name"]
-    title = song["title"]
-    duration = song["duration"]
-    preview_url = song["preview"]
+            # Get the song details
+            artist = song["artist"]["name"]
+            title = song["title"]
+            duration = song["duration"]
+            preview_url = song["preview"]
 
-    # Send a message to the user with the song details and a download link
-    client.send_message(message.chat.id, f"Artist: {artist}\nTitle: {title}\nDuration: {duration} seconds\nPreview: {preview_url}")
+            # Send a message to the user with the song details and a download link
+            message_text = f"Artist: {artist}\nTitle: {title}\nDuration: {duration} seconds\nPreview: {preview_url}"
+            await client.send_message(message.chat.id, message_text)
 
-    # Send a chat action to indicate that the bot is uploading an audio file
-    client.send_chat_action(message.chat.id, "upload_audio")
+            # Send a chat action to indicate that the bot is uploading an audio file
+            await client.send_chat_action(message.chat.id, "upload_audio")
 
-    # Send the audio file to the user
-    client.send_audio(message.chat.id, preview_url, title=title, performer=artist)
+            # Send the audio file to the user
+            await client.send_audio(message.chat.id, audio=preview_url, title=title, performer=artist)
+        else:
+            await client.send_message(message.chat.id, "No results found.")
+    else:
+        await client.send_message(message.chat.id, "Error accessing Deezer API.")
