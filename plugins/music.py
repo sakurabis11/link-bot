eba180ad32c2b59b73c892f95ef32ec726cd5a04
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-26 07:48:28 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -8,9 +8,14 @@ from youtube_dl import YoutubeDL
 from yt_dlp import YoutubeDL
 from random import randint
 import shutil
+import asyncio
+import os
 
 
 DOWNLOAD_DIRECTORY = "downloads"
+
+DOWNLOAD_DIRECTORY = "downloads"
+callback_data_mapping = {}
 
 @Client.on_message(filters.command("download"))
 async def download_music(client, message):
@@ -25,20 +30,29 @@ async def download_music(client, message):
             video_url = video_info['entries'][0]['url']
             video_title = video_info['entries'][0]['title']
 
-            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Download", callback_data=f"download:{video_url}")]])
+            # Use a short identifier as callback data
+            callback_data = f"download:{randint(1000, 9999)}"
+            callback_data_mapping[callback_data] = video_url
+
+            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Download", callback_data=callback_data)]])
             await message.reply("Song found:", reply_markup=keyboard)
     except Exception as e:
         await message.reply(f"Error: {e}")
 
-@Client.on_callback_query(("download:"))
+@Client.on_callback_query(filters.regex(r"download:\d+"))
 async def download_song(client, callback_query):
-    video_url = callback_query.data.split(":")[1]
-    song_title = video_url.split("/")[-1].split(".")[0]
+    callback_data = callback_query.data
+    video_url = callback_data_mapping.get(callback_data)
 
-    try:
-        with YoutubeDL({'format': 'best', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}]}) as ydl:
-            ydl.download([video_url])
+    if video_url:
+        song_title = video_url.split("/")[-1].split(".")[0]
 
-        await callback_query.edit_message_text(f"Song downloaded: {song_title}.mp3")
-    except Exception as e:
-        await callback_query.edit_message_text(f"Error downloading song: {e}")
+        try:
+            with YoutubeDL({'format': 'best', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}]}) as ydl:
+                ydl.download([video_url])
+
+            await callback_query.edit_message_text(f"Song downloaded: {song_title}.mp3")
+        except Exception as e:
+            await callback_query.edit_message_text(f"Error downloading song: {e}")
+    else:
+        await callback_query.edit_message_text("Invalid callback data. Please try again.")
