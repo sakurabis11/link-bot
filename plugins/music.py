1c2de3dc563009427d6f62f04b06e3b75bbb421c
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-26 13:59:50 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -5,7 +5,7 @@ import requests
 
 logging.basicConfig(level=logging.INFO)
 
-@Client.on_message(filters.text)
+@Client.on_message(filters.command("ringtune"))
 async def song(client, message):
     query = message.text
 
@@ -26,19 +26,18 @@ async def song(client, message):
             artist = song["artist"]["name"]
             title = song["title"]
             duration = song["duration"]
-            preview_url = song["preview"]
 
             # Send a message to the user with the song details and a download link
-            message_text = f"Artist: {artist}\nTitle: {title}\nDuration: {duration} seconds\nPreview: {preview_url}"
+            message_text = f"Artist: {artist}\nTitle: {title}\nDuration: {duration} seconds\n"
             await client.send_message(message.chat.id, message_text)
 
             # Send a chat action to indicate that the bot is uploading an audio file
             await client.send_chat_action(message.chat.id, "upload_audio")
 
             # Send the audio file to the user
-            await client.send_audio(message.chat.id, audio=preview_url, title=title, performer=artist)
+            await client.send_audio(message.chat.id, title=title, performer=artist)
         else:
             await client.send_message(message.chat.id, "No results found.")
     else:
-        await client.send_message(message.chat.id, "Error accessing Deezer API.")
+        await client.send_message(message.chat.id, "Error accessing\n forward this message to @mrtgbot_support.")
 
