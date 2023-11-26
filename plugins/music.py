feedcb76c8c24a850eca72dc82c461ee86ea5283
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-26 13:21:29 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -1,11 +1,11 @@
 import os
 import logging
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 import requests
 
 logging.basicConfig(level=logging.INFO)
 
-@Client.on_message(filters.command("music") & filters.text)
+@Client.on_message(filters.text)
 async def song(client, message):
     query = message.text
 
@@ -30,14 +30,15 @@ async def song(client, message):
 
             # Send a message to the user with the song details and a download link
             message_text = f"Artist: {artist}\nTitle: {title}\nDuration: {duration} seconds\nPreview: {preview_url}"
-            client.send_message(message.chat.id, message_text)
+            await client.send_message(message.chat.id, message_text)
 
             # Send a chat action to indicate that the bot is uploading an audio file
-            client.send_chat_action(message.chat.id, "upload_audio")
+            await client.send_chat_action(message.chat.id, "upload_audio")
 
             # Send the audio file to the user
-            client.send_audio(message.chat.id, preview_url, title=title, performer=artist)
+            await client.send_audio(message.chat.id, audio=preview_url, title=title, performer=artist)
         else:
-            client.send_message(message.chat.id, "No results found.")
+            await client.send_message(message.chat.id, "No results found.")
     else:
-        client.send_message(message.chat.id, "Error accessing Deezer API.")
+        await client.send_message(message.chat.id, "Error accessing Deezer API.")
+
