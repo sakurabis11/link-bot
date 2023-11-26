778b30351099aef39ec9328f693560a771bdee93
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-26 23:37:01 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -33,7 +33,7 @@ async def song(client, message):
                 preview_url = song["preview"]
 
                 # Send a message to the user with the song details and a download link
-                message_text = f""ʜᴇʏ <a href='tg://user?id={from_user.id}'><b>✨</b></a>\n\n\nʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ɪs {query}\n\nᴛɪᴛʟᴇ: {title}\nᴀʀᴛɪsᴛ: {artist}\nᴅᴜʀᴀᴛɪᴏɴ: {duration} sᴇᴄᴏɴᴅs\nᴅᴏᴡɴʟᴏᴀᴅ ғʀᴏᴍ ᴄʜʀᴏᴍᴇ: {preview_url}\n\nᴛʜɪs ɪs {query} ʀɪɴɢᴛᴜɴᴇ"
+                message_text = f""ʜᴇʏ <a href='tg://user?id={from_user.id}'><b> ʏᴏᴜ </b></a>\n\n\nʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ɪs {query}\n\nᴛɪᴛʟᴇ: {title}\nᴀʀᴛɪsᴛ: {artist}\nᴅᴜʀᴀᴛɪᴏɴ: {duration} sᴇᴄᴏɴᴅs\nᴅᴏᴡɴʟᴏᴀᴅ ғʀᴏᴍ ᴄʜʀᴏᴍᴇ: {preview_url}\n\nᴛʜɪs ɪs {query} ʀɪɴɢᴛᴜɴᴇ"
                 await client.send_message(message.chat.id, message_text)
 
                 # Send a chat action to indicate that the bot is uploading an audio file
