4069825543857ef36f60925bd88e3936b2b5c605
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-27 19:12:53 +0530

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 import requests
 import json
 
@@ -9,10 +9,10 @@ import json
 async def music(client, message):
     # Extract the query from the command message
     query = " ".join(message.command[1:])
-    
+
     # Check if a query is provided
     if not query:
-        client.send_message(message.chat.id, "Please provide a song name to search. Usage: /music <song_name>")
+        await client.send_message(message.chat.id, "Please provide a song name to search. Usage: /music <song_name>")
         return
 
     try:
@@ -27,40 +27,29 @@ async def music(client, message):
 
         # Check if there are any search results
         if "data" not in result or not result["data"]:
-            client.send_message(message.chat.id, "No results found for the given query.")
+            await client.send_message(message.chat.id, "No results found for the given query.")
             return
 
         # Get the first result (most relevant result)
         song = result["data"][0]
 
-        # Get the song details
-        artist = song["artist"]["name"]
-        title = song["title"]
-        duration = song["duration"]
-        preview_url = song["preview"]
+        # Create a dictionary to store the song information
+        song_info = {
+            "artist": song["artist"]["name"],
+            "title": song["title"],
+            "duration": song["duration"],
+            "preview_url": song["preview"],
+        }
 
-        # Send a message to the user with the song details and a download link as a reply to the original message
-        client.send_message(
-            message.chat.id,
-            f"Artist: {artist}\nTitle: {title}\nDuration: {duration} seconds\nPreview: {preview_url}",
-            reply_to_message_id=message.message_id
-        )
+        # Send a message to the user with the song details and a download link
+        await client.send_message(message.chat.id, f"Artist: {song_info['artist']}\nTitle: {song_info['title']}\nDuration: {song_info['duration']} seconds\nPreview: {song_info['preview_url']}")
 
         # Send chat action to indicate that the bot is uploading audio
-        client.send_chat_action(message.chat.id, "upload_audio")
+        await client.send_chat_action(message.chat.id, "upload_audio")
 
-        # Send the audio preview to the user as a reply to the original message
-        client.send_audio(message.chat.id, preview_url, title=title, performer=artist, reply_to_message_id=message.message_id)
-        
+        # Send the audio preview to the user
+        await client.send_audio(message.chat.id, song_info['preview_url'], title=song_info['title'], performer=song_info['artist'])
     except requests.RequestException as e:
         # Handle HTTP request errors
         logging.error(f"Error fetching song information: {e}")
-        client.send_message(message.chat.id, "Error fetching song information. Please try again later.")
-    except json.JSONDecodeError as e:
-        # Handle JSON decoding errors
-        logging.error(f"Error decoding JSON response: {e}")
-        client.send_message(message.chat.id, "Error decoding JSON response from Deezer API. Please try again later.")
-    except Exception as e:
-        # Handle other unexpected errors
-        logging.error(f"An unexpected error occurred: {e}")
-        client.send_message(message.chat.id, "An unexpected error occurred. Please try again later.")
+        await client.send_message
