0a25dfba36668185e82f5a9cb1e11dce9823b9d8
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-27 22:19:58 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -1,73 +1,33 @@
-import wget
-import discord
-import requests
-from pyrogram import filters, Client, enums
-from asyncio import sleep
-from info import LOG_CHANNEL
+import asyncio
+import os
+from pyrogram import Client, filters
+from pytube import YouTube
+from youtube_search import YoutubeSearch
 
-@Client.on_message(filters.command('song') & filters.text)
-async def song(client, message):
-    args = message.text.split(None, 1)[1]
-    if not args:
-        return await message.reply("/song requires an argument.")
 
-    # Download the song
+@Client.on_message(filters.command(["song"]))
+async def download_song(client, message):
+    song_name = message.text.split()[1]
 
-    pak = await message.reply('Downloading...')
+    # Search for the song on YouTube
+    search_results = YoutubeSearch(song_name, max_results=1).to_dict()
+    song_url = search_results[0]["url_suffix"]
+    song_title = search_results[0]["title"]
+
+    # Download the song using pytube
+    yt = YouTube(f"https://www.youtube.com{song_url}")
+    video = yt.streams.filter(only_audio=True).first()
+    audio_filename = f"{song_title}.mp3"
+
     try:
-        response = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
+        video.download(filename=audio_filename)
+        await message.reply(f"Song downloaded: {audio_filename}")
+
+        # Send the downloaded song to the user
+        await message.reply_audio(audio_filename)
+
+        # Delete the downloaded song after sending it
+        os.remove(audio_filename)
     except Exception as e:
-        await pak.edit(str(e))
-        return
+        await message.reply(f"Error downloading song: {e}")
 
-    # Extract song information
-
-    sname = response['data']['results'][0]['name']
-    slink = response['data']['results'][0]['downloadUrl'][4]['link']
-    ssingers = response['data']['results'][0]['primaryArtists']
-    thumbnail_url = response['data']['results'][0]['image'][2]['link']
-
-    # Download the thumbnail
-
-    thumbnail = await download_image(thumbnail_url)
-
-    # Download the song file
-
-    file = await download_song(slink)
-
-    # Convert MP4 to MP3
-
-    mp3_file = convert_mp4_to_mp3(file)
-
-    # Send the song message
-
-    await pak.edit('Uploading...')
-    await message.reply_audio(audio=mp3_file, title=sname, performer=ssingers, caption=f"[{sname}]({response['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
-
-    # Cleanup
-
-    os.remove(mp3_file)
-    os.remove(thumbnail)
-    await pak.delete()
-
-    # Log the request
-
-    await client.send_message(LOG_CHANNEL, f"<@{message.from_user.id}> requested a song: {sname}")
-
-
-async def download_image(url):
-    filename = f"song_thumbnail.{url.split('.')[-1]}"
-    await wget.download(url, filename)
-    return filename
-
-
-async def download_song(url):
-    filename = f"song.{url.split('.')[-1]}"
-    await wget.download(url, filename)
-    return filename
-
-
-def convert_mp4_to_mp3(filename):
-    mp3_filename = filename.replace("mp4", "mp3")
-    os.system(f"ffmpeg -i {filename} {mp3_filename}")
-    return mp3_filename
