63c91c157c27a68d78458fb95478fe2b57bf652f
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-11-29 00:20:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ from bs4 import BeautifulSoup
 import requests
 import tempfile
 
+PROGRESS_BAR = "\n\n📁 : {b} | {c}\n🚀 : {a}%\n⚡ : {d}/s\n⏱️ : {f}"
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -291,3 +292,43 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+async def progress_message(current, total, ud_type, message, start):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)                                    
+        progress = "\n{0}{1}".format(
+            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
+            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))]))                                  
+        tmp = progress + PROGRESS_BAR.format(
+            a=round(percentage, 2),
+            b=humanbytes(current),
+            c=humanbytes(total),
+            d=humanbytes(speed),
+            f=estimated_total_time if estimated_total_time != '' else "0 s")                               
+        try:
+            chance = [[InlineKeyboardButton("🚫 Cancel", callback_data="del")]]
+            await message.edit(text="{}\n{}".format(ud_type, tmp), reply_markup=InlineKeyboardMarkup(chance))         
+        except:
+            pass
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+          ((str(hours) + "h, ") if hours else "") + \
+          ((str(minutes) + "m, ") if minutes else "") + \
+          ((str(seconds) + "s, ") if seconds else "") + \
+          ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
