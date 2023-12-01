9ade6a6a2cc0b6a5381c18383e40dec588f349ab
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-01 23:12:44 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 from info import ADMINS
 
 @Client.on_message(filters.command("ping") & filters.user(ADMINS))
-async def ping(_, message):
-    start_t = time.time()
-    rm = await message.reply_text("...")
-    end_t = time.time()
-    time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
+async def ping(client, message):
+    start_time = time.time()
+    reply_message = await message.reply_text("...")
+    end_time = time.time()
+    time_taken_milliseconds = (end_time - start_time) * 1000
+    await reply_message.edit(f"Pong!\n{time_taken_milliseconds:.3f} ms")
