a8a64dff56d8172e62dfa27e4c2d7affd7933711
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-02 06:39:12 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -6,8 +6,8 @@ prefix = '.'
 
 @Client.on_message(filters.command("ping", [prefix=='.']) & filters.user(ADMINS))
 async def ping(client, message):
-  start_time = time.time()
-  reply_message = await message.reply_text("...")
-  end_time = time.time()
-  time_taken_milliseconds = (end_time - start_time) / 1000
-  await reply_message.edit(f"Pong!\n{time_taken_milliseconds:.3f} ms")
+ start_time = time.time()
+ reply_message = await message.reply_text("...")
+ end_time = time.time()
+ time_taken_milliseconds = (end_time - start_time) / 1000
+ await reply_message.edit(f"Pong!\n{time_taken_milliseconds:.3f} ms")
