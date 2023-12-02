52ffeab65201886464361a6862395a2eda313b18
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-02 20:05:37 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -7,40 +7,42 @@ import shutil
 
 TG_DOWNLOAD_DIRECTORY = "./DOWNLOADS/"
 
-@Client.on_message(filters.command("telegraph"))
+@Client.on_message(
+  filters.command("telegraph")
+)
 async def telegraph(client, message):
-    replied = message.reply_to_message
+  replied = message.reply_to_message
 
-    if not replied:
-        await message.reply_text("Reply to a supported media file")
-        return
+  if not replied:
+    await message.reply_text("Reply to a supported media file")
+    return
 
-    file_path = get_file_path(replied)
+  file_path = get_file_path(replied)
 
-    if not file_path:
-        await message.reply_text("Not supported!")
-        return
+  if not file_path:
+    await message.reply_text("Not supported!")
+    return
 
-    try:
-        response = upload_file(file_path)
-    except Exception as document:
-        await message.reply_text(message, text=document)
-    else:
-        await message.reply(
-            f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>",
-            disable_web_page_preview=True
-        )
+  try:
+    response = upload_file(file_path)
+  except Exception as document:
+    await message.reply_text(message, text=document)
+  else:
+    await message.reply(
+      f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>",
+      disable_web_page_preview=True
+    )
 
-    finally:
-        if os.path.exists(file_path):
-            os.remove(file_path)
+  # Only delete the file if the upload was successful
+  if os.path.exists(file_path) and response:
+    os.remove(file_path)
 
 def get_file_path(message):
-    if message.photo or message.video:
-        file_path = message.download(TG_DOWNLOAD_DIRECTORY)
-    elif message.document:
-        file_path = message.document.file_path
-    else:
-        return None
+  if message.photo or message.video:
+    file_path = message.download(TG_DOWNLOAD_DIRECTORY)
+  elif message.document:
+    file_path = message.document.file_path
+  else:
+    return None
 
-    return file_path
+  return file_path
