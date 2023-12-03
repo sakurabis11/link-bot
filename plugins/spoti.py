ecddd4fe87b53c0832d96ce71d3d93bc88f04e25
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-04 00:59:44 +0530

    Update spoti.py

--- plugins/spoti.py
+++ plugins/spoti.py
@@ -1,24 +1,31 @@
-import os
-import requests
+import re
 from pyrogram import Client, filters
 from pyrogram.types import *
-
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import spotdl
+import os
+import requests
+import base64
+import pytube
 
 # Define your client id and client secret
-client_id = 'd3a0f15a75014999945b5628dca40d0a'
-client_secret = 'e39d1705e35c47e6a0baf50ff3bb587f'
+CLIENT_ID = "d3a0f15a75014999945b5628dca40d0a"
+CLIENT_SECRET = "e39d1705e35c47e6a0baf50ff3bb587f"
 
-# Set up spotipy with your client credentials
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+# Encode the client id and client secret
+CREDENTIALS = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode("utf-8")).decode("utf-8")
 
-@Client.on_message(filters.command("spoti"))
+
+async def get_access_token():
+    url = "https://accounts.spotify.com/api/token"
+    headers = {"Authorization": f"Basic {CREDENTIALS}", "Content-Type": "application/x-www-form-urlencoded"}
+    data = {"grant_type": "client_credentials"}
+    response = requests.post(url, headers=headers, data=data)
+    return response.json()["access_token"]
+
+@Client.on_message(filters.command("spotify"))
 async def spotify(client, message):
-    # Get the song name or Spotify URL from the command
-    song_name_or_url = message.command[1:]
+    # Get the song name
+    song_name = message.text.split(" ")[1]
+        song_name_or_url = message.command[1:]
     song_name_or_url = " ".join(song_name_or_url)
 
     # Check if the command argument is a Spotify URL
@@ -29,23 +36,55 @@ async def spotify(client, message):
     else:
         # If it is not a Spotify URL, search for the song on Spotify
         song_name = song_name_or_url
-        results = sp.search(q=song_name, limit=1)
-        song_id = results['tracks']['items'][0]['id']
 
-    # Get the song details from Spotify
-    track = sp.track(song_id)
-    artist = track['artists'][0]['name']
-    name = track['name']
-    album = track['album']['name']
-    release_date = track['album']['release_date']
-    popularity = track['popularity']
+async def search_and_get_song_id(song_name, access_token):
+    url = f"https://api.spotify.com/v1/search?q={song_name}&type=album,track"
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-    # Download the song from Spotify
-    song_url = f"https://open.spotify.com/track/{song_id}"
-    spotdl.download_track(song_url)
+    # Get the first search result
+    item = data["tracks"]["items"][0]
 
-    # Send the song details to the user
-    await message.reply_text(f"Title: {name}\nArtist: {artist}\nAlbum: {album}\nRelease Date: {release_date}\nPopularity: {popularity}")
+    # Get the song ID
+    song_id = item["id"]
 
-    # Upload the song to Telegram as an mp3 file
-    await message.reply_audio(audio=open('song.mp3', 'rb'))
+    return song_id
+
+
+async def get_song_data(song_id, access_token):
+    url = f"https://api.spotify.com/v1/tracks/{song_id}"
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    # Get the song thumbnail and details
+    thumbnail_url = data["album"]["images"][0]["url"]
+    artist = data["artists"][0]["name"]
+    name = data["name"]
+    duration_ms = data["duration_ms"]
+    duration = str(int(duration_ms / 1000)) + " seconds"
+    album = data["album"]["name"]
+    release_date = data["album"]["release_date"]
+    popularity = data["popularity"]
+
+    return thumbnail_url, artist, name, duration, album, release_date, popularity
+
+    # Get the access token and song information
+    access_token = await get_access_token()
+    song_id = await search_and_get_song_id(song_name, access_token)
+    thumbnail_url, artist, name, duration, album, release_date, popularity = await get_song_data(song_id, access_token)
+
+    # Download the song using pytube
+    yt = pytube.YouTube(f"https://open.spotify.com/track/{song_id}")
+    audio_streams = yt.streams.filter(only_audio=True)
+    audio_stream = audio_streams[0]  # Select the first audio stream
+    audio_stream.download()
+
+    # Get the downloaded song filename
+    downloaded_filename = audio_stream.default_filename
+
+    # Send the song file to the user along with song information
+    await message.reply_document(
+        document=downloaded_filename, caption=f"**Song:** {name}\n**Artist:** {artist}\n**Album:** {album}\n**Duration:** {duration}\n**Release Date:** {release_date}\n**Popularity:** {popularity}"
+    )
