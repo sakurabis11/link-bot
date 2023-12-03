74c5f3bd47141065f795b3ffe37a20a4c423468d
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-03 15:38:07 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -5,9 +5,12 @@ from pyrogram import Client, filters
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 
+SPOTIFY_CLIENT_ID = "d3a0f15a75014999945b5628dca40d0a",
+SPOTIFY_CLIENT_SECRET = "e39d1705e35c47e6a0baf50ff3bb587f"
+
 # Create a Spotify client instance
-spotify = Spotify(auth_manager=SpotifyOAuth(client_id=os.environ['d3a0f15a75014999945b5628dca40d0a'],
-                                             client_secret=os.environ['e39d1705e35c47e6a0baf50ff3bb587f'],
+spotify = Spotify(auth_manager=SpotifyOAuth(client_id=os.environ['SPOTIFY_CLIENT_ID'],
+                                             client_secret=os.environ['SPOTIFY_CLIENT_SECRET'],
                                              redirect_uri='http://localhost:8080'))
 
 # Define a command handler for the `/spotify` command
