d1fcb8bb098f47ae24a881d41c93a4dc559be39e
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-03 23:22:41 +0530

    Update git.py

--- plugins/git.py
+++ plugins/git.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("github"))
 async def github(client, message):
-    username = message.text.split(" ", 1)[1]
+    repo_name = message.text.split(" ", 1)[1]
 
-    URL = f"https://api.github.com/users/{username}"
+    URL = f"https://api.github.com/repos/{repo_name}"
     response = requests.get(URL)
 
     if response.status_code == 404:
@@ -17,29 +17,31 @@ async def github(client, message):
     try:
         url = result["html_url"]
         name = result["name"]
-        company = result["company"]
-        bio = result["bio"]
+        full_name = result["full_name"]
+        owner = result["owner"]["login"]
+        description = result["description"]
         created_at = result["created_at"]
-        avatar_url = result["avatar_url"]
-        blog = result["blog"]
-        location = result["location"]
-        repositories = result["public_repos"]
-        followers = result["followers"]
-        following = result["following"]
+        updated_at = result["updated_at"]
+        language = result["language"]
+        forks_count = result["forks_count"]
+        stargazers_count = result["stargazers_count"]
+        watchers_count = result["watchers_count"]
+        open_issues_count = result["open_issues_count"]
 
-        caption = f"""**Info Of {name}**
-**Username:** `{username}`
-**Bio:** `{bio}`
-**Profile Link:** [Here]({url})
-**Company:** `{company}`
+        caption = f"""**Info Of {name} Repository**
+**Full Name:** `{full_name}`
+**Owner:** `{owner}`
+**Description:** `{description}`
+**Repo Link:** Here
 **Created On:** `{created_at}`
-**Repositories:** `{repositories}`
-**Blog:** `{blog}`
-**Location:** `{location}`
-**Followers:** `{followers}`
-**Following:** `{following}`"""
+**Updated On:** `{updated_at}`
+**Language:** `{language}`
+**Forks:** `{forks_count}`
+**Stars:** `{stargazers_count}`
+**Watchers:** `{watchers_count}`
+**Open Issues:** `{open_issues_count}`"""
     except Exception as e:
         print(str(e))
         return
         
-    await client.send_photo(message.chat.id, avatar_url, caption=caption, reply_to_message_id=message.message_id)
+    await client.send_photo(message.chat.id, result["owner"]["avatar_url"], caption=caption, reply_to_message_id=message.message_id)
