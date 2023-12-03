e7e76b016de0b861585827d0802e47c34437f36c
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-04 00:45:32 +0530

    Update spoti.py

--- plugins/spoti.py
+++ plugins/spoti.py
@@ -3,26 +3,20 @@ import requests
 from pyrogram import Client, filters
 from pyrogram.types import *
 import youtube_dl
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotdl
 
-# Define a function to download the song from Spotify
-def download_song_from_spotify(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': 'song.mp3',
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+# Define your client id and client secret
+client_id = 'd3a0f15a75014999945b5628dca40d0a'
+client_secret = 'e39d1705e35c47e6a0baf50ff3bb587f'
+
+# Set up spotipy with your client credentials
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 @Client.on_message(filters.command("spoti"))
 async def spotify(client, message):
-    # Get the access token
-    access_token = get_access_token()
-
     # Get the song name or Spotify URL from the command
     song_name_or_url = message.command[1:]
     song_name_or_url = " ".join(song_name_or_url)
@@ -35,40 +29,23 @@ async def spotify(client, message):
     else:
         # If it is not a Spotify URL, search for the song on Spotify
         song_name = song_name_or_url
-        url = f'https://api.spotify.com/v1/search?q={song_name}&type=album,track'
-        headers = {"Authorization": f"Bearer {access_token}"}
-        response = requests.get(url, headers=headers)
-        data = response.json()
+        results = sp.search(q=song_name, limit=1)
+        song_id = results['tracks']['items'][0]['id']
 
-        # Get the first search result
-        item = data["tracks"]["items"][0]
-
-        # Get the song ID
-        song_id = item["id"]
-
-    # Get the song thumbnail and details from Spotify
-    url = f'https://api.spotify.com/v1/tracks/{song_id}'
-    headers = {"Authorization": f"Bearer {access_token}"}
-    response = requests.get(url, headers=headers)
-    data = response.json()
-
-    # Get the song thumbnail
-    thumbnail_url = data["album"]["images"][0]["url"]
-
-    # Get the song details
-    artist = data["artists"][0]["name"]
-    name = data["name"]
-    duration_ms = data["duration_ms"]
-    duration = str(int(duration_ms / 1000)) + " seconds"
-    album = data["album"]["name"]
-    release_date = data["album"]["release_date"]
-    popularity = data["popularity"]
+    # Get the song details from Spotify
+    track = sp.track(song_id)
+    artist = track['artists'][0]['name']
+    name = track['name']
+    album = track['album']['name']
+    release_date = track['album']['release_date']
+    popularity = track['popularity']
 
     # Download the song from Spotify
-    download_song_from_spotify(url)
+    song_url = f"https://open.spotify.com/track/{song_id}"
+    spotdl.download_track(song_url)
 
-    # Send the song thumbnail and details to the user
-    await message.reply_photo(photo=thumbnail_url, caption=f"Title: {name}\nArtist: {artist}\nDuration: {duration}\nAlbum: {album}\nRelease Date: {release_date}\nPopularity: {popularity}")
+    # Send the song details to the user
+    await message.reply_text(f"Title: {name}\nArtist: {artist}\nAlbum: {album}\nRelease Date: {release_date}\nPopularity: {popularity}")
 
     # Upload the song to Telegram as an mp3 file
     await message.reply_audio(audio=open('song.mp3', 'rb'))
