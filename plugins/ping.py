b3e7d89e54904617a158a1d8535305ee57baca2e
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-04 19:08:38 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -14,8 +14,6 @@ async def ping(client, message):
         end_time = time.time()
         ping_time = float(end_time - start_time)
         await message.edit("🏓 Pong!\n⏱ Speed was : {0:.2f}s".format(round(ping_time, 2) % 60))
-    except FloodWait as e:
-        await message.edit("❗️ Flood wait! Please try again in {0:.2f} seconds.".format(e.x))
     except Exception as e:
         logging.error(e)
         await message.edit("❗️ An error occurred while processing your request.")
