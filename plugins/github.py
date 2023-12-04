c16b294f611e25def79849b0681bb113963be288
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-04 07:34:24 +0530

    Update github.py

--- plugins/github.py
+++ plugins/github.py
@@ -1,39 +1,27 @@
-import requests
+import asyncio
 from pyrogram import Client, filters
+from info import GITHUB_TOKEN
 
+# Define a function to handle messages containing the `github` command
 @Client.on_message(filters.command("github"))
-async def github(client, message):
-    repo_name = message.text.split(" ", 1)[1]
+async def github_command(client, message):
+    # Extract the search query from the message
+    search_query = message.text.split(" ")[1]
 
-    URL = f"https://api.github.com/repos/{repo_name}"
-    response = requests.get(URL)
+    # Use the GitHub API to search for repositories
+    # (Replace `YOUR_GITHUB_TOKEN` with your actual GitHub token)
+    async with client.get_session() as session:
+        async with session.get(f"https://api.github.com/search/repositories?q={search_query}", headers={"Authorization": f"token GITHUB_TOKEN"}) as response:
+            response_json = await response.json()
 
-    if response.status_code == 404:
-        await client.send_message(message.chat.id, "404")
-        return
+    # Extract the search results from the response
+    repositories = response_json["items"]
 
-    result = response.json()
+    # Generate a message with the search results
+    result_message = ""
+    for repository in repositories:
+        result_message += f"Name: {repository['full_name']}\nDescription: {repository['description']}\nURL: {repository['html_url']}\n\n"
 
-    try:
-        name = result["name"]
-        full_name = result["full_name"]
-        owner = result["owner"]["login"]
-        description = result["description"]
-        created_at = result["created_at"]
-        updated_at = result["updated_at"]
-        language = result["language"]
-        forks_count = result["forks_count"]
-        stargazers_count = result["stargazers_count"]
-        watchers_count = result["watchers_count"]
-        open_issues_count = result["open_issues_count"]
+    # Send the search results to the user
+    await message.reply(result_message)
 
-        caption = f"**ɪɴғᴏ ᴏғ {name} ʀᴇᴘᴏsɪᴛᴏʀʏ**\n**Ғᴜʟʟ ɴᴀᴍᴇ:** `{full_name}`\n**ᴏᴡɴᴇʀ:** `{owner}`\n**ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:** `{description}`"
-    except Exception as e:
-        print(str(e))
-        return
-
-    if result["owner"]["avatar_url"]:
-        avatar_url = result["owner"]["avatar_url"]
-        await client.send_photo(message.chat.id, avatar_url, caption=caption, reply_to_message_id=message.message_id)
-    else:
-        await client.send_message(message.chat.id, caption, reply_to_message_id=message.message_id)
