3899a59d5acffbfc9469d64b723f729d1e7a894f
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-05 07:47:53 +0530

    Update carbon.py

--- plugins/carbon.py
+++ plugins/carbon.py
@@ -11,8 +11,7 @@ from random import randint
 from re import findall
 from re import sub as re_sub
 from sys import executable
-import aiofiles
-import speedtest
+import aiofiles 
 from PIL import Image
 from pyrogram.types import Message
 
