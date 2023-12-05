9fab7d2c35e53d0bc04ac2c47833d3e7b268e79f
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-06 05:13:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import re
 import os
 import psutil
 import time
+import math
 from datetime import datetime
 from typing import List
 from bs4 import BeautifulSoup
@@ -17,7 +18,7 @@ import tempfile
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-PROGRESS_BAR = "\n\n📁 : {b} | {c}\n🚀 : {a}%\n⚡ : {d}/s\n⏱️ : {f}"
+PROGRESS_BAR = "\n\n: {b} | {c}\n: {a}%\n⚡: {d}/s\n⏱️: {f}"
 
 BANNED = {}
 SMART_OPEN = '“'
@@ -345,6 +346,32 @@ async def progress_message(current, total, ud_type, message, start):
         pass
 
 
+async def progress_message(current, total, ud_type, message, start):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total if total else 0
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        progress = "\n{0}{1}".format("".join(["⬢" for i in range(math.floor(percentage / 5))]), "".join(["⬡" for i in range(20 - math.floor(percentage / 5))]))
+        tmp = (progress + PROGRESS_BAR).format(
+            a=round(percentage, 2),
+            b=humanbytes(current),
+            c=humanbytes(total),
+            d=humanbytes(speed),
+            f=estimated_total_time if estimated_total_time else "0 s")
+
+        try:
+            chance = [[InlineKeyboardButton(" Cancel", callback_data="del")]]
+            await message.edit(text="{}\n{}".format(ud_type, tmp), reply_markup=InlineKeyboardMarkup(chance))
+        except Exception:
+            # Log the error or handle it silently
+            pass
 
 def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
