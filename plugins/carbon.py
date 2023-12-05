b28bc2474d9cd45d662a25c2dbdfea537045ed1a
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-05 08:12:01 +0530

    Update carbon.py

--- plugins/carbon.py
+++ plugins/carbon.py
@@ -19,29 +19,25 @@ from pyrogram.types import Message
 aiohttpsession = ClientSession()
 
 async def make_carbon(code):
-    url = "https://carbonara.vercel.app/api/cook"
-    async with aiohttpsession.post(url, json={"code": code}) as resp:
-        image = BytesIO(await resp.read())
-    image.name = "carbon.png"
-    return image
-
+  url = "https://carbonara.vercel.app/api/cook"
+  async with aiohttpsession.post(url, json={"code": code}) as resp:
+    image = BytesIO(await resp.read())
+  image.name = "carbon.png"
+  return image
 
 @Client.on_message(filters.command("carbon"))
 async def carbon_func(_, message):
-    if not message.reply_to_message:
-        return await message.reply_text(
-            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
-        )
-    if not message.reply_to_message.text:
-        return await message.reply_text(
-            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
-        )
-    user_id = message.from_user.id
-    m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
-    carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
-    await message.reply_photo(
-        photo=carbon,
-        caption="**✨**")
-    await m.delete()
-    carbon.close()
+  if not message.reply_to_message:
+    return await message.reply_text(
+      "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
+    )
+ 
+  user_id = message.from_user.id
+  m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
+  carbon = await make_carbon(message.reply_to_message.text)
+  await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
+  await message.reply_photo(
+    photo=carbon,
+    caption="**✨**")
+  carbon.close()
+  await m.delete()
