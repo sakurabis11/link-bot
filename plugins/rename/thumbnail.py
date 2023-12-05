1ad3e55dc3ceb450ef7afe370019a922b7a3a20c
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-06 04:45:10 +0530

    Update thumbnail.py

--- plugins/rename/thumbnail.py
+++ plugins/rename/thumbnail.py
@@ -4,7 +4,7 @@ import os
 
 
 
-@Client.on_message(filters.private & filters.photo & filters.user(ADMIN))                            
+@Client.on_message(filters.private & filters.photo & filters.user(ADMINS))                            
 async def set_tumb(client, msg):       
     if len(dir) == 0:
         await bot.download_media(message=msg.photo.file_id, file_name=f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
@@ -15,7 +15,7 @@ async def set_tumb(client, msg):
         return await msg.reply(f"Your permanent thumbnail is saved in dictionary ✅️ \nif you change yur server or recreate the server app to again reset your thumbnail⚠️")            
 
 
-@Client.on_message(filters.private & filters.command("view") & filters.user(ADMIN))                            
+@Client.on_message(filters.private & filters.command("view") & filters.user(ADMINS))                            
 async def view_tumb(client, msg):
     try:
         await msg.reply_photo(photo=f"{DOWNLOAD_LOCATION}/thumbnail.jpg", caption="this is your current thumbnail")
@@ -23,7 +23,7 @@ async def view_tumb(client, msg):
         print(e)
         return await msg.reply_text(text="you don't have any thumbnail")
 
-@Client.on_message(filters.private & filters.command(["del", "del_thumb"]) & filters.user(ADMIN))                            
+@Client.on_message(filters.private & filters.command(["del", "del_thumb"]) & filters.user(ADMINS))                            
 async def del_tumb(client, msg):
     try:
         os.remove(f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
