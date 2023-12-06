8ceb558dfd610601f4c6ee0a469a9ddbdfd77c1c
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-06 21:14:00 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -72,6 +72,8 @@ async def spotify(client, message):
     # Send the song thumbnail and details to the user
     await message.reply_photo(photo=thumbnail_url, caption=f"ʜᴇʏ {message.from_user.mention}\n\n ᴛɪᴛʟᴇ: <code>{name}</code>\nᴀʀᴛɪsᴛ: <code>{artist}</code>\nᴀʟʙᴜᴍ: <code>{album}</code>\nʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ: <code>{release_date}</code>\n")
 
+message = Message
+
 user_id = message.from_user.id
 
 # Download logic using a function instead of undefined `spotify.download`
