4b5b11e222d7434748733a865de47e29db45f101
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-06 14:06:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,25 +15,25 @@ def is_enabled(value, default):
 
 class Info(object):
 # bot information
-API_ID = int(os.environ.get('API_ID', '8914119'))
-API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '6645084082:AAEvdcfTZ0uFS_Gu6KvVPVBMNlrlWiESPiM')
-PORT = os.environ.get("PORT", "8080")
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1342641151').split()]
+    API_ID = int(os.environ.get('API_ID', '8914119'))
+    API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '6645084082:AAEvdcfTZ0uFS_Gu6KvVPVBMNlrlWiESPiM')
+    PORT = os.environ.get("PORT", "8080")
+    CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+    ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1342641151').split()]
 
 # for mongodb
-DB_NAME = os.environ.get("DB_NAME", "mrtg")     
-DB_URL  = os.environ.get("DB_URL", "mongodb+srv://mrtg:3rqnL0nfKO1DgVM2@cluster0.m4nrgsu.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = os.environ.get("DB_NAME", "mrtg")     
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://mrtg:3rqnL0nfKO1DgVM2@cluster0.m4nrgsu.mongodb.net/?retryWrites=true&w=majority")
 
 # for openai
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
 
 #for spotify 
-SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID', 'd3a0f15a75014999945b5628dca40d0a')
-SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET', 'e39d1705e35c47e6a0baf50ff3bb587f')
+    SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID', 'd3a0f15a75014999945b5628dca40d0a')
+    SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET', 'e39d1705e35c47e6a0baf50ff3bb587f')
 
 # optional
-PIC = (environ.get('PICS', 'https://telegra.ph/file/67a238112341dc2da77d9.jpg')).split()
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002084798134'))
+    PIC = (environ.get('PICS', 'https://telegra.ph/file/67a238112341dc2da77d9.jpg')).split()
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002084798134'))
 
