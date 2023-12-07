d020e27bd0850175af2547739db581ad473012cf
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-07 13:19:29 +0530

    Update and rename .py to stats.py

--- plugins/stats.py
+++ plugins/stats.py
@@ -9,8 +9,6 @@ from Script import script
 from pyrogram.errors import ChatAdminRequired
 import asyncio
 
-"""-----------------------------------------https://t.me/GetTGLink/4179 --------------------------------------"""
-
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
     r_j_check = [u.id for u in message.new_chat_members]
