5dd055ff842c641421f803baade7799336b2c2c2
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-08 12:42:19 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -49,7 +49,6 @@ async def start(client, message):
         if not await db.get_chat(message.chat.id):
             total = await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))
-            await db.add_chat(message.chat.id, message.chat.title)
         return
 
     if not await db.is_user_exist(message.from_user.id):
