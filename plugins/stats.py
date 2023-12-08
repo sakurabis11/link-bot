bccd793b684451e0edbd69eed724b800acef9c16
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-08 07:53:36 +0530

    Update stats.py

--- plugins/stats.py
+++ plugins/stats.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, MELCOW_NEW_USERS, MELCOW_IMG, MELCOW_VID
-from database.users import db
+from database.users_db import db
 from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
