05a11f652f792c9e2c970a509d314507f4dc5592
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-08 14:14:26 +0530

    Update stats.py

--- plugins/stats.py
+++ plugins/stats.py
@@ -1,24 +1,24 @@
 import pyrogram
 from pyrogram import Client
 from pymongo import MongoClient
+from database.users_db import db
 from info import DATABASE_NAME, DATABASE_URI
-
-MONGO_URI = DATABASE_URI
-
-    client = MongoClient(MONGO_URI)
-    db = client["mrtg"]
+import psutil
 
 @Client.on_message(filters.command(["stats"]))
 async def stats(client, message):
-  
-    user_count = await db.users.count_documents({})
-    group_count = await db.groups.count_documents({})
-    free_space = psutil.disk_usage('/').free
-    db_stats = await db.command("dbStats")
-    db_size = db_stats['dataSize']
-    await message.reply(f"**Stats:**\n\n"
-                      f"Users: {user_count}\n"
-                      f"Groups: {group_count}\n"
-                      f"Free Space: {free_space / 1024 ** 3:.2f} GB\n"
-                      f"Mongo Space: {db_size / 1024 ** 3:.2f} GB")
 
+  client = MongoClient(DATABASE_URI)
+  db = client[DATABASE_NAME]
+
+  user_count = await db.users.count_documents({})
+  group_count = await db.groups.count_documents({})
+  free_space = psutil.disk_usage('/').free
+  db_stats = await db.command("dbStats")
+  db_size = db_stats['dataSize']
+
+  await message.reply(f"**Stats:**\n\n"
+                       f"Users: {user_count}\n"
+                       f"Groups: {group_count}\n"
+                       f"Free Space: {free_space / 1024 ** 3:.2f} GB\n"
+                       f"Mongo Space: {db_size / 1024 ** 3:.2f} GB")
