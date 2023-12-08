87f9e3f20b18b3e4fe0f8a4b95b0005a12debbf0
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-08 12:40:08 +0530

    Update stats.py

--- plugins/stats.py
+++ plugins/stats.py
@@ -8,67 +8,6 @@ from Script import script
 from pyrogram.errors import ChatAdminRequired
 import asyncio
 
-@Client.on_message(filters.new_chat_members & filters.group)
-async def save_group(bot, message):
-    r_j_check = [u.id for u in message.new_chat_members]
-    if temp.ME in r_j_check:
-        if not await db.get_chat(message.chat.id):
-            total = await bot.get_chat_members_count(message.chat.id)
-            r_j = message.from_user.mention if message.from_user else "Anonymous"
-            await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, r_j))
-            await db.add_chat(message.chat.id, message.chat.title)
-        if message.chat.id in temp.BANNED_CHATS:
-            # Inspired from a boat of a banana tree
-            buttons = [[
-                InlineKeyboardButton('Support', url="https://t.me/amal_nath_05")
-            ]]
-            reply_markup = InlineKeyboardMarkup(buttons)
-            k = await message.reply(
-                text='<b>CHAT NOT ALLOWED \n\nMy admins has restricted me from working here ! If you want to know more about it contact support..</b>',
-                reply_markup=reply_markup,
-            )
-
-            try:
-                await k.pin()
-            except:
-                pass
-            await bot.leave_chat(message.chat.id)
-            return
-
-        buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/mrtgcoderbot?start=help"),
-            InlineKeyboardButton(' Updates', url="https://t.me/amal_nath_05")
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
-            text=f"<b>Thankyou For Adding Me In {message.chat.title} \n\nIf you have any questions & doubts about using me contact support.</b>",
-            reply_markup=reply_markup
-        )
-    else:
-        settings = await get_settings(message.chat.id)
-        if settings["welcome"]:
-            for u in message.new_chat_members:
-                if (temp.MELCOW).get('welcome') is not None:
-                    try:
-                        await (temp.MELCOW['welcome']).delete()
-                    except:
-                        pass
-                temp.MELCOW['welcome'] = await message.reply_video(
-                    video=(MELCOW_VID),
-                    caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
-                    reply_markup=InlineKeyboardMarkup(
-                        [[
-                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url="https://t.me/+1YR5aYuCdr40N2M1"),
-                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url="https://t.me/amal_nath_05")
-                        ]]
-                    ),
-                    parse_mode=enums.ParseMode.HTML
-                )
-
-        if settings["auto_delete"]:
-            await asyncio.sleep(600)
-            await (temp.MELCOW['welcome']).delete()
-
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
     if len(message.command) == 1:
