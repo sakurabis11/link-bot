a2fa2c9722a32e333ca96f222b586d2432a322dc
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-09 00:51:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,3 +11,6 @@ class script(object):
 ◉ ᴀᴄᴄ-ɴᴀᴍᴇ: {}"""
 
     STATUS_TXT =  """<b>◉ ᴛᴏᴛᴀʟ ᴜꜱᴇʀꜱ: <code>{}</code>"""
+
+    ADMIN_COMMANDS = """/broadcast ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ ᴀ ᴍᴇssɢᴇ ᴛᴏ ʟʟ ᴜsᴇʀs\n/leave ᴛᴏ ʟᴇᴀᴠᴇ ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ\n/ban ᴛᴏ ʙᴀɴ ᴀ ᴜsᴇʀ\n/unban ᴛᴏ ᴜɴʙᴀɴ ᴛʜᴇ ʙᴀɴɴᴇᴅ ᴜsᴇʀ\n/users ᴛᴏ ɢᴇᴛ ᴛʜᴇ ʟɪsᴛ ᴏғ ᴜsᴇʀs\n.ping ғᴏʀ ᴛʜᴇ ᴘᴏɴɢ"""
+
