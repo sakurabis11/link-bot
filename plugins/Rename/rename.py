51364e60ae26952a211fde2458211df78527b36b
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-09 13:31:58 +0530

    Update rename.py

--- plugins/Rename/rename.py
+++ plugins/Rename/rename.py
@@ -13,27 +13,36 @@ from asyncio import sleep
 from PIL import Image
 import os, time
 
-
-@Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
+@Client.on_message(filters.private & filters.command("rename"))
 async def rename_start(client, message):
+    # Check if it's the "/rename" command
+    if message.text == "/rename":
+        await message.reply_text("Please reply to a message containing a file to rename it!")
+        return
+
+    # Process media message
     file = getattr(message, message.media.value)
-    filename = file.file_name  
+    filename = file.file_name
+
+    # Check file size
     if file.file_size > 2000 * 1024 * 1024:
-         return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 2Gʙ")
+        await message.reply_text("Sorry Bro This Bot Doesn't Support Uploading Files Bigger Than 2GB")
+        return
 
     try:
+        # Request new filename
         await message.reply_text(
-            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
-	    reply_to_message_id=message.id,  
-	    reply_markup=ForceReply(True)
-        )       
+            text=f"**__Please Enter New Filename...__**\n\n**Old File Name** :- `{filename}`",
+            reply_to_message_id=message.id,
+            reply_markup=ForceReply(True)
+        )
         await sleep(30)
     except FloodWait as e:
         await sleep(e.value)
         await message.reply_text(
-            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
-	    reply_to_message_id=message.id,  
-	    reply_markup=ForceReply(True)
+            text=f"**__Please Enter New Filename...__**\n\n**Old File Name** :- `{filename}`",
+            reply_to_message_id=message.id,
+            reply_markup=ForceReply(True)
         )
     except:
         pass
