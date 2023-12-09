10d98e51aa55dcaeeda2a6827da729b7391eb085
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-09 12:01:53 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -199,11 +199,11 @@ async def callback_handler(client, callback_query):
         
         reply_markup = InlineKeyboardMarkup(buttons)
         users = await db.total_users_count()
-        monsize = await db.get_db_size()
-        free = 536870912 - monsize
-        monsize = get_size(monsize)
-        free = get_size(free)        
-        await query.message.edit_text(text=script.STATUS_TXT.format(users, monsize, free),reply_markup=reply_markup,parse_mode=enums.ParseMode.HTML)
+        size = await db.get_db_size()
+        free = 536870912 - size
+        size = get_size(size)
+        free = get_size(free)     
+        await query.message.edit_text(text=script.STATUS_TXT.format(total_users, size, free),reply_markup=reply_markup,parse_mode=enums.ParseMode.HTML)
     
     if query.data == "about":
         buttons = [[
