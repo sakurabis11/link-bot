de06350cd49f3a8a6174ab4e5b112659246f218e
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-10 19:22:46 +0530

    Update ytv.py

--- plugins/ytv.py
+++ plugins/ytv.py
@@ -1,22 +1,41 @@
 from pyrogram import Client, filters
 from pytube import YouTube
+import os
+
+def update_message(client, message, bytes_remaining):
+    # Update download progress message
+    remaining_mb = round(bytes_remaining / 1048576, 2)
+    await client.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text=f"Downloading... (Remaining: {remaining_mb} MB)")
 
 @Client.on_message(filters.command("ytv"))
 async def download_video(client, message):
-  try:
-    # Extract the YouTube URL from the message text
-    url = message.text.split(" ")[1]
+    try:
+        # Get video URL from message
+        url = message.text.split(" ")[1]
 
-    # Download the video using pytube
-    yt = YouTube(url)
-    video = yt.streams.first()
-    video.download()
+        # Download video using pytube
+        yt = YouTube(url)
 
-    # Upload the downloaded video to the user
-    await client.send_video(message.chat.id, video.filename, caption=f"Downloaded video from: {url}")
+        # Get best available video format
+        video = yt.streams.filter(progressive=True, only_audio=False).order_by('resolution').desc().first()
 
-    # Delete the downloaded file
-    os.remove(video.filename)
-  except Exception as e:
-    await client.send_message(message.chat.id, f"Error downloading video: {e}")
+        # Create temporary video filename with extension
+        filename = f"{yt.title}.{video.extension}"
+
+        # Download video and update progress
+        video.download(filename=filename, progress_hook=lambda chunk, file_handle, bytes_remaining: update_message(client, message, bytes_remaining))
+
+        # Upload video with caption
+        await client.send_video(message.chat.id, filename, caption=f"Downloaded video from: {url}")
+
+        # Delete downloaded file
+        os.remove(filename)
+    except Exception as e:
+        # Handle different exceptions with specific messages
+        if isinstance(e, KeyError):
+            await client.send_message(message.chat.id, f"Invalid video format.")
+        elif isinstance(e, OSError):
+            await client.send_message(message.chat.id, f"Error writing video file.")
+        else:
+            await client.send_message(message.chat.id, f"Error downloading video: {e}")
 
