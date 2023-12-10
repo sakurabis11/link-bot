8af695a520823890035e4d7138f3846c415a8ab5
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-10 13:07:33 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -4,6 +4,7 @@ import spotipy
 from info import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 import base64
 
+
 # Define your client id and client secret
 client_id = SPOTIFY_CLIENT_ID
 client_secret = SPOTIFY_CLIENT_SECRET
@@ -23,7 +24,6 @@ ydl_opts = {
     ],
 }
 
-sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(client_id, client_secret, redirect_uri=None))
 
 @Client.on_message(filters.command("spotify"))
 async def handle_spotify_command(client, message):
@@ -46,17 +46,20 @@ async def handle_spotify_command(client, message):
                 ydl.download([info["id"]])
                 client.send_audio(message.chat.id, f"{info['title']}.mp3")
         except Exception as e:
+            # Handle any errors during Spotify URL processing
             client.send_message(message.chat.id, f"Error extracting information from Spotify URL: {e}")
     else:
         # Handle normal query if no Spotify URL
         query = message.text.split(" ", 1)[1]
         try:
+            # Search for songs on Spotify
             results = sp.search(q=query, type="track", limit=5)
             if len(results["tracks"]["items"]) == 0:
+                # Inform user if no results found
                 client.send_message(message.chat.id, "No results found.")
                 return
 
-            # Send song information and thumbnails
+            # Send song information and thumbnails for each result
             for i, track in enumerate(results["tracks"]["items"]):
                 thumbnail = track["album"]["images"][0]["url"]
                 client.send_photo(message.chat.id, thumbnail)
@@ -66,15 +69,19 @@ async def handle_spotify_command(client, message):
             # Wait for user selection
             user_choice = client.listen(filters.text & filters.reply_to(message), timeout=60)
             if not user_choice:
+                # Inform user if no selection within timeout
                 client.send_message(message.chat.id, "Timed out. Please try again.")
                 return
 
+            # Get chosen track based on user selection
             chosen_track = results["tracks"]["items"][int(user_choice.text) - 1]
 
-            # Download and send song
+            # Download and send chosen song
             with YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(chosen_track["external_urls"]["spotify"], download=False)
                 ydl.download([info["id"]])
                 client.send_audio(message.chat.id, f"{info['title']}.mp3")
         except Exception as e:
+            # Handle any errors during search or download
             client.send_message(message.chat.id, f"Error: {e}")
+
