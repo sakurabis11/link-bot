1fbfb28b0863269b4402a00057b9ca7b1a67711b
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-12 09:01:11 +0530

    Create report.py

---
+++ plugins/report.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+
+@Client.on_message(filters.reply & filters.command("admins"))
+async def report_to_admins(client, message):
+    # Get the replied message
+    replied_message = message.reply_to_message
+
+    # Check if the replied message exists
+    if not replied_message:
+        await message.reply("Please reply to a message you want to report.")
+        return
+
+    # Get the chat members list
+    chat_members = await client.get_chat_members(message.chat.id)
+
+    # Filter admins and owners
+    admins = [member for member in chat_members if member.status in ("administrator", "creator")]
+
+    # Send the report to each admin
+    for admin in admins:
+        await client.send_message(admin.user.id, f"**New report from {message.from_user.mention} in group {message.chat.title}**:\n\n{replied_message.text}")
+
+    # Send confirmation message to the user
+    await message.reply("Your report has been sent to the admins.")
+
