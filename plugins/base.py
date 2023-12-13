954ae1a3d29245e32be29c6153806e09d840e67f
master-code-r <151423041+master-code-r@users.noreply.github.com>
2023-12-13 09:09:29 +0530

    Update base.py

--- plugins/base.py
+++ plugins/base.py
@@ -56,7 +56,7 @@ async def save_group(bot, message):
                     await (temp.MELCOW['welcome']).delete()
                 except:
                     pass
-            temp.MELCOW['welcome'] = await message.reply(text=script.WELCME_TXT.format(message.from_user.mention, today, time, message_count, message.chat.title))
+            temp.MELCOW['welcome'] = await message.reply(text=script.WELCME_TXT.format(message.from_user.mention, today, time, message.chat.title))
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
