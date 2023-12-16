210ed65bb56add72decef80353f056cfb8b7da61
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-16 18:52:24 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -8,38 +8,55 @@ from youtube_search import YoutubeSearch
 
 @Client.on_message(filters.command(["song"]))
 async def download_song(client, message):
-    # Check if the user has provided a song name
+    # Check for song name, video link, or playlist link
     if len(message.text.split()) < 2:
-        await message.reply("Please provide the song name you want to download")
+        await message.reply("Please provide a song name, YouTube video link, or playlist link.")
         return
 
-    song_name = message.text.split()[1:]  # Extract the song name from the message
-    song_name = " ".join(song_name)  # Combine the song name parts into a single string
+    # Extract the input
+    input_text = message.text.split()[1]
 
-    # Search for the song on YouTube
-    search_results = YoutubeSearch(song_name, max_results=1).to_dict()
-    song_url = search_results[0]["url_suffix"]
-    song_title = search_results[0]["title"]
+    # Check if it's a song name
+    if re.search(r"\s+", input_text):
+        # Search for the song on YouTube
+        search_results = YoutubeSearch(input_text, max_results=1).to_dict()
+        song_url = search_results[0]["url_suffix"]
+        song_title = search_results[0]["title"]
+        download_from_url(client, message, f"https://www.youtube.com{song_url}", song_title)
+    elif input_text.startswith("https://www.youtube.com/"):
+        # Assume it's a YouTube video link
+        download_from_url(client, message, input_text, None)
+    elif input_text.startswith("https://music.youtube.com/"):
+        # Assume it's a YouTube playlist link
+        download_playlist(client, message, input_text)
+    else:
+        await message.reply("Invalid input. Please provide a valid song name, video link, or playlist link.")
 
-    # Download the song using pytube
-    yt = YouTube(f"https://www.youtube.com{song_url}")
-    audio_streams = yt.streams.filter(only_audio=True)
 
-    if not audio_streams:
-        await client.send_message(message.chat.id, "No audio stream found for the specified video")
-        return
-
-    video = audio_streams.first()
-    audio_filename = f"{song_title}.mp3"
-
+def download_from_url(client, message, url, title=None):
+    # Download and send the song
     try:
-        video.download(filename=audio_filename)
-        await message.reply(f"**Song downloaded: {audio_filename}**")
-
-        # Send the downloaded song to the user
-        await message.reply_audio(audio_filename)
-
-        # Delete the downloaded song after sending it
-        os.remove(audio_filename)
+        yt = YouTube(url)
+        audio_streams = yt.streams.filter(only_audio=True)
+        if not audio_streams:
+            raise Exception("No audio stream found for the specified video.")
+        video = audio_streams.first()
+        filename = f"{title or yt.title}.mp3"
+        video.download(filename=filename)
+        await message.reply(f"**Song downloaded: {filename}**")
+        await message.reply_audio(filename)
+        os.remove(filename)
     except Exception as e:
         await message.reply(f"Error downloading song: {e}")
+
+
+def download_playlist(client, message, playlist_url):
+    # Download all songs in the playlist
+    try:
+        playlist = YouTube(playlist_url).playlist
+        for item in playlist.videos:
+            download_from_url(client, message, item.url, item.title)
+        await message.reply("Playlist downloaded successfully!")
+    except Exception as e:
+        await message.reply(f"Error downloading playlist: {e}")
+
