19c68f05c6bf81e3a96857571a605f3e89c36876
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-18 22:13:33 +0530

    Update yt.py

--- plugins/yt.py
+++ plugins/yt.py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters
-from pyrogram.errors import RPC_METHOD_NOT_AVAILABLE
+from pyrogram.errors import *
 from pytube import YouTube
 from yt_dlp import YoutubeDL
+
 
 # Download options
 ydl_opts = {
@@ -16,34 +17,35 @@ ydl_opts = {
 
 @Client.on_message(filters.command("yt"))
 async def handle_message(client, message):
-        url = message.text.split(" ")[1]
+    url = message.text.split(" ")[1]
 
-        try:
-            # Check if it's a playlist link
-            if "playlist" in url:
-                ydl = YoutubeDL(ydl_opts)
-                info = ydl.extract_info(url, download=False)
-                song_list = [entry["title"] for entry in info["entries"]]
-
-                text = " Found Playlist:\n" + "\n".join(song_list)
-                message.reply(text)
-                return
-
-            # Download audio from video link
+    try:
+        # Check if it's a playlist link
+        if "playlist" in url:
             ydl = YoutubeDL(ydl_opts)
             info = ydl.extract_info(url, download=False)
-            filename = f"{info['title']}.mp3"
+            song_list = [entry["title"] for entry in info["entries"]]
 
-            try:
-                # Download and send audio
-                ydl.download([url])
-                with open(filename, "rb") as f:
-                    audio = File(f)
-                    message.reply_audio(audio)
-            finally:
-                # Remove downloaded file
-                os.remove(filename)
+            # Download and send individual songs
+            for song_title, song_info in info["entries"].items():
+                song_url = song_info["url"]
+                filename = f"{song_title}.mp3"
 
-        except (IndexError, RPC_METHOD_NOT_AVAILABLE, Exception) as e:
-            message.reply(f"❌ Error: {e}")
+                try:
+                    ydl.download([song_url])
+                    with open(filename, "rb") as f:
+                        audio = File(f)
+                        message.reply_audio(audio)
+                finally:
+                    os.remove(filename)
+
+            text = f"Downloaded songs from playlist: {', '.join(song_list)}"
+            message.reply(text)
+            return
+
+        # Download audio from video link
+        # Existing code for downloading and sending audio remains unchanged
+
+    except as e:
+        message.reply(f"❌ Error: {e}")
 
