4db12584ae82839589f34c39504ca2bd401e0aed
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-19 20:09:44 +0530

    Create insta.py

---
+++ plugins/insta.py
@@ -0,0 +1,10 @@
+import pyrogram 
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("insta"))
+async def insta(client, message):
+    if len(message.command) < 2:
+        await message.reply("Please provide the Instagram reel ID.")
+    else:
+        query = " ".join(message.command[1:])
+        await message.reply(f"https://www.ddinstagram.com/reel/{query}")
