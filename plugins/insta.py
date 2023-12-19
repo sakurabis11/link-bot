9bb77454e8704de7e6fafa422313e0fd896fcc82
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-19 23:58:06 +0530

    Update insta.py

--- plugins/insta.py
+++ plugins/insta.py
@@ -1,13 +1,39 @@
-import pyrogram 
+import os
 from pyrogram import Client, filters
+from instaloader import Instaloader
+
+L = Instaloader()
+
+# Download and send reel function
+def download_and_send_reel(client, chat_id, url):
+    try:
+        # Download reel with Instaloader
+        post = L.context.get_media_by_url(url)
+        video_path = post.download_video()
+        caption = post.caption
+
+        # Open video file in binary mode
+        with open(video_path, "rb") as f:
+            video_data = f.read()
+
+        # Send video as document
+        client.send_document(chat_id, video_data, document_name="reel.mp4")
+
+        # Send caption (optional)
+        if caption:
+            client.send_message(chat_id, caption)
+
+        # Delete downloaded file
+        os.remove(video_path)
+    except Exception as e:
+        client.send_message(chat_id, f"Error downloading reel: {e}")
 
 @Client.on_message(filters.command("insta"))
-async def insta(client, message):
-    if len(message.command) < 2:
-        await message.reply("Please provide the Instagram reel ID.")
+async def reel_handler(client, message):
+    url = message.text.strip()
+    if url.startswith("https://www.instagram.com/reel/"):
+        download_and_send_reel(client, message.chat_id, url)
     else:
-        query = " ".join(message.command[1:])
+        client.send_message(message.chat_id, "Invalid URL. Please send a valid Instagram reel URL.")
 
-        reels = f"<a href='https://www.ddinstagram.com/reel/{query}'>https://www.instagram.com/reel/{query}</a>"
 
-        await message.reply(reels)
