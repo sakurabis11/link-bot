263d630d3f4e70c5cac9a0020c25b903e44ca8b4
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-19 19:16:10 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -8,52 +8,53 @@ from youtube_search import YoutubeSearch
 
 @Client.on_message(filters.command(["song"]))
 async def download_song(client, message):
-    # Check if user provided song name
+    # Check if the user has provided a song name
     if len(message.text.split()) < 2:
-        await message.reply("Please provide song name you want to download")
+        await message.reply("Please provide the song name you want to download")
         return
 
-    song_name = message.text.split()[1:]  # Extract song name
-    song_name = " ".join(song_name)  # Combine parts
+    song_name = message.text.split()[1:]  # Extract the song name parts into a list
+    song_name = " ".join(song_name)  # Combine the song name parts into a single string
 
-    # Search for song on YouTube
+    # Send "Searching..." message before searching
+    await message.reply("Searching for your song...")
+
+    # Search for the song on YouTube
     search_results = YoutubeSearch(song_name, max_results=1).to_dict()
+    if not search_results:
+        await message.reply("No song found with that name")
+        return
+
     song_url = search_results[0]["url_suffix"]
     song_title = search_results[0]["title"]
+    duration = search_results[0]["duration"]
 
-    # Send hourglass emoji before searching
-    await message.reply(f"⏳ Searching for your song... Please wait!")
-
-    # Download song using pytube
+    # Download the song using pytube
     yt = YouTube(f"https://www.youtube.com{song_url}")
     audio_streams = yt.streams.filter(only_audio=True)
 
     if not audio_streams:
-        await client.send_message(message.chat.id, "No audio stream found for the specified video")
+        await message.reply("No audio stream found for the specified video")
         return
 
     video = audio_streams.first()
     audio_filename = f"{song_title}.mp3"
 
     try:
-        # Download song
         video.download(filename=audio_filename)
 
-        # Wait for 6 seconds before removing emoji
-        await asyncio.sleep(6)
-        await message.edit_text(None)
+        # Prepare message caption with title, duration, and YouTube link
+        caption = f"**{song_title}**\n\n" + \
+                    f"Duration: {duration}\n" + \
+                    f"YouTube Link: https://www.youtube.com{song_url}"
 
-        # New message for song details - avoids editing issue
-        new_message = client.send_message(
-            message.chat.id,
-            f"**Downloaded: {song_title}**\n⏱️ Duration: {int(video.duration / 60)}:{int(video.duration % 60)} minutes\n YouTube Link: https://www.youtube.com{song_url}",
-        )
+        # Send downloaded song with caption
+        await message.reply_audio(audio_filename, caption=caption)
 
-        # Send downloaded song with new message
-        await new_message.reply_audio(audio_filename)
-
-        # Delete downloaded song afterwards
+        # Delete the downloaded song after sending it
         os.remove(audio_filename)
+
     except Exception as e:
         await message.reply(f"Error downloading song: {e}")
+
 
