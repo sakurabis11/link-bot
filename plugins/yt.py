5c563b1ab9beb6e723e2044a779e5c677fdd9237
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-19 07:53:38 +0530

    Update yt.py

--- plugins/yt.py
+++ plugins/yt.py
@@ -5,9 +5,13 @@ from yt_dlp import YoutubeDL
 @Client.on_message(filters.command("yt"))
 async def youtube_to_audio(client, message):
     # Extract YouTube link from message
-    video_url = message.text.split()[1]
+    try:
+        video_url = message.text.split()[1]
+    except IndexError:
+        await message.reply("Please provide a YouTube link! (e.g., /yt https://www.youtube.com/watch?v=...)")
+        return
 
-    # Download audio using yt_dlp with preferred codec and quality
+    # Download audio using yt-dlp with preferred codec and quality
     ydl_opts = {
         "format": "bestaudio/best",
         "postprocessors": [{
@@ -16,28 +20,31 @@ async def youtube_to_audio(client, message):
             "preferredquality": "192",
         }],
     }
-    with YoutubeDL(ydl_opts) as ydl:
-        # Extract info and prepare filename
-        info = ydl.extract_info(video_url, download=False)
-        audio_file = ydl.prepare_filename(info)
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            # Extract info and prepare filename
+            info = ydl.extract_info(video_url, download=False)
+            audio_file = ydl.prepare_filename(info)
 
-        # Download audio data directly
-        with open(audio_file, "wb") as file:
-            ydl.process_video(video_url, download=False, callback=lambda data: file.write(data))
+            # Download audio data directly
+            with open(audio_file, "wb") as file:
+                ydl.process_video(video_url, download=False, callback=lambda data: file.write(data))
 
-    # Extract artist and thumbnail (optional)
-    artist = info.get("artist")
-    thumbnail_url = info["thumbnails"][0]["url"] if info["thumbnails"] else None
+            # Extract artist and thumbnail (optional)
+            artist = info.get("artist")
+            thumbnail_url = info["thumbnails"][0]["url"] if info["thumbnails"] else None
 
-    # Send downloaded audio directly with info
-    await message.reply_audio(
-        audio_file=audio_file,
-        title=info["title"],
-        duration=info["duration"],
-        performer=artist,
-        thumb=thumbnail_url,
-    )
+            # Send downloaded audio directly with info
+            await message.reply_audio(
+                audio_file=audio_file,
+                title=info["title"],
+                duration=info["duration"],
+                performer=artist,
+                thumb=thumbnail_url,
+            )
 
-    # Delete temporary audio file
-    os.remove(audio_file)
+            # Delete temporary audio file
+            os.remove(audio_file)
+    except Exception as e:
+        await message.reply(f"Error downloading audio: {e}")
 
