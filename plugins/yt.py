8f9800c70284382cf1f13f3373fb984fffcc45fd
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-19 13:15:24 +0530

    Update yt.py

--- plugins/yt.py
+++ plugins/yt.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pytube import YouTube
 
 # Audio qualities and corresponding bitrates
