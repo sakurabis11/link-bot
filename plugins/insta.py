00743355ab655bb5f50fef403a17fc51860d5846
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-19 22:21:12 +0530

    Update insta.py

--- plugins/insta.py
+++ plugins/insta.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 async def handle_insta_command(client, message):
     # Check if there is text after the command
     if len(message.text) == 5:
-        message.reply_text("Please provide an Instagram Reels link after the /insta command.")
+        await message.reply_text("Please provide an Instagram Reels link after the /insta command.")
         return
 
     # Extract the text after the command
@@ -12,7 +12,7 @@ async def handle_insta_command(client, message):
 
     # Check if the link starts with "https://www.instagram.com/reel/"
     if not insta_link.startswith("https://www.instagram.com/reel/"):
-        message.reply_text("Invalid Instagram Reels link. Please provide a proper link.")
+        await message.reply_text("Invalid Instagram Reels link. Please provide a proper link.")
         return
 
     # Extract the Reels ID after "reel/"
