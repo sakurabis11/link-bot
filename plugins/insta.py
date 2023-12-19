480d63b254685d25b94d361f77baa626f6f1284e
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-19 22:04:35 +0530

    Update insta.py

--- plugins/insta.py
+++ plugins/insta.py
@@ -1,13 +1,22 @@
-import pyrogram 
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command("insta"))
-async def insta(client, message):
-    if len(message.command) < 2:
-        await message.reply("Please provide the Instagram reel ID.")
-    else:
-        query = " ".join(message.command[1:])
+async def handle_insta_command(client, message):
+    # Check if there is text after the command
+    if len(message.text) == 5:
+        message.reply_text("Please provide an Instagram Reels link after the /insta command.")
+        return
 
-        reels = f"https://www.ddinstagram.com/reel/{query}"
-            
-        await message.reply(reels)
+    # Extract the text after the command
+    insta_link = message.text[5:]
+
+    # Check if the link starts with "https://www.instagram.com/reel/"
+    if not insta_link.startswith("https://www.instagram.com/reel/"):
+        message.reply_text("Invalid Instagram Reels link. Please provide a proper link.")
+        return
+
+    # Extract the Reels ID after "reel/"
+    reels_id = insta_link[len("https://www.instagram.com/reel/"):]
+
+    # Reply with the extracted Reels ID
+    message.reply_text(f"https://www.ddinstagram.com/reel/{reels_id}")
