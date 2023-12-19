712fb9efcdb7a3705a6914a11eb98d5ed8e98717
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-19 16:13:56 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -4,29 +4,30 @@ import re
 from pyrogram import Client, filters
 from pytube import YouTube
 from youtube_search import YoutubeSearch
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 @Client.on_message(filters.command(["song"]))
 async def download_song(client, message):
-    # Check if the user has provided a song name
+    # Check if song name provided
     if len(message.text.split()) < 2:
         await message.reply("Please provide the song name you want to download")
         return
 
-    song_name = message.text.split()[1:]  # Extract the song name from the message
-    song_name = " ".join(song_name)  # Combine the song name parts into a single string
+    song_name = message.text.split()[1:]
+    song_name = " ".join(song_name)
 
-    # Search for the song on YouTube
+    # Search and download with processing emoji
+    await message.reply("⏳ Searching...")
+    await asyncio.sleep(1)
     search_results = YoutubeSearch(song_name, max_results=1).to_dict()
     song_url = search_results[0]["url_suffix"]
     song_title = search_results[0]["title"]
 
-    # Download the song using pytube
     yt = YouTube(f"https://www.youtube.com{song_url}")
     audio_streams = yt.streams.filter(only_audio=True)
 
     if not audio_streams:
-        await client.send_message(message.chat.id, "No audio stream found for the specified video")
+        await message.reply("No audio stream found for the specified video")
         return
 
     video = audio_streams.first()
@@ -34,12 +35,24 @@ async def download_song(client, message):
 
     try:
         video.download(filename=audio_filename)
-        await message.reply(f"**Song downloaded: {audio_filename}**")
+        duration = youtube_dl.utils.time_format(video.duration)
+        await message.reply_audio(
+            audio_filename,
+            caption=f"**{song_title} - {duration}**\n[Watch on YouTube](https://www.youtube.com{song_url})",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Send me personally", callback_data=f"send_{audio_filename}")]])
+        )
 
-        # Send the downloaded song to the user
-        await message.reply_audio(audio_filename)
-
-        # Delete the downloaded song after sending it
+        # Delete downloaded file after sending
         os.remove(audio_filename)
     except Exception as e:
         await message.reply(f"Error downloading song: {e}")
+
+@Client.on_callback_query()
+async def send_personal_song(client, callback_query):
+    callback_data = callback_query.data
+    if callback_data.startswith("send_"):
+        audio_filename = callback_data[5:]
+        await client.send_audio(callback_query.message.sender.id, audio_filename)
+        os.remove(audio_filename)
+        await callback_query.answer()
+
