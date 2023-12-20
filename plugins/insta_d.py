502108853634098fa82392a25e8ca4bd43326901
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-20 15:25:13 +0530

    Update insta_d.py

--- plugins/insta_d.py
+++ plugins/insta_d.py
@@ -3,11 +3,12 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("reel"))
 async def extract_reel_link(client, message):
-    link = message.text.split(" ")[1]  # Extract the link from the command
+    link = message.text.split(" ")[1]
 
     if "https://www.instagram.com/reel/" in link:
-        reel_id = link.replace("https://www.instagram.com/reel/", "").split("reel/")[0]  # Extract the reel ID
-        await message.reply_text(f"Reel ID: {reel_id}")  # Send the extracted reel ID to the user
+        reel_id = link.replace("https://www.instagram.com/reel/", "").split("reel/")[0]
+        reels = f"<a href='https://www.ddinstagram.com/reel/{reel_id}'>https://www.instagram.com/reel/{reel_id}</a>"
+        await message.reply_text(f"{reels}")
     else:
         await message.reply_text("Invalid link format. Please provide a valid Instagram reel link.")
 
