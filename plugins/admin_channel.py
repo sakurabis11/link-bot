ca15af5d1e69298e31ade3012a53e7ac90aeb0fa
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-20 19:58:01 +0530

    Update admin_channel.py

--- plugins/admin_channel.py
+++ plugins/admin_channel.py
@@ -1,22 +1,16 @@
 import pyrogram
 from pyrogram import Client, filters
-import os
+from info import ADMIN_CHANNEL_ID
 
-admin_channel_id = -1002079640571
+@Client.on_message(filters.private & filters.chat)
+async def forward_private_messages(client, message):
+    # Construct the forwarded message with tag and user mention
+    forwarded_message = f"#ForwardedMessage\n{message.from_user.mention}\n{message.text}"
 
-@Client.on_message(filters.chat(~admin_channel_id) & (filters.text | filters.command | filters.forwarded))
-async def forward_message_with_tag(client, message):
-    if message.forward_from:
-        # Forwarded message with tag
-        await client.send_message(
-            admin_channel_id,
-            f"#forwarded\n{message.text}\n\nFrom: {message.forward_from.mention}",
-            disable_web_page_preview=True  # Prevent potential privacy leaks
-        )
-    else:
-        # Text message or command
-        await client.send_message(
-            admin_channel_id,
-            f"#message\n{message.text}\n\nFrom: {message.from_user.mention}",
-            disable_web_page_preview=True
-        )
+    # Forward the message to the admin channel, keeping original formatting
+    await client.forward_messages(
+        ADMIN_CHANNEL_ID,
+        message.chat.id,
+        message_ids=message.message_id,
+        as_copy=True  # Preserve original formatting
+    )
