99094f357286acc9921e401733f40b9239bd48e1
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-20 21:22:59 +0530

    Update insta_r.py

--- plugins/insta_r.py
+++ plugins/insta_r.py
@@ -9,20 +9,21 @@ insta_loader = instaloader.Instaloader()
 
 @Client.on_message(filters.regex(r"https://www\.instagram\.com/reel/(.*)"))
 async def extract_reel_link(client, message):
-    link = message.text
+  link = message.text
 
-    if "https://www.instagram.com/reel/" in link:
-        reel_id = link.replace("https://www.instagram.com/reel/", "").split("/")[0]
+  if "https://www.instagram.com/reel/" in link:
+    reel_id = link.replace("https://www.instagram.com/reel/", "").split("/")[0]
 
-        # Download the reel video
-        try:
-            video_filename = f"reel_{reel_id}.mp4"
-            insta_loader.download_reels(reel_id, target=video_filename)
+    # Attempt to download the reel video using the appropriate method
+    try:
+      video_filename = f"reel_{reel_id}.mp4"
 
-            # Send the video to the user
-            await message.reply_video(video=video_filename, supports_streaming=True)
-        except Exception as e:
-            await message.reply_text(f"Error downloading the reel: {str(e)}")
-    else:
-        await message.reply_text("Invalid link format. Please provide a valid Instagram reel link.")
+      # Replace 'download_reels' with the correct method based on your Instaloader version
+      # Refer to the Instaloader documentation for guidance
+      insta_loader.download_post(reel_id, target=video_filename)  # Example assuming the correct method is 'download_post'
 
+      await message.reply_video(video=video_filename, supports_streaming=True)
+    except Exception as e:
+      await message.reply_text(f"Error downloading the reel: {str(e)}")
+  else:
+    await message.reply_text("Invalid link format. Please provide a valid Instagram reel link.")
