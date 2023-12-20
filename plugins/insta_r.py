8e462c5976cf28602b40fcdeef89ef845c6b4760
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-20 21:45:48 +0530

    Update insta_r.py

--- plugins/insta_r.py
+++ plugins/insta_r.py
@@ -1,14 +1,70 @@
-import pyrogram
-import re
-from pyrogram import Client, filters
-
 @Client.on_message(filters.regex(r"https://www\.instagram\.com/reel/(.*)"))
-async def extract_reel_link(client, message):
-    link = message.text
 
-    if "https://www.instagram.com/reel/" in link:
-        reel_id = link.replace("https://www.instagram.com/reel/", "").split("reel/")[0]
-        reels = f"<a href='https://www.ddinstagram.com/reel/{reel_id}'>https://www.instagram.com/reel/{reel_id}</a>"
-        await message.reply_text(f"{reels}")
+
+import pyrogram
+from pyrogram import Client, Filters
+import requests
+
+app = Client("your_app_name")  # Replace with your app name
+
+@Client.on_message(filters.regex(r"https://www\.instagram\.com/(.*)"))
+async def send_post(client, message):
+    url = message.text
+
+    if "?" in url:
+        url += "&__a=1"
     else:
-        await message.reply_text("Invalid link format. Please provide a valid Instagram reel link.")
+        url += "?__a=1"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for non-200 status codes
+
+        data = response.json()
+        is_video = data["graphql"]["shortcode_media"]["is_video"]
+
+        try:
+            posts = data["graphql"]["shortcode_media"]["edge_sidecar_to_children"]["edges"]
+
+            for post in posts:
+                is_video = post["node"]["is_video"]
+
+                if is_video:
+                    video_url = post["node"]["video_url"]
+                    await message.reply_chat_action("upload_video")
+                    await message.reply_video(video_url)
+                else:
+                    post_url = post["node"]["display_url"]
+                    await message.reply_chat_action("upload_photo")
+                    await message.reply_photo(post_url)
+
+        except KeyError:
+            if is_video:
+                video_url = data["graphql"]["shortcode_media"]["video_url"]
+                await message.reply_chat_action("upload_video")
+                await message.reply_video(video_url)
+            else:
+                post_url = data["graphql"]["shortcode_media"]["display_url"]
+                await message.reply_chat_action("upload_photo")
+                await message.reply_photo(post_url)
+
+    except requests.exceptions.RequestException as e:
+        await message.reply_text("Send Me Only Public Instagram Posts")
+
+@Client.on_message(filters.command("dp"))
+async def send_dp(client, message):
+    username = message.text.split(" ")[1]
+    url = f"https://instagram.com/{username}/?__a=1"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for non-200 status codes
+
+        data = response.json()
+        user_profile = data["graphql"]["user"]["profile_pic_url_hd"]
+        await message.reply_chat_action("upload_photo")
+        await message.reply_photo(user_profile)
+    except (requests.exceptions.RequestException, KeyError) as e:
+        await message.reply_text("Send Me Only Existing Instagram Username")
+
+
