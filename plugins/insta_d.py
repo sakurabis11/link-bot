e063489fe3b47212ec0de46487ba3ae415ff3778
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-20 15:15:09 +0530

    Update insta_d.py

--- plugins/insta_d.py
+++ plugins/insta_d.py
@@ -6,7 +6,7 @@ async def extract_reel_link(client, message):
     link = message.text.split(" ")[1]  # Extract the link from the command
 
     if "https://www.instagram.com/reel/" in link:
-        reel_id = link.replace("https://www.instagram.com/reel/", "").split("reel/")[2]  # Extract the reel ID
+        reel_id = link.replace("https://www.instagram.com/reel/", "").split("reel/")[0]  # Extract the reel ID
         await message.reply_text(f"Reel ID: {reel_id}")  # Send the extracted reel ID to the user
     else:
         await message.reply_text("Invalid link format. Please provide a valid Instagram reel link.")
