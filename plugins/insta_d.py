3add9b0fa21dbd786b754aa9d107f469003a1e7f
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-20 15:11:00 +0530

    Update insta_d.py

--- plugins/insta_d.py
+++ plugins/insta_d.py
@@ -1,11 +1,13 @@
+import pyrogram
 from pyrogram import Client, filters
-from instagrapi import Client as InstaClient
 
-insta = InstaClient()
+@Client.on_message(filters.command("reel"))
+async def extract_reel_link(client, message):
+    link = message.text.split(" ")[1]  # Extract the link from the command
 
-@Client.on_message(filters.command("download"))
-async def download(client, message):
-    url = message.text.split(" ", 1)[1]
-    media = insta.media_download(url)
-    message.reply_document(media)
+    if "https://www.instagram.com/reel/" in link:
+        reel_id = link.replace("https://www.instagram.com/reel/", "").split("reel/")[2]  # Extract the reel ID
+        await message.reply_text(f"Reel ID: {reel_id}")  # Send the extracted reel ID to the user
+    else:
+        await message.reply_text("Invalid link format. Please provide a valid Instagram reel link.")
 
