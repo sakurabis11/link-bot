da56ddb3265037f717c748e6577a86215a28c248
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-22 22:47:51 +0530

    Update mrtg.py

--- plugins/mrtg.py
+++ plugins/mrtg.py
@@ -1,9 +1,6 @@
 import pyrogram
 from pyrogram import filters, Client
-import aiohttp 
-import re
-import requests
-from bs4 import BeautifulSoup
+import aiohttp
 import os
 
 @Client.on_message(filters.regex(r"https://open.spotify.com/track/(.*)"))
@@ -15,20 +12,23 @@ async def download_and_send(client, message):
         song_file = await download_track(track_url)
         try:
             await client.send_audio(user_id, audio=song_file, caption="Downloaded from Spotify")
-            os.remove(song_file)
+            os.remove(song_file)  # Optional: Delete the file after sending
         except Exception as e:
             await client.send_message(user_id, "Failed to send audio: {}".format(e))
     except Exception as e:
         await client.send_message(user_id, "Failed to download track: {}".format(e))
 
 async def download_track(track_url):
-    async with aiohttp.ClientSession() as session: 
-        response = await session.get(track_url) 
-        soup = BeautifulSoup(await response.text(), "html.parser")  
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(track_url)
+        soup = BeautifulSoup(await response.text(), "html.parser")
+
+        # Extract download link from HTML (replace with your specific logic)
+        download_link = soup.find("a", class_="track-download-link").get("href")  # Example placeholder
 
         download_response = await session.get(download_link)
         song_file = open("downloaded_song.mp3", "wb")
         song_file.write(await download_response.read())
         song_file.close()
 
-    return song_file
+        return song_file
