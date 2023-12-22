f6346e6579222c9853e165e77e3bb2bade9d7a91
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-22 17:39:00 +0530

    Update mrtg.py

--- plugins/mrtg.py
+++ plugins/mrtg.py
@@ -1,17 +1,37 @@
 import pyrogram
-from pyrogram import Client, filters
+from pyrogram import filters, Client
+import requests
+from bs4 import BeautifulSoup
+import os
 
-# Function to download and send song
-async def download_song(link, chat_id):
+@Client.on_message(filters.regex(r"https://open.spotify.com/track/(.*)"))
+async def download_and_send(client, message):
+    track_url = message.text
+    user_id = message.from_user.id
+
     try:
-        song = spotifydownload.download_song(link)
-        with open(song.file_path, "rb") as f:
-            await client.send_audio(chat_id, f, title=song.title, performer=song.artist)
+        song_file = await download_track(track_url)
+        try:
+            await client.send_audio(user_id, audio=song_file, caption="Downloaded from Spotify")
+            # Delete the file after sending (optional)
+            os.remove(song_file)
+        except Exception as e:
+            await client.send_message(user_id, "Failed to send audio: {}".format(e))
     except Exception as e:
-        await client.send_message(chat_id, f"Error downloading song: {e}")
+        await client.send_message(user_id, "Failed to download track: {}".format(e))
 
-# Handle Spotify links
-@Client.on_message(filters.regex(r"https://open.spotify.com/(track|album|playlist)/(\w+)"))
-async def handle_spotify_link(client, message):
-    await download_song(message.text, message.chat.id)
+async def download_track(track_url):
+    # Replace with your preferred Spotify download method
+    # Ensure it complies with Spotify's Terms of Service
+
+    response = await requests.get(track_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    # ... (Extract download link from the HTML using appropriate methods)
+
+    download_response = requests.get(download_link)
+    song_file = open("downloaded_song.mp3", "wb")
+    song_file.write(download_response.content)
+    song_file.close()
+
+    return song_file
 
