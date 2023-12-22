1b967f94c1ff11c19f814f1a43599d710a498993
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-22 22:31:00 +0530

    Update mrtg.py

--- plugins/mrtg.py
+++ plugins/mrtg.py
@@ -1,6 +1,8 @@
 import pyrogram
 from pyrogram import filters, Client
-import aiohttp  # Import aiohttp for asynchronous requests
+import aiohttp 
+import re
+import request
 from bs4 import BeautifulSoup
 import os
 
@@ -13,7 +15,6 @@ async def download_and_send(client, message):
         song_file = await download_track(track_url)
         try:
             await client.send_audio(user_id, audio=song_file, caption="Downloaded from Spotify")
-            # Delete the file after sending (optional)
             os.remove(song_file)
         except Exception as e:
             await client.send_message(user_id, "Failed to send audio: {}".format(e))
@@ -21,18 +22,13 @@ async def download_and_send(client, message):
         await client.send_message(user_id, "Failed to download track: {}".format(e))
 
 async def download_track(track_url):
-    # Replace with your preferred Spotify download method
-    # Ensure it complies with Spotify's Terms of Service
+    async with aiohttp.ClientSession() as session: 
+        response = await session.get(track_url) 
+        soup = BeautifulSoup(await response.text(), "html.parser")  
 
-    async with aiohttp.ClientSession() as session:  # Create an asynchronous session
-        response = await session.get(track_url)  # Await the asynchronous response
-        soup = BeautifulSoup(await response.text(), "html.parser")  # Parse the HTML
-
-        # ... (Extract download link from the HTML using appropriate methods)
-
-        download_response = await session.get(download_link)  # Await the download response
+        download_response = await session.get(download_link)
         song_file = open("downloaded_song.mp3", "wb")
-        song_file.write(await download_response.read())  # Write the content to the file
+        song_file.write(await download_response.read())
         song_file.close()
 
     return song_file
