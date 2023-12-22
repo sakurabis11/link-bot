3694d3b6f81ef5d43a01999f4ee03caa0469c7fa
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-22 16:53:47 +0530

    Create mrtg.py

---
+++ plugins/mrtg.py
@@ -0,0 +1,18 @@
+import pyrogram
+from pyrogram import Client, Filters
+import spotifydownload
+
+# Function to download and send song
+async def download_song(link, chat_id):
+    try:
+        song = spotifydownload.download_song(link)
+        with open(song.file_path, "rb") as f:
+            await client.send_audio(chat_id, f, title=song.title, performer=song.artist)
+    except Exception as e:
+        await client.send_message(chat_id, f"Error downloading song: {e}")
+
+# Handle Spotify links
+@Client.on_message(Filters.regex(r"https://open.spotify.com/(track|album|playlist)/(\w+)"))
+async def handle_spotify_link(client, message):
+    await download_song(message.text, message.chat.id)
+
