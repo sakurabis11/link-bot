cd88cfab52b2de37b3d3e698da0612d2657a760f
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-23 18:44:54 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,27 +1,32 @@
-import os
 from pyrogram import Client, filters
 import yt_dlp
 
-@Client.on_message(filters.regex(r"https?://(?:www\.)?youtu.be\?v=(\w+)"))
+@Client.on_message(filters.command("yt"))
 async def download_video(client, message):
-   link = message.text
+    try:
+        url = message.text.split(" ", 1)[1]  
 
-   try:
-       # Download the video using yt-dlp
-       ydl_opts = {
-           "outtmpl": "video.mp4",  # Output filename
-           "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"  # Preferred format
-       }
-       with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-           info_dict = ydl.extract_info(link, download=True)
+        ydl_opts = {
+            'outtmpl': '%(title)s.%(ext)s',  
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',  # Choose best format
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4'
+            }], 
+        }
 
-       # Send the downloaded video as a video message
-       with open("video.mp4", "rb") as video_file:
-           await client.send_video(message.chat.id, video_file)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            video_title = info_dict.get('title', None)
 
-       # Remove the downloaded file
-       os.remove("video.mp4")
+            await message.reply_text(f"Downloading {video_title}...")
 
-       await message.reply_text("Video sent!")
-   except Exception as e:
-       await message.reply_text(f"Error downloading video: {e}")
+            ydl.download([url])
+
+            await message.reply_video(video_file=f"{video_title}.mp4")  # Send the downloaded video
+
+            await message.reply_text("Video downloaded and sent!")
+
+    except Exception as e:
+        await message.reply_text(f"Error: {e}")
+
