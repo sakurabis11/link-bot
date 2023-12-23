93ebbe0c5ce0ec7f6ae0b1bc194ae63e4027075a
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-23 21:54:16 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -3,29 +3,30 @@ import yt_dlp
 
 @Client.on_message(filters.command("yt"))
 async def download_video(client, message):
- try:
-  url = message.text.split(" ", 1)[1]
+ try:
+   url = message.text.split(" ", 1)[1]
 
-  ydl_opts = {
-   'outtmpl': '%(title)s.%(ext)s',
-   'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]', 
-   'postprocessors': [{
-    'key': 'FFmpegVideoConvertor',
-    'preferedformat': 'mp4'
-   }],
-  }
+   ydl_opts = {
+     'outtmpl': '%(title)s.%(ext)s',
+     'format': 'bestvideo[height<=?720][ext=mp4]+bestaudio[ext=m4a]/best[height<=?720][ext=mp4]/best',  # Prioritize 720p
+     'postprocessors': [{
+       'key': 'FFmpegVideoConvertor',
+       'preferedformat': 'mp4'
+     }],
+   }
 
-  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-   info_dict = ydl.extract_info(url, download=False)
-   video_title = info_dict.get('title', None)
+   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+     info_dict = ydl.extract_info(url, download=False)
+     video_title = info_dict.get('title', None)
 
-   await message.reply_text(f"Downloading {video_title}...")
+     await message.reply_text(f"Downloading {video_title}...")
 
-   ydl.download([url])
+     ydl.download([url])
 
-   await message.reply_video(video=f"{video_title}.mp4", caption=video_title) # Send with caption
+     # Send the downloaded video with the caption:
+     await message.reply_video(video=f"{video_title}.mp4", caption=video_title)  # Added caption
 
-   await message.reply_text("Video downloaded and sent!")
+     await message.reply_text("Video downloaded and sent with caption!")
 
- except Exception as e:
-  await message.reply_text(f"Error: {e}")
+ except Exception as e:
+   await message.reply_text(f"Error: {e}")
