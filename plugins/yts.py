032404019e8b8b1b2a3db1b22872a1e8b5ce3b87
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-23 23:27:36 +0530

    Create yts.py

---
+++ plugins/yts.py
@@ -0,0 +1,30 @@
+import pyrogram
+from pyrogram import Client, filters
+import yt_dlp
+
+@Client.on_message(filters.command("yts"))
+async def yts(client, message):
+    url = message.text.split(" ", 1)[1]  # Extract URL from message
+
+    try:
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }],
+            "outtmpl": "%(title)s.%(ext)s",
+            "noplaylist": True,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            title = info_dict.get("title", "Unknown Title")
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.download([url])
+
+        await message.reply_audio(audio_file, caption=f"Downloaded song: {title}")
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
+
