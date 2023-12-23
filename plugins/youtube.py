02969430946cd89fbd1f073f4f272575be32c0df
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-23 22:34:37 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -3,8 +3,14 @@ import yt_dlp
 
 @Client.on_message(filters.command("yt"))
 async def download_video(client, message):
-    try:
-        url = message.text.split(" ", 1)[1]
+ try:
+   command_parts = message.text.split(" ", 1) 
+
+   if len(command_parts) == 1:
+     await message.reply_text("Please provide a YouTube video URL after the command.")
+     return
+
+   url = command_parts[1]
 
         ydl_opts = {
             'outtmpl': '%(title)s.%(ext)s',
@@ -19,17 +25,18 @@ async def download_video(client, message):
             info_dict = ydl.extract_info(url, download=False)
             video_title = info_dict.get('title', None)
 
-            if video_title:               
+            if video_title:  # Check if title is available
+                # Send the "Downloading..." message with scheduled deletion:
                 downloading_message = await message.reply_text(f"Downloading {video_title}...")
                 try:
-                    await downloading_message.delete(delay=10)  
+                    await downloading_message.delete(delay=10)  # Schedule deletion after 10 seconds
                 except Exception as e:
-                    print(f"Failed to delete message: {e}")  
+                    print(f"Failed to delete message: {e}")  # Log any deletion errors
 
                 ydl.download([url])
 
-               
-                await message.reply_video(video=f"{video_title}.mp4", caption=video_title)
+                # Send the downloaded video with the caption:
+                await message.reply_video(video=f"{video_title}.mp4", caption=video_title)  # Added caption
 
                 await message.reply_text("Video downloaded and sent with caption!")
             else:
