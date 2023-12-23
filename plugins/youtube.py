946d47e345b749e532e66ed88978957d816c2404
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-23 18:27:41 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,29 +1,27 @@
 import os
 from pyrogram import Client, filters
-import re
-import requests
 import yt_dlp
 
-@Client_on.message(filters.regex(r"https?://(?:www\.)?youtube\.com/watch\?v=(\w+)"))
+@Client.on_message(filters.regex(r"https?://(?:www\.)?youtu.be\?v=(\w+)"))
 async def download_video(client, message):
-    link = message.text
+   link = message.text
 
-    try:
-        # Download the video using yt-dlp
-        ydl_opts = {
-            "outtmpl": "video.mp4",  # Output filename
-            "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"  # Preferred format
-        }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(link, download=True)
+   try:
+       # Download the video using yt-dlp
+       ydl_opts = {
+           "outtmpl": "video.mp4",  # Output filename
+           "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"  # Preferred format
+       }
+       with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+           info_dict = ydl.extract_info(link, download=True)
 
-        # Send the downloaded video as a video message
-        with open("video.mp4", "rb") as video_file:
-            await client.send_video(message.chat.id, video_file)
+       # Send the downloaded video as a video message
+       with open("video.mp4", "rb") as video_file:
+           await client.send_video(message.chat.id, video_file)
 
-        # Remove the downloaded file
-        os.remove("video.mp4")
+       # Remove the downloaded file
+       os.remove("video.mp4")
 
-        await message.reply_text("Video sent!")
-    except Exception as e:
-        await message.reply_text(f"Error downloading video: {e}")
+       await message.reply_text("Video sent!")
+   except Exception as e:
+       await message.reply_text(f"Error downloading video: {e}")
