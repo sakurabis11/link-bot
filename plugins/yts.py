c371a4b7ab8b95457b32ceaef1d8dd9b836edb42
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-24 18:54:47 +0530

    Update yts.py

--- plugins/yts.py
+++ plugins/yts.py
@@ -4,27 +4,30 @@ import yt_dlp
 
 @Client.on_message(filters.command("yts"))
 async def yts(client, message):
-    url = message.text.split(" ", 1)[1]  # Extract URL from message
+ url = message.text.split(" ", 1)[1]  # Extract URL from message
 
-    try:
-        ydl_opts = {
-            "format": "bestaudio/best",
-            "postprocessors": [{
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }],
-            "outtmpl": "%(title)s.%(ext)s",
-            "noplaylist": True,
-        }
+ try:
+   ydl_opts = {
+     "format": "bestaudio/best",
+     "postprocessors": [
+       {
+         "key": "FFmpegExtractAudio",
+         "preferredcodec": "mp3",
+         "preferredquality": "192",
+       }
+     ],
+     "outtmpl": "%(title)s.%(ext)s",
+     "noplaylist": True,
+   }
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=False)
-            title = info_dict.get("title", "Unknown Title")
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.download([url])
+   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+     info_dict = ydl.extract_info(url, download=False)
+     title = info_dict.get("title", "Unknown Title")
+     audio_file = ydl.prepare_filename(info_dict)
 
-        await message.reply_audio(audio_file, caption=f"Downloaded song: {title}")
-    except Exception as e:
-        await message.reply_text(f"An error occurred: {e}")
+     # Download the audio file directly using `ydl.extract_info`
+     ydl.extract_info(url, download=True)  # Download the audio file
 
+     await message.reply_audio(audio_file, caption=f"Downloaded song: {title}")
+ except Exception as e:
+   await message.reply_text(f"An error occurred: {e}")
