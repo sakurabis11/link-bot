9f416a4c37b16c962eb1010da0dac62134532cff
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-24 19:05:32 +0530

    Update yts.py

--- plugins/yts.py
+++ plugins/yts.py
@@ -2,32 +2,35 @@ import pyrogram
 from pyrogram import Client, filters
 import yt_dlp
 
-@Client.on_message(filters.command("yts"))
-async def yts(client, message):
- url = message.text.split(" ", 1)[1]  # Extract URL from message
+@Client.on_message(filters.command("sng"))
+async def download_song(client, message):
+    try:
+        url = message.text.split(" ", 1)[1]  # Extract the YouTube URL from the message
 
- try:
-   ydl_opts = {
-     "format": "bestaudio/best",
-     "postprocessors": [
-       {
-         "key": "FFmpegExtractAudio",
-         "preferredcodec": "mp3",
-         "preferredquality": "192",
-       }
-     ],
-     "outtmpl": "%(title)s.%(ext)s",
-     "noplaylist": True,
-   }
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': '%(title)s.%(ext)s',
+            'quiet': True,
+            'no_warnings': True,
+        }
 
-   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-     info_dict = ydl.extract_info(url, download=False)
-     title = info_dict.get("title", "Unknown Title")
-     audio_file = ydl.prepare_filename(info_dict)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            video_title = info_dict.get('title', None)
+            await message.reply_text(f"Downloading {video_title}...")
 
-     # Download the audio file directly using `ydl.extract_info`
-     ydl.extract_info(url, download=True)  # Download the audio file
+            ydl.download([url])
 
-     await message.reply_audio(audio_file, caption=f"Downloaded song: {title}")
- except Exception as e:
-   await message.reply_text(f"An error occurred: {e}")
+            song_path = ydl.prepare_filename(info_dict)
+            await app.send_audio(message.chat.id, audio=song_path)
+
+            await message.reply_text(f"Song sent successfully!")
+
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {str(e)}")
+
