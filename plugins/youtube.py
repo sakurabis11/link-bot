5aa444f5d366b6f444c3afcad1533ce04a375d8c
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-24 19:51:08 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 import yt_dlp
+from yt_dlp import YoutubeDL
 import requests
 
 @Client.on_message(filters.command("yt"))
@@ -26,8 +27,9 @@ async def download_video(client, message):
       info_dict = ydl.extract_info(url, download=False)
       video_title = info_dict.get('title', None)
       duration = info_dict.get('duration', None)  # Get video duration
+      thumbnail_url = info_dict.get('thumbnail', None)  # Get thumbnail URL
 
-      if video_title:
+      if video_title and thumbnail_url:
         downloading_message = await message.reply_text(f"**Downloading {video_title}...**")
         try:
           await downloading_message.delete(delay=10)
@@ -39,14 +41,27 @@ async def download_video(client, message):
         # Convert duration to minutes
         duration_minutes = int(duration) // 60
 
-        await message.reply_video(
-          video=f"{video_title}.mp4",
-          caption=f"**Title:** {video_title}\n**Duration:** {duration_minutes} minutes\n",
-        )
+        # Download the thumbnail
+        response = requests.get(thumbnail_url, stream=True)
+        thumbnail_file = open("thumbnail.jpg", "wb")
+        for chunk in response.iter_content(1024):
+          thumbnail_file.write(chunk)
+        thumbnail_file.close()
 
-        await message.reply_text("Upload completed")
+        try:
+          # Send video and thumbnail as a single message using client.send_video
+          await client.send_video(
+            chat_id=message.chat.id,
+            video=f"{video_title}.mp4",
+            caption=f"**Title:** {video_title}\n**Duration:** {duration_minutes} minutes\n",
+            thumb="thumbnail.jpg"  # Attach the downloaded thumbnail
+          )
+          await message.reply_text("Upload completed")
+        except Exception as e:
+          print(f"Error sending video: {e}")
+
       else:
-        await message.reply_text("Unable to retrieve video title. Please check the video URL and try again.")
+        await message.reply_text("Unable to retrieve video title or thumbnail. Please check the video URL and try again.")
 
   except Exception as e:
     await message.reply_text(f"Error: {e}")
