04e224b61d54c4a8aecf36193b5e391c54d28684
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-24 09:56:14 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,46 +1,50 @@
 from pyrogram import Client, filters
 import yt_dlp
+from PIL import Image  # Import for thumbnail generation
 
 @Client.on_message(filters.command("yt"))
 async def download_video(client, message):
-    try:
-        command_parts = message.text.split(" ", 1)
+  try:
+    command_parts = message.text.split(" ", 1)
 
-        if len(command_parts) == 1:
-            await message.reply_text("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜᴛᴜʙᴇ ᴠɪᴅᴇᴏ ᴜʀʟ (ᴇɢ:- /yt https://www.youtube.com/watch?v=2Vv-BfVoq4g&pp=ygUHcGVyZmVjdA%3D%3D)")
-            return
+    if len(command_parts) == 1:
+      await message.reply_text("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜᴛᴜʙᴇ ᴠɪᴅᴇᴏ ᴜʀʟ (ᴇɢ:- /yt https://www.youtube.com/watch?v=2Vv-BfVoq4g&pp=ygUHcGVyZmVjdA%3D%3D)")
+      return
 
-        url = command_parts[1]
+    url = command_parts[1]
 
-        ydl_opts = {  # Removed extra indentation here
-            'outtmpl': '%(title)s.%(ext)s',
-            'format': 'bestvideo[height<=?720][ext=mp4]+bestaudio[ext=m4a]/best[height<=?720][ext=mp4]/best',  # Prioritize 720p
-            'postprocessors': [{
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4'
-            }],
-        }
+    ydl_opts = {
+      'outtmpl': '%(title)s.%(ext)s',
+      'format': 'bestvideo[height<=?720][ext=mp4]+bestaudio[ext=m4a]/best[height<=?720][ext=mp4]/best',
+      'postprocessors': [{
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4'
+      }],
+    }
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=False)
-            video_title = info_dict.get('title', None)
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+      info_dict = ydl.extract_info(url, download=False)
+      video_title = info_dict.get('title', None)
 
-            if video_title:  # Check if title is available
-                # Send the "Downloading..." message with scheduled deletion:
-                downloading_message = await message.reply_text(f"**Downloading {video_title}...**")
-                try:
-                    await downloading_message.delete(delay=10)  # Schedule deletion after 10 seconds
-                except Exception as e:
-                    print(f"Failed to delete message: {e}")  # Log any deletion errors
+      if video_title:
+        downloading_message = await message.reply_text(f"**Downloading {video_title}...**")
+        try:
+          await downloading_message.delete(delay=10)
+        except Exception as e:
+          print(f"Failed to delete message: {e}")
 
-                ydl.download([url])
+        ydl.download([url])
 
-                # Send the downloaded video with the caption:
-                await message.reply_video(video=f"{video_title}.mp4", caption=video_title)  # Added caption
+        # Generate thumbnail from the downloaded video:
+        thumbnail_file = f"{video_title}_thumb.jpg"
+        Image.open(f"{video_title}.mp4").save(thumbnail_file, "JPEG")  # Extract a JPEG thumbnail
 
-                await message.reply_text("ᴜᴘʟᴏᴀᴅ ᴄᴏᴍᴘʟᴇᴛᴇᴅ")
-            else:
-                await message.reply_text("ᴜɴᴀʙʟᴇ ᴛᴏ ʀᴇᴛʀɪᴇᴠᴇ ᴠɪᴅᴇᴏ ᴛɪᴛʟᴇ. ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴛʜᴇ ᴠɪᴅᴇᴏ ᴜʀʟ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ.")
+        # Send the video with the generated thumbnail:
+        await message.reply_video(video=f"{video_title}.mp4", thumb=thumbnail_file, caption=video_title)
 
-    except Exception as e:
-        await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
+        await message.reply_text("ᴜᴘʟᴏᴀᴅ ᴄᴏᴍᴘʟᴇᴛᴇᴅ")
+      else:
+        await message.reply_text("ᴜɴᴀʙʟᴇ ᴛᴏ ʀᴇᴛʀɪᴇᴠᴇ ᴠɪᴅᴇᴏ ᴛɪᴛʟᴇ. ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴛʜᴇ ᴠɪᴅᴇᴏ ᴜʀʟ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ.")
+
+  except Exception as e:
+    await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
