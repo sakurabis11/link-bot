fb27a19df911dd0be6a353c23cb9f8c71f8e82be
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-24 11:01:19 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, InlineKeyboardMarkup, InlineKeyboardButton
 import yt_dlp
 
 @Client.on_message(filters.command("yt"))
@@ -14,38 +14,44 @@ async def download_video(client, message):
 
        ydl_opts = {
            'outtmpl': '%(title)s.%(ext)s',
-           'format': 'bestvideo[height<=?720][ext=mp4]+bestaudio[ext=m4a]/best[height<=?720][ext=mp4]/best',  # Prioritize 720p
+           'format': 'bestvideo[height<=?720][ext=mp4]+bestaudio[ext=m4a]/best[height<=?720][ext=mp4]/best',
            'postprocessors': [{
                'key': 'FFmpegVideoConvertor',
                'preferedformat': 'mp4'
            }],
-           'writethumbnail': True  # Enable thumbnail extraction
        }
 
        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
            info_dict = ydl.extract_info(url, download=False)
            video_title = info_dict.get('title', None)
+           duration = info_dict.get('duration', None)  # Retrieve video duration
 
            if video_title:
-               # Send the "Downloading..." message with scheduled deletion:
                downloading_message = await message.reply_text(f"**Downloading {video_title}...**")
                try:
-                   await downloading_message.delete(delay=10)  # Schedule deletion after 10 seconds
+                   await downloading_message.delete(delay=10)
                except Exception as e:
-                   print(f"Failed to delete message: {e}")  # Log any deletion errors
+                   print(f"Failed to delete message: {e}")
 
                ydl.download([url])
 
-               # Get video duration and quality:
-               duration = info_dict.get('duration', None)
-               quality = info_dict.get('format_id', None)
+               video_path = f"{video_title}.mp4"
+               caption = f"**Title:** {video_title}\n**Duration:** {duration}"
 
-               # Send the downloaded video with title, duration, quality, and thumbnail:
-               await message.reply_video(
-                   video=f"{video_title}.mp4",
-                   caption=f"**{video_title}**\nDuration: {duration}\nQuality: {quality}",
-                   thumb=f"{video_title}.jpg"  # Use the extracted thumbnail
-               )
+               if message.chat.type == "group":  # Handle group requests
+                   keyboard = InlineKeyboardMarkup(
+                       [
+                           [
+                               InlineKeyboardButton(
+                                   text="Get Video in PM",
+                                   callback_data="send_video_pm"
+                               )
+                           ]
+                       ]
+                   )
+                   await message.reply_video(video=video_path, caption=caption, reply_markup=keyboard)
+               else:  # Send directly in private chats
+                   await message.reply_video(video=video_path, caption=caption)
 
                await message.reply_text("ᴜᴘʟᴏᴀᴅ ᴄᴏᴍᴘʟᴇᴛᴇᴅ")
            else:
@@ -53,3 +59,10 @@ async def download_video(client, message):
 
    except Exception as e:
        await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
+
+   @Client.on_callback_query()
+   async def handle_callback(client, callback_query):
+       if callback_query.data == "send_video_pm":
+           video_path = f"{callback_query.message.caption.splitlines()[1].split('**Title:** ')[1]}.mp4"
+           await client.send_video(callback_query.from_user.id, video=video_path, caption=callback_query.message.caption)
+           await callback_query.answer("Video sent to your PM!")
