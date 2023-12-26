1537f44ca397462973a23d957e93bbc0fa0467a8
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-26 21:12:59 +0530

    Update and rename yt_mp3.py to auto_delete.py

---
+++ plugins/auto_delete.py
@@ -0,0 +1,23 @@
+import pyrogram
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("clear", prefixes="/") & (filters.chat_type.supergroup | filters.chat_type.channel) & filters.me)
+async def handle_clear_command(client, message):
+    try:
+        chat_id = message.chat.id
+        await client.delete_messages(chat_id, message.message_id)
+
+        await client.send_message(chat_id, "Deleting all messages...")
+
+        async for msg in client.iter_history(chat_id, limit=None):
+            try:
+                await client.delete_messages(chat_id, msg.message_id)
+            except Exception as e:
+                print(f"Error deleting message {msg.message_id}: {e}")
+
+        await app.send_message(chat_id, "All messages deleted successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
+        await client.send_message(chat_id, "Failed to delete messages. Please check permissions.")
+
+
