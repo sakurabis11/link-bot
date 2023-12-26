77c6054c4f1a5c5d79d05763b5203bb34168451a
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-26 21:45:17 +0530

    Update auto_delete.py

--- plugins/auto_delete.py
+++ plugins/auto_delete.py
@@ -1,18 +1,28 @@
 import pyrogram
 from pyrogram import Client, filters
 
+DELETE_COMMAND = "/delete"
 
-@Client.on_message(filters.command(["deleteall"], prefixes="!"))
-async def delete_all_messages(client, message):
+@Client.on_message(filters.command(DELETE_COMMAND))
+async def delete_messages(client, message):
     chat_id = message.chat.id
-    user = await app.get_chat_member(chat_id, message.from_user.id)
+    user = message.from_user
 
-    if user.status in ["creator", "administrator"]:
+    if message.chat.type == "group" or message.chat.type == "supergroup":
+        is_admin = await client.get_chat_member(chat_id, user.id)
+        is_admin = is_admin.status in ["administrator", "creator"]
+    elif message.chat.type == "channel":
+        is_admin = await client.get_chat_member(chat_id, user.id)
+        is_admin = is_admin.status in ["creator", "administrator"]
+    else:
+        await message.reply_text("This command can only be used in groups or channels.")
+        return
+
+    if is_admin:
         try:
-            await client.delete_messages(chat_id, message.message_id - 1)
-            await message.reply_text("All messages deleted successfully!")
+            await client.delete_messages(chat_id, range(1))
+            await message.reply_text("All messages have been deleted.")
         except Exception as e:
             await message.reply_text("Failed to delete messages. Error: " + str(e))
     else:
-        await message.reply_text("You are not allowed to use this command.")
-
+        await message.reply_text("You are not allowed to send this command.")
