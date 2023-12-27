dcc2b6fbaa77ba386d4c31899d39daf98a299442
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-27 11:45:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -118,14 +118,15 @@ async def callback_handle(client, query):
 
     elif query.data == 'admin':
         user_id = query.from_user.id
-      if user_id in ADMINS: 
+        if user_id in ADMINS: 
         buttons = [[
             InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='help')
         ]]
+        user_id = query.from_user.id
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(text=script.ADMIN_CMD_TXT, reply_markup=reply_markup, parse_mode="HTML")
-          
-        return await query.answer("You are not authorized to access this feature.", show_alert=True) 
+        return await query.answer("You are not authorized to access this feature.", show_alert=True)
+
 
     elif query.data == 'telegraph':
         buttons = buttons = [[
