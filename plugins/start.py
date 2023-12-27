9a4500c0b32e307a9acdc6d8d004e77de5a676f0
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-27 10:40:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -117,11 +117,18 @@ async def callback_handle(client, query):
         await query.message.edit_text(text=script.HELP_TXT, reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
 
     elif query.data == 'admin':
+        user_id = query.from_user.id
+
+    if user_id in ADMINS:
+        # User is an admin, show the button
         buttons = [[
             InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(text=script.ADMIN_CMD_TXT, reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML) 
+        await query.message.edit_text(text="Your admin command text here", reply_markup=reply_markup, parse_mode="HTML")
+    else:
+        # User is not an admin, you can handle this case as needed
+        await query.answer("You are not authorized to access this feature.", show_alert=True)
 
     elif query.data == 'telegraph':
         buttons = buttons = [[
