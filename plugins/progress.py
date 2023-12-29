5859cac8b48683766d8fcadbf0171f6f4727ee52
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-29 19:02:41 +0530

    Update progress.py

--- plugins/progress.py
+++ plugins/progress.py
@@ -5,7 +5,7 @@ async def send_progress_bar(chat_id, progress, message="Progress:", comment=""):
     message = await app.send_message(chat_id, message + f" {progress}%")
     await message.edit(message + f" {progress}% ({comment})")  
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command("progress"))
 async def start_handler(client, message):
     await message.reply("Starting process...")
 
