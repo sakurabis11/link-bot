1ad330307ff6f7135ffe8a144b75415d9024b44d
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-30 22:17:45 +0530

    Update ott.py

--- plugins/ott.py
+++ plugins/ott.py
@@ -1,41 +1,23 @@
 import pyrogram
 from pyrogram import Client, filters
+import re
 import requests
-from bs4 import BeautifulSoup
 
-async def get_release_info(query):
-    url = f"https://www.google.com/search?q={query}_release_date_platform"
-
-    try:
-        response = requests.get(url)
-        data = response.json()
-        if data["items"]:
-            first_result = data["items"][0]
-            return first_result["snippet"].split(" - ")[0], first_result["snippet"].split(" - ")[1]
-        else:
-            return None, None
-    except Exception as e:
-        print(f"Error fetching release info: {e}")
-        return None, None
-
-@Client.on_message(filters.command("search"))
+@Client.on_message(filters.command(["search"]))
 async def search_movie_or_series(client, message):
-    query = message.text.split(" ", 1)[1]
+    query = message.text.split(" ", 1)[1] 
 
-    try:
-        release_date, platform = await get_release_info(query)
-        if release_date and platform:
-            await message.reply(f"Release Date: {release_date}\nPlatform: {platform}")
-        else:
-            await message.reply("Release date and platform information not found.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error making request: {e}")
-        await message.reply("Error fetching data. Please check your internet connection.")
-    except ValueError as e:
-        print(f"Error parsing data: {e}")
-        await message.reply("Error processing information. Please try again later.")
-    except Exception as e:
-        print(f"Error processing search: {e}")
-        await message.reply("An error occurred while processing your request.")
+    url = f"https://www.google.com/search?q={query}_release_date_platform"  
+    response = requests.get(url)
+    data = response.json()
 
+    if data["Response"] == "True":
+        try:
+            release_date = data["Search"][0]["Year"]
+            platform = "OTT platform information not available"  # Replace with logic to fetch platform
+            await message.reply_text(f"{query} was released in {release_date}. It's available on {platform}")
+        except IndexError:
+            await message.reply_text(f"Movie/series '{query}' not found.")
+    else:
+        await message.reply_text(f"An error occurred while searching for '{query}'. Please try again.")
 
