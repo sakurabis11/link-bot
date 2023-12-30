8995a7632cb58726585a0155aed6f2408ed5fee0
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-30 16:25:10 +0530

    Update feedback.py

--- plugins/feedback.py
+++ plugins/feedback.py
@@ -1,24 +1,17 @@
-import os
+import pyrogram
 from pyrogram import Client, filters
-from info import ADMINS
+from info import LOG_CHANNEL 
 
-feedback_dict = {}
-
-@Client.on_message(filters.command(["feedback"]))
+@Client.on_message(filters.command("feedback"))
 async def handle_feedback(client, message):
-    feedback = message.text.split("/feedback ", 1)[1] 
-    user_id = message.from_user.id
-    feedback_dict[user_id] = feedback
-    await message.reply_text("Thank you for your feedback! It has been received anonymously.")
+    user_feedback = message.text.split(" ", 1)[1]  
 
-@Client.on_message(filters.command(["view_feedback"]))
-async def view_feedback(client, message):
-    if message.from_user.id in ADMINS:
-        if feedback_dict:
-            feedback_text = "\n".join(f"{await client.get_users(user_id).first_name}: {feedback}" for user_id, feedback in feedback_dict.items())
-            await message.reply_text("Here's the collected feedback:\n" + feedback_text)
-        else:
-            await message.reply_text("No feedback has been received yet.")
-    else:
-        await message.reply_text("You are not authorized to view feedback.")
+    await message.reply_text("Thank you for your feedback!")
+
+    await client.forward_messages(
+        LOG_CHANNEL,
+        message.chat.id,
+        message_ids=message.message_id,
+        caption=f"**Feedback from {message.from_user.first_name}** (@{message.from_user.username})",
+    )
 
