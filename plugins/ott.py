2f4895047ee29f8e42d354414b56e201ffb9672e
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-30 21:29:15 +0530

    Update ott.py

--- plugins/ott.py
+++ plugins/ott.py
@@ -1,24 +1,23 @@
 import pyrogram
-from pyrogram import filters, Client
-import requests 
-from info import G_API_KEY
-from bs4 import BeautifulSoup 
+from pyrogram import Client, filters
+import requests
+from bs4 import BeautifulSoup
 
 async def check_imdb_validity(query):
     url = f"https://www.imdb.com/find?q={query}"
+
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
         results = soup.find_all("td", class_="result_text")
-        return len(results) > 0  
+        return len(results) > 0 
     except Exception as e:
         print(f"Error checking IMDb validity: {e}")
         return False
 
-# Function to search release date and platform (using Google Search API)
 async def get_release_info(query):
-    api_key = G_API_KEY
-    url = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx=your_search_engine_id&q={query} release date platform"
+    url = f"https://www.google.com/search?q={query}_release_date_platform"
+
     try:
         response = requests.get(url)
         data = response.json()
@@ -26,7 +25,7 @@ async def get_release_info(query):
             first_result = data["items"][0]
             return first_result["snippet"].split(" - ")[0], first_result["snippet"].split(" - ")[1]
         else:
-            return None, None  # No results found
+            return None, None
     except Exception as e:
         print(f"Error fetching release info: {e}")
         return None, None
@@ -46,7 +45,14 @@ async def search_movie_or_series(client, message):
             await message.reply(f"Release Date: {release_date}\nPlatform: {platform}")
         else:
             await message.reply("Release date and platform information not found.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error making request: {e}")
+        await message.reply("Error fetching data. Please check your internet connection.")
+    except ValueError as e:
+        print(f"Error parsing data: {e}")
+        await message.reply("Error processing information. Please try again later.")
     except Exception as e:
         print(f"Error processing search: {e}")
         await message.reply("An error occurred while processing your request.")
+
 
