68f12f208e1c85fe33e2ba9da534854b667a83ac
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-31 17:11:54 +0530

    Update feedback.py

--- plugins/feedback.py
+++ plugins/feedback.py
@@ -7,7 +7,7 @@ async def feedback(client, message):
 
 @Client.on_message(filters.command("fp"))
 async def feedback_p(client, message):
-  fp = message.text.split("/fp ", 1)[1] 
-  await message.reply_text(f"Hi {message.from_user.mention},\n Thank u for the feedback {fb}")
+  users_message = message.text.split(" ", 1)[1]
+  await message.reply_text(f"Hi {message.from_user.mention},\n Thank u for the feedback {users_message}")
 
-  await client.send_message(LOG_CHANNEL, text=f"<b>New Feedback From {message.from_user.mention}</b>\n His text : <code>{fb}</code>")
+  await client.send_message(LOG_CHANNEL, text=f"<b>New Feedback From {message.from_user.mention}</b>\n His text : <code>{users_message}</code>")
