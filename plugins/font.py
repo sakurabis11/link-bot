4229af9c6515e7e738622c855ae78708c985dcdc
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-01 22:11:58 +0530

    Update font.py

--- plugins/font.py
+++ plugins/font.py
@@ -6,4 +6,4 @@ async def stylize_text(client, message):
     text_to_stylize = message.text.split(" ", 1)[1]  
     stylized_text = Font.SD(text_to_stylize)  
 
-    await message.reply_text(f"ʜᴇʀᴇ ʏᴏᴜʀ ᴛᴇxᴛ: <code>{stylized_text}</code>") 
+    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴛᴇxᴛ: <code>{stylized_text}</code>") 
