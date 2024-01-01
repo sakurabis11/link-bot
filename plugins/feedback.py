078b10a0b8986c94d9050d7e3d3f7ccfb48980fe
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-01 12:22:22 +0530

    Update feedback.py

--- plugins/feedback.py
+++ plugins/feedback.py
@@ -10,12 +10,11 @@ async def feedback_p(client, message):
   fp = message.text.split(" ", 1)[1]
   await message.reply_text(f"ʜɪ {message.from_user.mention},\n ᴛʜᴀɴᴋ ᴜ ғᴏʀ ᴛʜᴇ ғᴇᴇᴅʙᴀᴄᴋ")
 
-  await client.send_message(LOG_CHANNEL, text=script.LOG_TEXT_FP.format(message.from_user.mention, fp))
-
+  await client.send_message(LOG_CHANNEL, text=f"#ɴᴇᴡ_ғᴇᴇᴅʙᴀᴄᴋ_ᴘᴜʙʟɪᴄ\nғᴇᴇᴅʙᴀᴄᴋ ғʀᴏᴍ {message.from_user.mention}\n ᴛʜᴇ ᴛᴇxᴛ ɪs : <code>{fp}</code>")
 
 @Client.on_message(filters.command("fa"))
 async def feedback_a(client, message):
   fa = message.text.split(" ", 1)[1]
   await message.reply_text(f"ʜɪ {message.from_user.mention},\n ᴛʜᴀɴᴋ ᴜ ғᴏʀ ᴛʜᴇ ғᴇᴇᴅʙᴀᴄᴋ")
 
-  await client.send_message(LOG_CHANNEL, text=script.LOG_TEXT_FA.format(message.from_user.mention, fa))
+  await client.send_message(LOG_CHANNEL, text=f"#ɴᴇᴡ_ғᴇᴇᴅʙᴀᴄᴋ_ᴀɴᴏɴʏᴍᴏᴜsʟʏ\nғᴇᴇᴅʙᴀᴄᴋ ғʀᴏᴍ {message.from_user.mention}</b>\n ᴛʜᴇ ᴛᴇxᴛ ɪs : <code>{fa}</code>")
