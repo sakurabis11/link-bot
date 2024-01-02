749045beac6991f3032959786e64f2ab55ec45c6
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-02 16:45:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 
 API_ID = int(os.environ.get('API_ID', '8914119'))
 API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '6645084082:AAEBSY-LiHUhMKvMY9Tp5YY0wzerO2p6as0')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '6645084082:AAH6KAE6RtPoheRXNwwADCgUPhFSPJVcdkc')
 PORT = os.environ.get("PORT", "8080")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1342641151').split()]
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002084798134'))
