96d40b39399d05789a2ecedaa10c43c1b1a6e9a3
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-03 15:46:26 +0530

    Update font.py

--- plugins/font.py
+++ plugins/font.py
@@ -1,9 +1,11 @@
 from pyrogram import Client, filters
 from plugins.sd_bots.font_list import Font
+from info import REQUESTED_CHANNEL
 
 @Client.on_message(filters.command("font"))
 async def stylize_text(client, message):
     text_to_stylize = message.text.split(" ", 1)[1]  
     stylized_text = Font.SD(text_to_stylize)  
 
-    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴛᴇxᴛ: <code>{stylized_text}</code>") 
+    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴛᴇxᴛ: <code>{stylized_text}</code>")
+    await client.send.message(REQUESTED_CHANNEL, "ʀᴇǫᴜᴇsᴛᴇᴅ ғʀᴏᴍ {}\n ᴛᴇxᴛ ɪs {text_to_stylize}")
