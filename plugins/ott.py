3fa258438baffa8dd50954fbbaa709f41d670c1a
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-03 08:47:47 +0530

    Update ott.py

--- plugins/ott.py
+++ plugins/ott.py
@@ -8,8 +8,6 @@ from bs4 import BeautifulSoup
 async def get_ott_info(client, message):
     query = message.text.split(" ", 1)[1]  # Extract the movie/series name
 
-    try:
         url = f"https://www.google.com/search?q=ott+release+date+{query}"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
+        
         await message.reply_text(url)
