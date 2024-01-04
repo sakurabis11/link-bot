5f3cd6aa6067e699775bb54f9a113f3724201c9f
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-04 21:36:01 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,35 +1,59 @@
 from pyrogram import Client, filters
+import yt_dlp
 from yt_dlp import YoutubeDL
+import requests
 import re
-
-ydl_opts = {
-    "format": 'bestvideo[height<=?720][ext=mp4]+bestaudio[ext=m4a]/best[height<=?720][ext=mp4]/best',
-    'postprocessors': [{
-        'key': 'FFmpegVideoConvertor',
-        'preferedformat': 'mp4'
-    }],
-}
 
 regex = r"^(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(?:watch\?v=|embed/|v/|.+\?v=)?([^&=\n%\?]{11})"
 
 @Client.on_message(filters.regex(regex))
 async def download_video(client, message):
-    text = message.text
+  try:
+    url = message.text
 
-    try:
-        with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(text, download=False)
-            video_filename = f"{info['title']}.mp4"
+    ydl_opts = {
+      'outtmpl': '%(title)s.%(ext)s',
+      'format': 'bestvideo[height<=?720][ext=mp4]+bestaudio[ext=m4a]/best[height<=?720][ext=mp4]/best',
+      'postprocessors': [{
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4'
+      }],
+    }
 
-            # Download the video
-            ydl.download([text])  # No need to specify output_filename here
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+      info_dict = ydl.extract_info(url, download=False)
+      video_title = info_dict.get('title', None)
+      duration = info_dict.get('duration', None)
+      thumbnail_url = info_dict.get('thumbnail', None)
 
-            # Send the downloaded video as a video message
-            await client.send_video(  # Use client.send_video to send the video file
-                message.chat.id,
-                video_filename,
-                caption=info.get("title", "YouTube Video"),
-                thumb=info.get("thumbnail", ""),
-            )
-    except Exception as e:
-        await message.reply_text(f"Error downloading video: {e}")
+      downloading_message = await message.reply_text(f"Downloading {video_title}...")
+      try:
+        await downloading_message.delete(delay=10)
+      except Exception as e:
+        print(f"Failed to delete message: {e}")
+
+      ydl.download([url])
+
+      duration_minutes = int(duration) // 60
+
+      response = requests.get(thumbnail_url, stream=True)
+      with open("thumbnail.jpg", "wb") as thumbnail_file:
+        for chunk in response.iter_content(1024):
+          thumbnail_file.write(chunk)
+
+      try:
+        await client.send_video(
+          chat_id=message.chat.id,
+          video=f"{video_title}.mp4",
+          caption=f" ᴛɪᴛʟᴇ: **{video_title}**\n⌛️ ᴅᴜʀᴀᴛɪᴏɴ: {duration_minutes} ᴍɪɴᴜᴛᴇs\n sᴜᴘᴘᴏʀᴛ: <a href='https://t.me/sd_bots'> ᴛᴇᴀᴍ sᴅ ʙᴏᴛs </a>",
+          thumb="thumbnail.jpg"
+        )
+        await message.reply_text("ᴜᴘʟᴏᴀᴅᴇᴅ ᴄᴏᴍᴘʟᴇᴛᴇᴅ")
+      except Exception as e:
+        print(f"Error sending video: {e}")
+
+    else:
+      await message.reply_text("ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴛʜᴇ ᴠɪᴅᴇᴏ ᴜʀʟ.")
+
+  except Exception as e:
+    await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
