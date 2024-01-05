d6c6b1a32d5e5f90c25b0167a16380bb381c9016
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-06 01:09:41 +0530

    Update auto_request.py

--- plugins/auto_request.py
+++ plugins/auto_request.py
@@ -1,10 +1,12 @@
 import pyrogram
 from pyrogram import Client, filters
 
-@Client.on_message(filters.chat_member(new_chat_members=True))
-async def auto_accept(client, message):
-    WELCOME_MESSAGE = f"ʜɪ {message.from_user.mention}, ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ɪs ᴀᴄᴄᴘᴇᴛᴇᴅ "
+WELCOME_MESSAGE = f"ʜɪ {user.mention}, ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ɪs ᴀᴄᴄᴘᴇᴛᴇᴅ "
 
+@Client.on_chat_join_request(filters.chat_member(new_chat_members=True))
+async def auto_accept(client, message):
+    chat=message.chat
+    user=message.from_user
     for new_member in message.new_chat_members:
         if not new_member.is_self:
             await client.approve_chat_join_request(message.chat.id, new_member.id)
