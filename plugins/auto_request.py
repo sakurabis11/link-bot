bfbe6056c0037f747dc2e958c7d9f712e451c7cb
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-06 08:29:03 +0530

    Create auto_request.py

---
+++ plugins/auto_request.py
@@ -0,0 +1,13 @@
+import pyrogram
+from pyrogram import Client, filters
+
+@Client.on_chat_member_request()
+async def auto_accept_request(client, chat_member_update):
+    chat_id = chat_member_update.chat.id
+    user_id = chat_member_update.from_user.id
+
+    try:
+        await client.get_chat_member(chat_id, client.me.id)
+        await client.approve_chat_join_request(chat_id, user_id)
+    except Exception as e:
+        print(f"Error approving request: {e}")
