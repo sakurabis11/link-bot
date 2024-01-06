cbd0852f27dc2a7fe0d8dd127302bf3493dfcbfa
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-06 07:09:54 +0530

    Update feedback.py

--- plugins/feedback.py
+++ plugins/feedback.py
@@ -14,7 +14,7 @@ async def feedback_p(client, message):
   await client.send_message(LOG_CHANNEL, text=f"#ɴᴇᴡ_ғᴇᴇᴅʙᴀᴄᴋ_ᴘᴜʙʟɪᴄ\nғᴇᴇᴅʙᴀᴄᴋ ғʀᴏᴍ {message.from_user.mention}\n ᴛʜᴇ ᴛᴇxᴛ ɪs : <code>{fp}</code>")
 
 @Client.on_message(filters.command("fa"))
-async def feedback_a(client, message:Message):
+async def feedback_a(client, message):
   fa = message.text.split(" ", 1)[1]
   await message.reply_text(f"ʜɪ {message.from_user.mention},\n ᴛʜᴀɴᴋ ᴜ ғᴏʀ ᴛʜᴇ ғᴇᴇᴅʙᴀᴄᴋ")
 
