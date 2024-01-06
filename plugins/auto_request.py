f032357a33aebfdb47139a8613285e9750695660
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-06 16:58:57 +0530

    Update auto_request.py

--- plugins/auto_request.py
+++ plugins/auto_request.py
@@ -7,13 +7,11 @@ async def auto_accept_request(client, chat_member_update):
     user_id = chat_member_update.from_user.id
 
     try:
-        await client.get_chat_member(chat_id, client.me.id)  # Check bot's membership
-        await client.approve_chat_join_request(chat_id, user_id)  # Approve request
-
-        # Send welcome message to user's PM
+        await client.get_chat_member(chat_id, client.me.id) 
+        await client.approve_chat_join_request(chat_id, user_id)
         await client.send_message(
             user_id,
-            f"Welcome to {chat_member_update.chat.title}, {chat_member_update.from_user.first_name}! "
+            f"ʜᴇʏ {chat_member_update.from_user.mention}!\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ {chat_member_update.chat.title}"
         )
 
     except Exception as e:
