52ae5001633c451108d5d63295402661b6fe64e7
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-07 22:42:43 +0530

    Update pinterest.py

--- plugins/pinterest.py
+++ plugins/pinterest.py
@@ -7,15 +7,15 @@ STICKER_ID = "CAACAgUAAxkBAAIefmWa2mFflQjODv8DcWTwKN5rb7x3AAJyCgACywLBVKKgVw2dk9
 @Client.on_message(filters.text & filters.regex(r"https://pin\.it/\w+"))
 async def download_photo(client, message):
     try:
-        await message.reply_sticker(STICKER_ID)  # Send the sticker
+        await message.reply_sticker(STICKER_ID)  
 
         pin_url = message.text
         response = requests.get(pin_url)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
+        response.raise_for_status()  
 
         image_url = response.url  # Extract the direct image URL
 
-        await app.send_photo(message.chat.id, image_url)  # Send the downloaded photo
+        await client.send_photo(message.chat.id, image_url)  
     except Exception as e:
         await message.reply_text(f"Error downloading photo: {e}")
 
