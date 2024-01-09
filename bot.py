5a9f8778739c8aee0514541a7a0e58609e509117
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-10 00:38:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,26 +5,32 @@ from pytz import timezone
 import logging.config
 from pyrogram.errors import BadRequest, Unauthorized
 from typing import Union, Optional, AsyncGenerator
-from plugins import web_server 
 import pytz
 import aiohttp
-from aiohttp import web
 from utils import temp
 from pyrogram.raw.all import layer
 from pyrogram import types
 from Script import script 
 import aiohttp
-
+import logging
+import logging.config
 
 logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, WARN_DATA_ID, WARN_SETTINGS_ID
+from utils import temp
+
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="simple-bot",
+            session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -32,45 +38,23 @@ class Bot(Client):
             plugins={"root": "plugins"},
             sleep_threshold=5,
         )
+
     async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
         await super().start()
+        await Media.ensure_indexes()
         me = await self.get_me()
         temp.ME = me.id
         temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
         self.username = '@' + me.username
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        await web.TCPSite(app, "0.0.0.0", 8080).start()
-        logger.info("Running...")
-        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...")
-        if LOG_CHANNEL:
-            try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
-                date = curr.strftime('%d %B, %Y')
-                time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
-            except:
-                print("ʀᴜɴɴɪɴɢ..")
+        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")        
 
-       
     async def stop(self, *args):
-       await super().stop()      
-       print("Bot Restarting...")
-
-async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
+        await super().stop()
+        print("Bot stopped. Bye.")
 
 
-if __name__ == "__main__":
-   app = Bot()
-   app.run()
+app = Bot()
+app.run()
