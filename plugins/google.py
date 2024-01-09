56912de112f386e26c8159bb37882a79fa0717f4
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-09 22:02:18 +0530

    Update google.py

--- plugins/google.py
+++ plugins/google.py
@@ -9,6 +9,7 @@ async def google(_, message):
         return
     query = message.text.split(None, 1)[1]
     m = await message.reply_text("searching")
+    google_search = f"https://www.google.com/search?q={query}+ott+release+date+and+platform"
     try:
         results = await google_search(query, num_results=5)
         for result in results:
