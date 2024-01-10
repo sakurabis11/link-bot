e011796ea8e76924a8340c9fdb4dcb5718df9c4f
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-10 22:34:07 +0530

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -23,3 +23,24 @@ async def create_file(bot, message):
         
     except Exception as e:
         await message.reply(f"An error occurred: {e}")  
+
+@Client.on_message(filters.command('f2t') & filters.reply)
+async def create_file(bot, message):
+    try:
+        query = message.reply_to_document
+        
+        x = await message.reply('ᴄᴏɴᴠᴇʀᴛɪɴɢ.')
+        a = await x.edit('ᴄᴏɴᴠᴇʀᴛɪɴɢ..')
+        c = await a.edit('ᴄᴏɴᴠᴇʀᴛɪɴɢ...')
+
+        with open(file_name, 'w+') as outfile:
+            outfile.write(query)
+
+        await message.reply_text(query)
+        await c.delete()
+        y = f"{message.from_user.mention},\n ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴄᴏᴍᴘʟᴇᴛᴇᴅ"
+        await message.reply_text(y)
+        
+        
+    except Exception as e:
+        await message.reply(f"An error occurred: {e}")  
