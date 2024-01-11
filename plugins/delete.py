d903b2749de29b0a161066a1f7a53fbf26da54b7
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-11 19:38:02 +0530

    Update delete.py

--- plugins/delete.py
+++ plugins/delete.py
@@ -1,29 +1,29 @@
 import asyncio
-from pyrogram import Client, filters
+from pyrogram import Client, filters, app  # Import app
 
 @Client.on_message(filters.command("all_delete") & filters.group)
 async def delete_all_messages(client, message):
-    # Check if the user who sent the command is an admin
+    # Check if the user is an admin
     user_id = message.from_user.id
     chat_id = message.chat.id
-
     user_info = client.get_chat_member(chat_id, user_id)
     if not user_info.status == "administrator":
-        message.reply_text("You need to be an admin to use this command.")
+        await message.reply_text("You need to be an admin to use this command.")
         return
 
     # Check if the bot is an admin
-    bot_info = client.get_chat_member(chat_id, app.get_me().id)
+    bot_info = client.get_chat_member(chat_id, app.get_me().id)  # Use app correctly
     if not bot_info.status == "administrator":
-        message.reply_text("Please make sure the bot is an admin.")
+        await message.reply_text("Please make sure the bot is an admin.")
         return
 
-    # Delete all messages in the group
+    # Delete messages efficiently
     messages = client.get_chat_history(chat_id)
-    for msg in messages:
-        client.delete_messages(chat_id, msg.message_id)
+    message_ids = [msg.message_id for msg in messages]
+    await client.delete_messages(chat_id, message_ids)
 
-    message.reply_text("All messages deleted successfully.")
+    await message.reply_text("All messages deleted successfully.")
+
 
 
 
