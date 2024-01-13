68e09e00f20ab542139dedb8ac6eaa57221615f5
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-13 15:11:16 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -1,7 +1,4 @@
-"""
-$ pip install google-generativeai
-"""
-
+python
 from pyrogram import Client, filters
 import requests
 from info import REQUESTED_CHANNEL
@@ -13,43 +10,44 @@ genai.configure(api_key="AIzaSyDzq1pXw1-9JS7Z1fQ0m1RGdHK6vRY9I7Q")
 async def ai_generate(client, message):
     user_input = message.text.split()[1:]
     user_input = " ".join(user_input)
-  if message.text is not in user_input:
-    await message.reply_text("please send ur question with <code>/ai</code>")
-    return
-  else:
-    generation_config = {
-        "temperature": 0.9,
-        "top_p": 1,
-        "top_k": 1,
-        "max_output_tokens": 2048,
-    }
 
-    safety_settings = [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-    ]
+    if message.text not in user_input:
+        await message.reply_text("please send ur question with /ai")
+        return
+    else:
+        generation_config = {
+            "temperature": 0.9,
+            "top_p": 1,
+            "top_k": 1,
+            "max_output_tokens": 2048,
+        }
 
-    model = genai.GenerativeModel(
-        model_name="gemini-pro",
-        generation_config=generation_config,
-        safety_settings=safety_settings
-    )
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+        ]
 
-    prompt_parts = [user_input]
-    response = model.generate_content(prompt_parts)
-    await message.reply_text(response.text)  
-    await client.send_message(REQUESTED_CHANNEL, text=f"ᴏᴘᴇɴᴀɪ ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
+        model = genai.GenerativeModel(
+            model_name="gemini-pro",
+            generation_config=generation_config,
+            safety_settings=safety_settings
+        )
+
+        prompt_parts = [user_input]
+        response = model.generate_content(prompt_parts)
+        await message.reply_text(response.text)
+        await client.send_message(REQUESTED_CHANNEL, text=f"google ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
