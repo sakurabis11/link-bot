013edd28260896d201394dd8c7dc8bd2c56ba4fa
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-14 21:19:37 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -1,60 +1,56 @@
-from pyrogram import Client, filters, enums
+# sd bots
+
+from pyrogram import Client, filters
 import requests
-from info import REQUESTED_CHANNEL, SAMPLE
+from info import REQUESTED_CHANNEL, GOOGLE_API_KEY
 import google.generativeai as genai
+# how to get the api key == https://t.me/sd_bots/256 (copy this link and search on telegram)
 
-genai.configure(api_key="AIzaSyD214hhYJ-xf8rfaWX044_g1VEBQ0ua55Q")
+genai.configure(api_key=GOOGLE_API_KEY)
 
-@Client.on_message(filters.command("ai") & filters.chat(SAMPLE) & filters.incoming)
+@Client.on_message(filters.command("ai"))
 async def ai_generate(client, message):
-    if message.chat.id != SAMPLE:
-        await message.reply_text("This bot only works in the specified GROUP")
-        return
-     
-    user_input = message.text.split()[1:]
+   user_input = message.text.split()[1:]
 
-    if not user_input:
-        await message.reply_text("Please provide your question after /ai")
-        return
+   if not user_input:
+       await message.reply_text("Please provide your question after /ask")
+       return
 
-    user_input = " ".join(user_input)
+   user_input = " ".join(user_input)
 
-    generation_config = {
-        "temperature": 0.9,
-        "top_p": 1,
-        "top_k": 1,
-        "max_output_tokens": 2048,
-    }
+   generation_config = {
+       "temperature": 0.9,
+       "top_p": 1,
+       "top_k": 1,
+       "max_output_tokens": 2048,
+   }
 
-    safety_settings = [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-    ]
+   safety_settings = [
+       {
+           "category": "HARM_CATEGORY_HARASSMENT",
+           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+       },
+       {
+           "category": "HARM_CATEGORY_HATE_SPEECH",
+           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+       },
+       {
+           "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+       },
+       {
+           "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+       },
+   ]
 
-    model = genai.GenerativeModel(
-        model_name="gemini-pro",
-        generation_config=generation_config,
-        safety_settings=safety_settings
-    )
+   model = genai.GenerativeModel(
+       model_name="gemini-pro",
+       generation_config=generation_config,
+       safety_settings=safety_settings
+   )
 
-    prompt_parts = [user_input]
-    response = model.generate_content(prompt_parts)
-    await message.reply_text(response.text)
-    await client.send_message(REQUESTED_CHANNEL, text=f"Google request from {message.from_user.mention}\nQuery is:- {user_input}")
-     
-    if message.chat.type in [enums.ChatType.PRIVATE]:
-        await message.reply_text("This feature will only work in groups.")
+   prompt_parts = [user_input]
+   response = model.generate_content(prompt_parts)
+   await message.reply_text(response.text)
+   await client.send_message(REQUESTED_CHANNEL, text=f"#google_ai ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
