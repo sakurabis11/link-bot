66857a167f58b6006483008ab64a5edfd09b215e
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-14 22:24:52 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -22,7 +22,7 @@ async def ai_generate(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker("CAACAgUAAxkBAAIjWGWkDiJW1Dyn6n8CjbbwxExf0FEIAAJyCgACywLBVKKgVw2dk9PbHgQ")
-        await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}", reply_markup=reply_mark_up)
+        await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}", reply_markup=reply_markup)
         return
 
     generation_config = {
