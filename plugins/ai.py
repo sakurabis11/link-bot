c6b343cd6c2e10cf15bcd53d212f555165841ddd
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-14 22:40:02 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -16,7 +16,7 @@ async def ai_generate(client, message):
 
     user_input = " ".join(user_input)
 
-    user_input.lower() in ["who is your owner", "what is your owner name"]
+    if user_input.lower() in ["who is your owner", "what is your owner name"]:  # Fixed indentation here
         buttons = [[
             InlineKeyboardButton("developer", url="https://t.me/sd_bots")
         ]]
@@ -24,40 +24,3 @@ async def ai_generate(client, message):
         await message.reply_sticker("CAACAgUAAxkBAAIjWGWkDiJW1Dyn6n8CjbbwxExf0FEIAAJyCgACywLBVKKgVw2dk9PbHgQ")
         await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}", reply_markup=reply_markup)
         return
-
-    generation_config = {
-        "temperature": 0.9,
-        "top_p": 1,
-        "top_k": 1,
-        "max_output_tokens": 2048,
-    }
-
-    safety_settings = [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-    ]
-
-    model = genai.GenerativeModel(
-        model_name="gemini-pro",
-        generation_config=generation_config,
-        safety_settings=safety_settings
-    )
-
-    prompt_parts = [user_input]
-    response = model.generate_content(prompt_parts)
-    await message.reply_text(response.text)
-    await client.send_message(REQUESTED_CHANNEL, text=f"#google_ai ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
