a2f5090c13c0cdec41f5dc28c2a033a62391cd90
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-15 15:29:06 +0530

    Delete plugins/file.py

--- plugins/file.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.document & filters.private)
-async def document_handler(client, message):
-    await message.reply(message.document.file_id)
-    await message.reply_text(f"https://t.me/mrtgcoderbot?start={message.document.file_id}")
