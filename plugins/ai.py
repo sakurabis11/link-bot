92f47f0720ad52e7dd72390c47c79cfaf1be6c4f
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-15 14:20:31 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import requests
 from info import REQUESTED_CHANNEL, SAMPLE
 import google.generativeai as genai
