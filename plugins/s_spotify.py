e25b03b2bf41dc128c74c0bd48650e65b49d1f71
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-18 10:45:43 +0530

    Add files via upload

---
+++ plugins/s_spotify.py
@@ -0,0 +1,32 @@
+from pyrogram import Client, filters
+import spotipy
+import re
+from info import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+
+   # Create a Spotify API client instance
+spotify = spotipy.Spotify(
+    client_credentials_manager=spotipy.SpotifyClientCredentials(
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+    )
+)
+
+spotify_link_regex = re.compile(r"(https://open.spotify.com/track/\w+)")
+   
+@client.on_message(spotify_link_regex)
+async def download_song(client, message):
+       # Extract the song name from the message
+       song_name = message.text
+
+       # Search for the song on Spotify
+       results = spotify.search(q=song_name, type="track")
+
+       # Get the Spotify track ID
+       track_id = results["tracks"]["items"][0]["id"]
+
+       # Download the song in MP3 format
+       song_url = spotify.track(track_id)["preview_url"]
+       song_data = requests.get(song_url)
+
+       # Send the song as MP3 file to the user
+       client.send_audio(message.chat.id, song_data, title=song_name, performer="Spotify")
