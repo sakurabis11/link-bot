5d919f1a96b39964c42d5863c7b5f1e9f2c56088
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-19 10:50:17 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -1,33 +1,70 @@
-### This download from saavn.me an unofficial api
+import os
+import random
+import shutil
 
-from pyrogram import Client,filters
-import requests,os,wget 
+from asgiref.sync import sync_to_async
+from pyrogram import Client, filters, enums
+from yt_dlp import YoutubeDL
 
-@Client.on_message(filters.command('song'))
-async def song(client, message):
+async def download_songs(query, download_directory="."):
+    query = f"{query} Lyrics".replace(":", "").replace("\"", "")
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "default_search": "ytsearch",
+        "noplaylist": True,
+        "nocheckcertificate": True,
+        "outtmpl": f"{download_directory}/%(title)s.mp3",
+        "quiet": True,
+        "addmetadata": True,
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        try:
+            video = ydl.extract_info(f"ytsearch:{query}", download=False)["entries"][0]["id"]
+            info = ydl.extract_info(video)
+            filename = ydl.prepare_filename(info)
+            if not filename:
+                print(f"Track Not Found⚠️")
+            else:
+                path_link = filename
+                return path_link
+        except Exception as e:
+            pass
+            print(e)
+    return video
+
+@Client.on_message(filters.command("song"))
+async def song(_, message):
     try:
-       song_name = message.text.split(None, 1)[1]
-    except:
-        return await message.reply("/song requires an argument.")
-    if song_name.startswith(" "):
-        await message.reply("/song requires an argument.")
-        return ""
-    m = await message.reply('Downloading...')
-    try:
-        r = requests.get(f"https://saavn.me/search/songs?query={song_name}&page=1&limit=1").json()
+        await message.reply_chat_action(enums.ChatAction.TYPING)
+        k = await message.reply("⌛")
+        print("⌛")
+        try:
+            randomdir = f"/tmp/{str(random.randint(1, 100000000))}"
+            os.mkdir(randomdir)
+        except Exception as e:
+            await message.reply_text(
+                f"Failed to send song retry after sometime  reason: {e}"
+            )
+            return await k.delete()
+        query = message.text.split(None, 1)[1]
+        await message.reply_chat_action(enums.ChatAction.RECORD_AUDIO)
+        path = await download_songs(query, randomdir)
+        await message.reply_chat_action(enums.ChatAction.UPLOAD_AUDIO)
+        await k.edit("uploading")
+        await message.reply_audio(path)
+
+    except IndexError:
+        await message.reply("song requires an argument `eg /song faded`")
+        return await k.delete()
     except Exception as e:
-        await pak.edit(str(e))
-        return
-    sname = r['data']['results'][0]['name']
-    slink = r['data']['results'][0]['downloadUrl'][4]['link']
-    ssingers = r['data']['results'][0]['primaryArtists']
-    img = r['data']['results'][0]['image'][2]['link']
-    thumbnail = wget.download(img)
-    file = wget.download(slink)
-    ffile = file.replace("mp4", "mp3")
-    os.rename(file, ffile)
-    await pak.edit('Uploading...')
-    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
-    os.remove(ffile)
-    os.remove(thumbnail)
-    await m.delete()
+        await message.reply_text(f"Failed to send song  reason: {e}")
+    finally:
+        try:
+            shutil.rmtree(randomdir)
+            return await k.delete()
+        except:
+            pass
