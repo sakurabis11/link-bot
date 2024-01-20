3509bc7564e3e300fd592df08846eb4867043b69
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-20 12:41:42 +0530

    Create rename.py

---
+++ plugins/rename.py
@@ -0,0 +1,37 @@
+import os
+from pyrogram import Client, filters
+from info import ADMIN
+
+@Client.on_message(filters.photo & filters.private & filters.user(ADMIN))
+async def save_thumbnail(client, message):
+    thumbnail_file = await message.download()
+    await message.reply("Thumbnail saved!")
+
+@Client.on_message(filters.command("rename") & filters.private & filters.user(ADMIN))
+async def rename_file(client, message):
+    try:
+        reply_message = await message.get_reply_message()
+        new_file_name = message.command[1]
+
+        if reply_message.video or reply_message.document:
+            await message.reply("Downloading and renaming...")
+            await client.download_media(message=reply_message, file_name=new_file_name)
+
+            if os.path.exists("thumbnail.jpg"):
+                if reply_message.video:
+                    await client.send_video(
+                        YOUR_ADMIN_ID, new_file_name, thumb="thumbnail.jpg"
+                    )
+                else:
+                    await client.send_document(
+                        YOUR_ADMIN_ID, new_file_name, thumb="thumbnail.jpg"
+                    )  # Use send_document for documents
+            else:
+                await client.send_video(YOUR_ADMIN_ID, new_file_name)  # Adjust for document
+
+            await message.reply("File renamed and sent!")
+        else:
+            await message.reply("Reply to a video or document to rename.")
+    except Exception as e:
+        await message.reply("Error: " + str(e))
+
