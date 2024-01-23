d37d5e548844dbb78e6f062eedb14ee9bca84e9a
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-23 21:42:04 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
-from database.welcome_db import set_welcome, remove_welcome
+from database.users_db import db
 from info import DATABASE_NAME, DATABASE_URI
 
 @Client.on_message(filters.command("set_welcome"))
@@ -14,7 +14,7 @@ async def set_welcome(client, message: Message):
            raise ValueError("Please provide a welcome message")
 
        welcome_message = message.text.split(" ", 1)[1]
-       await set_welcome(welcome_message, message.chat.id)  # Corrected line
+       await db.set_welcome(welcome_message, message.chat.id)  # Corrected line
        await message.reply_text(f"Welcome message successfully set for this chat")
    except Exception as e:
        await message.reply_text(f"An error occurred: {e}")
@@ -31,7 +31,7 @@ async def remove_welcome(client, message: Message):
         if not welcome_message:
             raise ValueError(f"There's no welcome message set for this chat")
 
-        await set_welcome(message.chat.id, welcome_message=None)
+        await db.set_welcome(message.chat.id, welcome_message=None)
         await message.reply_text(f"Welcome message successfully removed from this chat")
     except Exception as e:
         await message.reply_text(f"An error occurred: {e}")
