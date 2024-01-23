1e70551ca0d053fbfce28638db9af1f1f564a5d7
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-23 15:32:47 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -6,12 +6,12 @@ from database.users_db import db
 async def set_welcome(client, message: Message):
    user = await client.get_chat_member(message.chat.id, message.from_user.id)
    if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
-      return await message.reply_text("your are not allowed to use this command")
-   if len(message.command) == 1:
-      return await message.reply_text("please provide a welcome message")
-    welcome_message = message.text.split(" ", 1)[1]
-    await db.set_welcome(message.chat.id, welcome_message=welcome_message)
-    await message.reply_text("sucessfully set welcoem to this {chat.title}")
+       return await message.reply_text("your are not allowed to use this command")
+     if len(message.command) == 1:
+        return await message.reply_text("please provide a welcome message")
+   welcome_message = message.text.split(" ", 1)[1]
+   await db.set_welcome(message.chat.id, welcome_message=welcome_message)
+   await message.reply_text("sucessfully set welcoem to this {chat.title}")
 
 @Client.on_message(filters.command("welcome_message_remove"))
 async def set_welcome(client, message: Message):
