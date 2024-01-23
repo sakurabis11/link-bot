3c7a19108c290401f3bf2ccb60ce4da3128edb2e
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-23 15:27:35 +0530

    Delete database/welcome_db.py

--- database/welcome_db.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from pyrogram import enums
-from info import DATABASE_URI, DATABASE_NAME
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-
-myclient = pymongo.MongoClient(DATABASE_URI)
-mydb = myclient[DATABASE_NAME]
