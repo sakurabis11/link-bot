0efd5d47076188f8d5efaded1aa9b448a39c6773
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-23 15:37:37 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -4,25 +4,22 @@ from database.users_db import db
 
 @Client.on_message(filters.command("set_welcome"))
 async def set_welcome(client, message: Message):
-   user = await client.get_chat_member(message.chat.id, message.from_user.id)
-   if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
-       return await message.reply_text("your are not allowed to use this command")
-     if len(message.command) == 1:
-        return await message.reply_text("please provide a welcome message")
-   welcome_message = message.text.split(" ", 1)[1]
-   await db.set_welcome(message.chat.id, welcome_message=welcome_message)
-   await message.reply_text("sucessfully set welcoem to this {chat.title}")
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
+        return await message.reply_text("You are not allowed to use this command")
+    if len(message.command) == 1:
+        return await message.reply_text("Please provide a welcome message")
+    welcome_message = message.text.split(" ", 1)[1]
+    await db.set_welcome(message.chat.id, welcome_message=welcome_message)
+    await message.reply_text(f"Welcome message successfully set for {message.chat.title}")  # Use f-string for formatting
 
 @Client.on_message(filters.command("welcome_message_remove"))
-async def set_welcome(client, message: Message):
-   user = await client.get_chat_member(message.chat.id, message.from_user.id)
-   if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
-      return await message.reply_text("your are not allowed to use this command")
-     if not welcome_message:
-       return await message.reply_text("you don't have any welcome message in this {chat.title}")
-   await db.set_welcome(message.chat.id, welcome_message=None)
-   await message.reply_text("sucessfully removed the welcome meassage from your {chat.title}")
-
-
-
-    
+async def remove_welcome(client, message: Message):  # Function name corrected
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
+        return await message.reply_text("You are not allowed to use this command")
+    welcome_message = await db.get_welcome(message.chat.id)  # Retrieve welcome message
+    if not welcome_message:
+        return await message.reply_text(f"There's no welcome message set in {message.chat.title}")
+    await db.set_welcome(message.chat.id, welcome_message=None)
+    await message.reply_text(f"Welcome message successfully removed from {message.chat.title}")
