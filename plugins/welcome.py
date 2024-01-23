26d114f90615fb48ab87a0b3de20bb76968e7f2a
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-23 21:12:53 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -14,7 +14,7 @@ async def set_welcome(client, message: Message):
             raise ValueError("Please provide a welcome message")
 
         welcome_message = message.text.split(" ", 1)[1]
-        await db.set_welcome(message.chat.id)
+        await set_welcome(message.chat.id)
         await message.reply_text(f"Welcome message successfully set for this chat")
     except Exception as e:
         await message.reply_text(f"An error occurred: {e}")
@@ -30,7 +30,7 @@ async def remove_welcome(client, message: Message):
         if not welcome_message:
             raise ValueError(f"There's no welcome message set for this chat")
 
-        await db.set_welcome(message.chat.id, welcome_message=None)
+        await set_welcome(message.chat.id, welcome_message=None)
         await message.reply_text(f"Welcome message successfully removed from this chat")
     except Exception as e:
         await message.reply_text(f"An error occurred: {e}")
