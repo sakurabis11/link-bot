bbbd48e7b31281f8ebeafa9d2f40548f1788b085
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-25 22:16:10 +0530

    Update grp_management.py

--- plugins/grp_management.py
+++ plugins/grp_management.py
@@ -3,14 +3,14 @@ from pyrogram.types import Message
 from pyrogram.errors import *
 
 @Client.on_message(filters.command("kick"))
-async def kick_user(client, message: Message):
+async def kick_user(bot, message: Message):
   try:
-    user = await client.get_chat_member(message.chat.id, message.from_user.id)
+    user = await bot.get_chat_member(message.chat.id, message.from_user.id)
     if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
       raise PermissionError("You are not allowed to use this command")
 
     user_id = message.reply_to_message.from_user.id
-    await client.kick_chat_member(message.chat.id, user_id)
+    await bot.kick_chat_member(user_id)
     await message.reply_text("User has been kicked from this group")
   except PermissionError as e:
     await message.reply_text(str(e))
@@ -18,10 +18,10 @@ async def kick_user(client, message: Message):
     await message.reply_text(str(e))
 
 @Client.on_message(filters.command("kickme"))
-async def kick_i(client, message: Message):
+async def kick_i(bot, message: Message):
   try:
      user_id = message.from_user.id
-     await client.kick_chat_member(message.chat.id, user_id)
+     await bot.kick_chat_member(user_id)
      await message.reply_text(f"{message.user.mention} has been kicked from this group by himself/herself")
   except Exception as e:
      await message.reply_text(str(e))
