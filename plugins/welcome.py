4a4448be186c42cce0cb7201a5e6ef4699aa333a
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-27 23:59:29 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -21,19 +21,22 @@ async def set_welcome(client, message: Message):
 
 @Client.on_message(filters.command("show_welcome_message"))
 async def show_welcome_message(client, message: Message):
-  try:
-    user = await client.get_chat_member(message.chat.id, message.from_user.id)
-    if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
-      raise PermissionError("You are not allowed to use this command")
-    welcome_message = await db.get_welcome(message.chat.id, welcome_message)  
+    try:
+        user = await client.get_chat_member(message.chat.id, message.from_user.id)
+        if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
+            raise PermissionError("You are not allowed to use this command")
 
-    if welcome_message:
-      await message.reply_text(welcome_message)
-    else:
-      await message.reply_text("no welcome message for this chat")
+        # Retrieve the welcome message without passing it as an argument:
+        welcome_message = await db.get_welcome(message.chat.id)  # Corrected line
 
-  except Exception as e:
-      await message.reply_text(f"An error occurred: {e}")
+        if welcome_message:
+            await message.reply_text(welcome_message)
+        else:
+            await message.reply_text("no welcome message for this chat")
+
+    except Exception as e:
+            await message.reply_text(f"An error occurred: {e}")
+
 
 @Client.on_message(filters.command("welcome_message_remove"))
 async def remove_welcome(client, message: Message):
