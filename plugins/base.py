ded712fa3cc9582c73bad85d9399d9bce22e63a0
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-27 10:07:54 +0530

    Update base.py

--- plugins/base.py
+++ plugins/base.py
@@ -57,7 +57,7 @@ async def save_group(bot, message):
                     await (temp.MELCOW['welcome']).delete()
                 except:
                     pass
-            temp.MELCOW['welcome'] = await message.reply(text=script.WELCME_TXT.format(message.from_user.mention, today, time, message.chat.title))
+            temp.MELCOW['welcome'] = await message.reply(text=script.WELCME_TXT.format(message.from_user.mention, message.from_user.id,today, time, message.chat.title))
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
