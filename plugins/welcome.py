073c5d3cbc57969a6ed34b1cb2c8ae70eeab851d
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-27 09:42:54 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -11,10 +11,7 @@ welcome_messages = db["welcome_messages"]
 
 @Client.on_message(filters.command("w_help"))
 async def help(client, message):
-    client.send_message(chat_id=message.chat.id, text="Available commands:\n"
-                                                     "/set_welcome {welcome_message}\n"
-                                                     "/view_message\n"
-                                                     "/del_message")
+    await message.reply_text("Available commands:\n/set_welcome {welcome_message}\n/view_message\n/del_message")
 
 @Client.on_message(filters.command("set_welcome") & filters.group)
 async def set_welcome(client, message):
@@ -22,7 +19,7 @@ async def set_welcome(client, message):
     chat_id = message.chat.id
 
     welcome_messages.update_one({"chat_id": chat_id}, {"$set": {"message": welcome_message}}, upsert=True)
-    client.send_message(chat_id=chat_id, text="Welcome message set successfully!")
+    await client.send_message(chat_id=chat_id, text="Welcome message set successfully!")
 
 @Client.on_message(filters.command("view_message"))
 async def view_message(client, message):
@@ -30,15 +27,15 @@ async def view_message(client, message):
     stored_message = welcome_messages.find_one({"chat_id": chat_id})
 
     if stored_message:
-        client.send_message(chat_id=chat_id, text=stored_message["message"])
+        await client.send_message(chat_id=chat_id, text=stored_message["message"])
     else:
-        client.send_message(chat_id=chat_id, text="No welcome message set for this chat.")
+        await client.send_message(chat_id=chat_id, text="No welcome message set for this chat.")
 
 @Client.on_message(filters.command("del_message"))
 async def del_message(client, message):
     chat_id = message.chat.id
     welcome_messages.delete_one({"chat_id": chat_id})
-    client.send_message(chat_id=chat_id, text="Welcome message deleted successfully!")
+    await client.send_message(chat_id=chat_id, text="Welcome message deleted successfully!")
 
 @Client.on_message(filters.new_chat_members)
 async def welcome(client, message):
@@ -49,5 +46,5 @@ async def welcome(client, message):
             mention_format = f"<a href='tg://user?id={user.id}'>{user.full_name} ({user.id})</a>"
             welcome_text = stored_message["message"].format(mention_format=mention_format,
                                                            group_name=message.chat.title)
-            client.send_message(chat_id=message.chat.id, text=welcome_text)
+            await client.send_message(chat_id=message.chat.id, text=welcome_text)
 
