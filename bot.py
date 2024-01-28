9c1ebf47f7db49599742d2a1e6ef8c6b01283e67
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-28 08:55:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,15 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", 8080).start()
         logger.info("Running...")
         print(f"{me.first_name} | @{me.username} started...")
-       
+     if LOG_CHANNEL:
+         try:
+             curr = datetime.now(timezone("Asia/Kolkata"))
+             date = curr.strftime('%d %B, %Y')
+             time = curr.strftime('%I:%M:%S %p')
+             await self.send_message(LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")
+         except:
+             print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
+ 
     async def stop(self, *args):
        await super().stop()      
        print("Bot Restarting...")
