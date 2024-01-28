33f4168535e4283c28528feb9600b0cc061b9d85
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-28 10:49:53 +0530

    Update f_sub.py

--- plugins/f_sub.py
+++ plugins/f_sub.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from utils import not_subscribed 
 
-@Client.on_message(filters.create(not_subscribed))
+@Client.on_message(filters.create(not_subscribed) & ~filters.text)
 async def is_not_subscribed(client, message):
     buttons = [[ InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url=client.invitelink) ]]
     text = f"sᴏʀʀʏ {message.from_user.mention}\nʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ ʙᴏᴛ."
