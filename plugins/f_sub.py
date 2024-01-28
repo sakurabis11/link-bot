fe52de5aaf7b2cdf92b272a466761cfee12e763a
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-28 10:02:13 +0530

    Update f_sub.py

--- plugins/f_sub.py
+++ plugins/f_sub.py
@@ -4,6 +4,6 @@ from utils import not_subscribed
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
-    buttons = [[ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=client.invitelink) ]]
+    buttons = [[ InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url=client.invitelink) ]]
     text = f"sᴏʀʀʏ {message.from_user.mention}\nʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ ʙᴏᴛ."
     await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
