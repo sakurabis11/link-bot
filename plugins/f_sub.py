1a991c9ae30291b4169194568ada860cf9a5b473
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-30 01:20:40 +0530

    Update f_sub.py

--- plugins/f_sub.py
+++ plugins/f_sub.py
@@ -1,10 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from utils import not_subscribed
+import re
 
 @Client.on_message(filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
-    if message.text.startswith('/'):  
+    if message.text.startswith('/' and 'https?://.*instagram[^\s]+'): 
         buttons = [[InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url=client.invitelink)]]
         text = f"sᴏʀʀʏ {message.from_user.mention}\nʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ ʙᴏᴛ."
         await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
