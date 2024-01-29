f210ff931de9536febd1d82dd057e676233f2d40
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-30 01:25:32 +0530

    Update f_sub.py

--- plugins/f_sub.py
+++ plugins/f_sub.py
@@ -5,7 +5,7 @@ import re
 
 @Client.on_message(filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
-    if message.text.startswith('/' and 'https?://.*instagram[^\s]+'): 
+    if message.text.startswith('/'): 
         buttons = [[InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url=client.invitelink)]]
         text = f"sᴏʀʀʏ {message.from_user.mention}\nʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ ʙᴏᴛ."
         await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
