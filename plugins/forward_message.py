2ac7bd055617a5d1fafa6bbf3c6c9225f568f056
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-30 21:10:50 +0530

    Update forward_message.py

--- plugins/forward_message.py
+++ plugins/forward_message.py
@@ -4,24 +4,12 @@ from pyrogram.types import *
 
 OWNER_GID = -1004075001750
 
-@Client.on_message(filters.command("send") & filters.private)
+@Client.on_message(filters.command("send") & filters.private & filters.reply)
 async def send(client, message:Message):
-  msg = message.text.split()[1:]
-  if len(msg) == 1:
-    return await message.reply("**usage:**\n<code>/send (message)</code>")
-  msg = " ".join(msg)
-  await client.forward_messages(OWNER_GID, messages=msg)
-  await message.reply_text("**message sent**")
-
-@Client.on_message(filters.command("reply") & filters.chat(int(OWNER_GID)) & filters.reply)
-async def reply(client, message:Message):
-  rply = message.text.split()[1:]
-  user = message.reply_to_message.forward_from.id
-  if len(rply) == 1:
-    return await message.reply("**usage:**\n<code>/reply (message)</code>")
-  if not message.reply_to_message:
-    return await message.reply_text("**reply to forward message")
-  rply = " ".join(msg)
-  await client.send_message(user, rply)
-  
-  
+  if message.reply_to_message:
+     user_id = message.from_user.id
+     text = message.reply_to_message.text
+     await message.reply_to_message.forward(OWNER_GID, message=text)
+     await message.reply_text("message is sented, please wait for my admin to reply")
+  else:
+     await message.reply_text("reply to a message")
