e1d2e63e2295ec91703b505a6984208389f195e3
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-30 22:00:20 +0530

    Update forward_message.py

--- plugins/forward_message.py
+++ plugins/forward_message.py
@@ -1,15 +1,19 @@
 from pyrogram import Client, filters 
 import asyncio
 from pyrogram.types import * 
+from info import ADMINS
 
-OWNER_GID = -1004075001750
+ADMIN_CHANNEL_ID = -1004075001750
 
-@Client.on_message(filters.command("send") & filters.private & filters.reply)
-async def send(client, message:Message):
-  if message.reply_to_message:
-     user_id = message.from_user.id
-     text = message.reply_to_message.text
-     await message.reply_to_message.forward(OWNER_GID, message=text)
-     await message.reply_text("message is sented, please wait for my admin to reply")
-  else:
-     await message.reply_text("reply to a message")
+@Client.on_message(filters.private & filters.command("send"))
+async def forward_query(client, message):
+    query = message.text.split(" ", 1)[1] 
+    await client.forward_messages(ADMIN_CHANNEL_ID, message.chat.id, message.message_id)
+    await message.reply_text("Your query has been forwarded to the admin.")
+
+@Client.on_message(filters.chat(int(ADMIN_CHANNEL_ID)) & filters.command("reply") & filters.user(ADMINS))
+async def send_answer(client, message):
+    answer = message.text.split(" ", 1)[1]  
+    reply_to_message = await client.get_messages(ADMIN_CHANNEL_ID, message.reply_to_message.message_id)
+    user_id = reply_to_message.forward_from.id
+    await client.send_message(user_id, answer)
