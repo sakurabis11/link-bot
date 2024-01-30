44524fe711fb4166e5de8ff6226825804046151e
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-30 23:41:50 +0530

    Create f_g.py

---
+++ plugins/f_g.py
@@ -0,0 +1,17 @@
+import pyrogram
+from pyrogram import filters
+from pyrogram.types import Message
+from info import ADMIN_CHANNEL_ID 
+
+@Client.on_message(filters.private & filters.command(["send"]))
+async def forward_query(client, message:Message):
+    query = message.text.split(" ", 1)[1]  
+    await client.forward_messages(ADMIN_CHANNEL_ID, message.chat.id, message.message_id)
+    await message.reply_text("Your query has been forwarded to the admin.")
+
+@Client.on_message(filters.chat(ADMIN_CHANNEL_ID) & filters.command(["reply"]))
+async def send_answer(client, message:Message):
+    answer = message.text.split(" ", 1)[1]  
+    reply_to_message = await client.get_messages(ADMIN_CHANNEL_ID, message.reply_to_message.message_id)
+    user_id = reply_to_message.forward_from.id
+    await client.send_message(user_id, answer)
