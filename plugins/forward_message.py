58db93085a72c3c510bcfc3f0d97b1ccf43ca9f0
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-01-30 20:35:11 +0530

    Create forward_message.py

---
+++ plugins/forward_message.py
@@ -0,0 +1,27 @@
+from pyrogram import Client, filters 
+import asyncio
+from pyrogram.types import * 
+
+OWNER_GID = -1004075001750
+
+@Client.on_message(filters.command("send") & filters.private)
+async def send(client, message:Message):
+  msg = message.text.split()[1:]
+  if len(msg) == 1:
+    return await message.reply("**usage:**\n<code>/send (message)</code>")
+  msg = " ".join(msg)
+  await client.forward_messages(chat_id=OWNER_GID, messages=msg)
+  await message.reply("**message sent**")
+
+@Client.on_message(filters.command("reply") & filters.chat(int(OWNER_GID)) & filters.reply)
+async def reply(client, message:Message):
+  rply = message.text.split()[1:]
+  if len(rply) == 1:
+    return await message.reply("**usage:**\n<code>/reply (message)</code>")
+  if not message.reply_to_message:
+    return await message.reply("**reply to forward message")
+ user = message.reply_to_message.forward_from.id
+ rply = " ".join(msg)
+ await client.send_message(user, rply)
+  
+  
