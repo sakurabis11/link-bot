764dfda306ca1a592436d01944ad8d7b6de663c2
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-03 00:10:23 +0530

    Update testing.py

--- plugins/testing.py
+++ plugins/testing.py
@@ -8,7 +8,7 @@ async def forward_message_to_group(client, message):
     text = message.text.split(" ", 1)[1] 
     await message.forward(ADMIN_GROUP_ID)
     await message.reply_text("Message forwarded to the admins.")
- except Exception as e
+ except Exception as e:
     await message.reply_text(f"error{e}")
 
 @Client.on_message(filters.command("reply"))
@@ -17,5 +17,5 @@ async def reply_to_forwarded_message(client, message):
     reply_text = message.text.split(" ", 1)[1] 
     await message.reply_to_message.forward(message.reply_to_message.from_user.id)
     await app.send_message(message.reply_to_message.from_user.id, reply_text)
- except Exception as e
+ except Exception as e:
     await message.reply_text(f"error{e}")
