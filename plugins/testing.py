c35f57d0f1a47315ef8a05b3c97ba023a387e2ee
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-03 01:12:54 +0530

    Update testing.py

--- plugins/testing.py
+++ plugins/testing.py
@@ -2,11 +2,13 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 ADMIN_GROUP_ID = -1002059805189
+user_id = []
 
 @Client.on_message(filters.private & filters.command("send"))
 async def forward_message_to_group(client, message):
  try:
     text = message.text.split(" ", 1)[1] 
+    user_id = message.from_user.id
     await message.forward(ADMIN_GROUP_ID)
     await message.reply_text("Message forwarded to the admins.")
  except Exception as e:
@@ -15,8 +17,8 @@ async def forward_message_to_group(client, message):
 @Client.on_message(filters.command("reply"))
 async def reply_to_forwarded_message(client, message:Message):
  try:  
-    user_id = message.reply_to_message.forward_from_message_id
-    reply_text = message.text.split(" ", 1)[1]
+
+    reply_text = message.text.split(" ", 1)[1] 
     await message.reply_text(f"reply send to {user_id}")
  except Exception as e:
     await message.reply_text(f"error{e}")
