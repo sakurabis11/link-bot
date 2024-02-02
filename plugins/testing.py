4c4eca283e0173cc3cb5a1efab5eb09ba66be69f
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-03 00:17:27 +0530

    Update testing.py

--- plugins/testing.py
+++ plugins/testing.py
@@ -14,8 +14,10 @@ async def forward_message_to_group(client, message):
 @Client.on_message(filters.command("reply"))
 async def reply_to_forwarded_message(client, message):
  try:  
-    reply_text = message.text.split(" ", 1)[1] 
-    await message.reply_to_message.forward(message.reply_to_message.from_user.id)
-    await app.send_message(message.reply_to_message.from_user.id, reply_text)
+    user_id = message.reply_to_message.forward_from.id
+    name = message.reply_to_message.forward_from.first_name
+    reply_text = message.text.split(" ", 1)[1]
+    await client.send_message(user_id, reply_text)
+    await message.reply_text(f"reply send to {name}")
  except Exception as e:
     await message.reply_text(f"error{e}")
