40497401114689abb0ba79f609d3b4750d0c551d
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-03 23:11:03 +0530

    Update testing.py

--- plugins/testing.py
+++ plugins/testing.py
@@ -28,12 +28,12 @@ async def reply_to_forwarded_message(client, message: Message):
         if message.reply_to_message and message.reply_to_message.forward_from:
             msg_id = message.reply_to_message.forward_from.id
             reply_t = message.text.split(" ", 1)[1] 
-          await client.send_message(msg_id, reply_t)
+            await client.send_message(msg_id, reply_t)
         
         else:
-          await message.reply_text(f"Reply the forward message or Use this command: <code>!reply <user_id> <reply_message>")
+            await message.reply_text(f"Reply the forward message or Use this command: <code>!reply <user_id> <reply_message>")
     except Exception as e:
-          await message.reply_text(f"An error occurred: {e}\n\nIf there is an error then use this command: <code>!ans <user_id> <reply_message>")
+            await message.reply_text(f"An error occurred: {e}\n\nIf there is an error then use this command: <code>!ans <user_id> <reply_message>")
 
 @Client.on_message(filters.command("ans", "!") & filters.user(ADMINS) & filters.chat(int(ADMIN_GROUP_ID)))
 async def reply_to_forwarded_message(client, message:Message):
