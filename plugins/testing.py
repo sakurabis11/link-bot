0df75bd8eaffb48efcccdb0fdae226a77eadcdf6
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-03 22:19:29 +0530

    Update testing.py

--- plugins/testing.py
+++ plugins/testing.py
@@ -1,3 +1,6 @@
+# It is not a professional code, it is a basic idea from the mrtg brain.
+# Don't laugh, when u see this code, please!
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from info import ADMINS
@@ -10,8 +13,11 @@ async def forward_message_to_group(client, message):
     text = message.text.split(" ", 1)[1] 
     user_id = message.from_user.id
     await message.forward(ADMIN_GROUP_ID)
-    await client.send_message(ADMIN_GROUP_ID, text=f"<code>{user_id}</code>")
-    await message.reply_text("Message forwarded to the admins.")
+    await client.send_message(ADMIN_GROUP_ID, text=f"ᴜꜱᴇʀ-ɪᴅ= <code>{user_id}</code>")
+    success_message = await message.reply_text("Message forwarded to the admins.")
+    await asyncio.sleep(10)
+    await success_message.delete()
+    
  except Exception as e:
     await message.reply_text(f"error{e}")
 
@@ -22,4 +28,16 @@ async def reply_to_forwarded_message(client, message: Message):
          reply_t = message.text.split(" ", 1)[1]
          await client.send_message(msg_id, reply_t)
     except Exception as e:
-        await message.reply_text(f"An error occurred: {e}")
+        await message.reply_text(f"An error occurred: {e}\n\nIf there is an error then use this command: <code>!reply <user_id> <reply_message>")
+
+@Client.on_message(filters.command("reply", "!") & filters.user(ADMINS) & filters.chat(int(ADMIN_GROUP_ID)))
+async def reply_to_forwarded_message(client, message:Message):
+ try: 
+    mrtg = message.text.split(" ", 2)
+    user_id = int(mrtg[1])
+    reply_text = mrtg[2]
+    await client.send_message(user_id, text=f"<code>{reply_text}</code>")
+    await message.reply_text(f"reply send to {user_id}")
+ except Exception as e:
+    await message.reply_text(f"error{e}")
+
