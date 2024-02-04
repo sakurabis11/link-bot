1b5e344e10a8d660bd1f92778c637990f617c8ac
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-04 12:13:15 +0530

    Update contact_admin.py

--- plugins/contact_admin.py
+++ plugins/contact_admin.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 from info import ADMINS
 import asyncio
 
-ADMIN_GROUP_ID = -1002059805189
+ADMIN_GROUP_ID = 6645084082
 
 @Client.on_message(filters.private & filters.command("send"))
 async def forward_message_to_group(client, message):
@@ -19,14 +19,29 @@ async def forward_message_to_group(client, message):
  except Exception as e:
     await message.reply_text(f"error{e}")
 
-@Client.on_message(filters.command("ans", "!") & filters.user(ADMINS) & filters.chat(int(ADMIN_GROUP_ID)))
-async def reply_to_forwarded_message(client, message:Message):
- try: 
-    mrtg = message.text.split(" ", 2)
-    user_id = int(mrtg[1])
-    reply_text = mrtg[2]
-    await client.send_message(user_id, text=f"Rᴇᴘʟʏ ғʀᴏᴍ ᴍʏ ᴀᴅᴍɪɴ")
-    await client.send_message(user_id, text=f"<code>{reply_text}</code>")
-    await message.reply_text(f"ᴍᴇssᴀɢᴇ sᴇɴᴛ sᴜᴄᴇssғᴜʟʟʏ ᴛᴏ <a href='tg://user?id={user_id}'><b>ᴜsᴇʀ</b></a>")
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from info import ADMINS
+
+ADMIN_GROUP_ID = 6645084082
+
+@Client.on_message(filters.private & filters.command("send"))
+async def forward_message_to_group(client, message):
+ try:
+    text = message.text.split(" ", 1)[1] 
+    user_id = message.from_user.id
+    await message.forward(ADMIN_GROUP_ID)
+    await client.send_message(ADMIN_GROUP_ID, text=f"<code>{user_id}</code>")
+    await message.reply_text("Message forwarded to the admins.")
  except Exception as e:
     await message.reply_text(f"error{e}")
+
+@Client.on_message(filters.command("reply") & filters.user(ADMINS) & filters.private & filters.reply)
+async def reply_to_forwarded_message(client, message: Message):
+    try:
+            msg_id = message.reply_to_message.forward_from.id
+            reply_t = message.text.split(" ", 1)[1] 
+            await client.send_message(msg_id, reply_t)
+            await message.reply_text("suceess")
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
