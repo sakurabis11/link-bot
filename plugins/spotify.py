5d00ab9493a2caf4bad5b10b35401130a02837ab
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-16 16:31:37 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -31,7 +31,8 @@ def get_access_token():
     response = requests.post(url, headers=headers, data=data)
     return response.json()['access_token']
 
-async def download_songs(song_name, download_directory="."):
+async def download_songs(query, download_directory="."):
+  query = f"{query} Lyrics".replace(":", "").replace("\"", "")
   ydl_opts = {
       "format": "bestaudio/best",
       "default_search": "ytsearch",
@@ -47,7 +48,7 @@ async def download_songs(song_name, download_directory="."):
 
   with YoutubeDL(ydl_opts) as ydl:
       try:
-          video = ydl.extract_info(f"ytsearch:{song_name}", download=False)["entries"][0]["id"]
+          video = ydl.extract_info(f"ytsearch:{query}", download=False)["entries"][0]["id"]
           info = ydl.extract_info(video)
           filename = ydl.prepare_filename(info)
           if not filename:
@@ -65,27 +66,27 @@ async def spotify(client, message):
     access_token = get_access_token()
 
     # Get the song name or Spotify URL from the command
-    song_name_or_url = message.command[1:]
-    song_name_or_url = " ".join(song_name_or_url)
+    query = message.text.split(None, 1)[1]
 
-    # Check if the command argument is a Spotify URL
-    match = re.match(r'https://open\.spotify\.com/track/([a-zA-Z0-9]+)', song_name_or_url)
-    if match:
-        # If it is a Spotify URL, extract the song ID from the URL
-        song_id = match.group(1)
-    else:
         # If it is not a Spotify URL, search for the song on Spotify
-        song_name = song_name_or_url
-        url = f'https://api.spotify.com/v1/search?q={song_name}&type=album,track'
-        headers = {"Authorization": f"Bearer {access_token}"}
-        response = requests.get(url, headers=headers)
-        data = response.json()
+    song_name = query
+    url = f'https://api.spotify.com/v1/search?q={song_name}&type=album,track'
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
         # Get the first search result
-        item = data["tracks"]["items"][0]
+    item = data["tracks"]["items"][0]
 
         # Get the song ID
-        song_id = item["id"]
+    song_id = item["id"]
+
+    randomdir = f"/tmp/{str(random.randint(1, 100000000))}"
+    os.mkdir(randomdir)
+
+    path, info = await download_songs(query, randomdir)
+
+    song_caption = f"🍂 sᴜᴘᴘᴏʀᴛ: <a href='https://t.me/sd_bots'>sᴅ ʙᴏᴛs</a>" 
 
     # Get the song thumbnail and details from Spotify
     url = f'https://api.spotify.com/v1/tracks/{song_id}'
@@ -102,15 +103,8 @@ async def spotify(client, message):
     album = data["album"]["name"]
     release_date = data["album"]["release_date"]
 
-    randomdir = f"/tmp/{str(random.randint(1, 100000000))}"
-    os.mkdir(randomdir)
-  
-    path, info = await download_songs(song_name, randomdir)
-  
-    song_caption = f"🍂 sᴜᴘᴘᴏʀᴛ: <a href='https://t.me/sd_bots'>sᴅ ʙᴏᴛs</a>" 
-
     # Send the song thumbnail and details to the user
     await message.reply_photo(photo=thumbnail_url, caption=f"🎧 ᴛɪᴛʟᴇ: <code>{name}</code>\n🎼 ᴀʀᴛɪsᴛ: <code>{artist}</code>\n🎤 ᴀʟʙᴜᴍ: <code>{album}</code>\n🗓️ ʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ: <code>{release_date}</code>\n")
     await client.send_message(REQUESTED_CHANNEL, text=f"#sᴘᴏᴛꞮҒʏ\nʀᴇǫᴜᴇsᴛᴇᴅ ғʀᴏᴍ {message.from_user.mention}\nʀᴇǫᴜᴇsᴛ ɪs {song_name_or_url}")
 
-    await message.reply_audio(path, caption=song_caption, title=name, performer=artist, thumb=thumbnail_url)
+    await message.reply_audio(path, caption=song_caption, title=name)
