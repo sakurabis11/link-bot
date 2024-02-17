fe053b3c6df4def359885db6c5278c567f386a2a
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-18 00:06:58 +0530

    Create blacklist.py

---
+++ plugins/blacklist.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import asyncio
+
+
+lock_types=[]
+
+@Client.on_message(filters.command("lock") & filters.chat(int(-1002029706461)))
+async def lock(client, message: Message):
+    lock=message.split(" ", 1)
+    lock_types=lock
+    await message.reply_text(f"{lock_types} is added succesfully")
+
+@Client.on_message()
+async def chheck(client, message: Message):
+    if message.text == lock_types:
+      message.delete()
+      k=await message.reply_text(f"{message.from_user.mention} you are using blacklist word, so we delete that message")
+      asyncio(10)
+      k.delete()
+    return
