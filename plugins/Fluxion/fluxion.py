d0fc0ef42458b9d7cff0fa9a2ca7eb0ef1178214
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-17 12:15:59 +0530

    Update fluxion.py

--- plugins/Fluxion/fluxion.py
+++ plugins/Fluxion/fluxion.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import *
 
-Client.on_message = bot_message
+@Client.on_message = @bot_message
 filters.command = bot_command
 filters.group = group 
 message.reply_text = message
