95bc2b1d324bb919a4bfa2a3d59c24784682de3c
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-17 12:20:49 +0530

    Delete plugins/Fluxion/fluxion.py

--- plugins/Fluxion/fluxion.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters, enums
-from pyrogram.types import *
-
-@Client.on_message = @bot.message
-filters.command = bot.command
-filters.group = group 
-message.reply_text = message
-client.send_message = send_msg 
