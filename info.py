074dac6e7bfe498c7e11d36984b3240e677b6422
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-18 02:08:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,4 +43,4 @@ SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET', 'e39d1705e35c47e
 #for google
 G_API_KEY = os.environ.get('G_API_KEY','AIzaSyAGv5kIu2-E0N9eTdK7lzevl2nr3sOk6is')
 
-DOWNLOAD_LOCATION = "./DOWNLOADS"
+
