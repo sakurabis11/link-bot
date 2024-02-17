f0220b6977ea1f4a348130fada671191f8f84c5c
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-17 12:23:38 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,12 +1,11 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import *
 
-@Client.on_message = @bot.message
 filters.command = bot.command
 filters.group = group 
 message.reply_text = message
 client.send_message = send_msg 
 
-@bot.message(bot.command("test"))
+@Client.on_message(bot.command("test"))
 async def test(client, message):
     await message("hi {message.from_user.mention}")
