8d056cf7c0bf20bccf39668bc51117f2ea57cba1
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-17 12:21:14 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,5 +1,12 @@
-from plugins.Fluxion import fluxion
+from pyrogram import Client, filters, enums
+from pyrogram.types import *
 
-@bot_message(bot.command("test"))
+@Client.on_message = @bot.message
+filters.command = bot.command
+filters.group = group 
+message.reply_text = message
+client.send_message = send_msg 
+
+@bot.message(bot.command("test"))
 async def test(client, message):
     await message("hi {message.from_user.mention}")
