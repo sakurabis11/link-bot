cb22ba8caf5d57a869a7aeef57948c885b99f52e
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-19 01:11:43 +0530

    Update printrest.py

--- plugins/printrest.py
+++ plugins/printrest.py
@@ -19,3 +19,21 @@ async def download(client, message):
  except Exception as e:
         await message.reply_text(f"{e}")
 
+@Client.on_message(filters.command("imagine"))
+async def imagine(client, message):
+ try:
+    width = 500
+    height = 500
+    url = f"https://picsum.photos/{width}/{height}"
+    response = requests.get(url, stream=True)
+
+    if response.status_code == 200:
+        with open('image.jpg', 'wb') as f:
+            f.write(response.content)
+
+        await message.reply_photo(photo='image.jpg')
+    else:
+        await message.reply_text('Failed to generate image.')
+ except Exception as e:
+        await message.reply_text(f"{e}")
+
