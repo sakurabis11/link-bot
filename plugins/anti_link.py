25f657fa1ccb2a88de351cb6e8b8772c505ca708
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-19 20:51:12 +0530

    Create anti_link.py

---
+++ plugins/anti_link.py
@@ -0,0 +1,44 @@
+from pyrogram import Client, filters, enums
+from pymongo import MongoClient
+from info import DATABASE_URI, DATABASE_NAME
+
+client = pymongo.MongoClient(DATABASE_URI)
+db = client[DATABASE_NAME]
+linked_chats = db["linked_chats"]
+
+@Client.on_message(filters.command("antilink") & filters.group)
+async def antilink(client, message):
+    chat_id = message.chat.id
+    command = message.text.split()[1]
+    if command == "on":
+        if chat_id in linked_chats:
+            await message.reply_text("anti-link is already on")
+        else:
+            linked_chats.insert_one({"chat_id": chat_id, "enabled": True})
+            await message.reply_text("Anti-link is now enabled.")
+    if command == "off":
+        if chat_id not in linked_chats:
+            await message.reply_text("anti-link is already off")
+        else:
+            linked_chats.update_one({"chat_id": chat_id}, {"$set": {"enabled": False}})
+            await message.reply_text("Anti-link is now disabled.")
+    else:
+        await message.reply_text("Invalid command. Use /antilink on or /antilink off")
+
+@Client.on_message(filters.text & filters.group)
+async def link(client, message):
+  try:
+    msg = message.text
+    chat_id = message.chat.id
+    if chat_id in linked_chats:
+       if linked_chats.find_one({"chat_id": chat_id})["enabled"]:
+          if "https" in msg or "t.me/" in msg:
+              await msg.delete()
+              await message.reply_text("Links are not allowed in this group")
+          else:
+              pass
+       else:
+          pass 
+  except Exception as e:
+    await message.reply_text(f"{e}")
+                  
