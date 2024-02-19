a7dc3ed5219d5ca5a273639ce37dadf8372243be
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-19 13:02:28 +0530

    Update mrtg.py

--- plugins/mrtg.py
+++ plugins/mrtg.py
@@ -4,17 +4,15 @@ import os
 
 @Client.on_message(filters.command("mrtg"))
 async def download(client, message):
- try:
-    url = message.text.split()[1]
-    response = requests.get(url, stream=True)
-
-    if response.status_code == 200:
-        with open("path",'rb') as f:
-           f.write(get(link).content)
-
-        await message.reply_photo(photo='image.JPEG')
-        os.remove('image.JPEG')
-    else:
-        await message.reply_text('Failed to download image.')
- except Exception as e:
+    try:
+        url = message.text.split()[1]
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open("image.JPEG",'rb') as f:
+                f.write(get(link).content)
+            await message.reply_photo(photo='image.JPEG')
+            os.remove('image.JPEG')
+        else:
+            await message.reply_text('Failed to download image.')
+    except Exception as e:
         await message.reply_text(f"{e}")
