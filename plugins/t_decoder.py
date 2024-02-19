047252395ec698f68a542870a564a3aa97c9adec
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-19 15:17:11 +0530

    Update t_decoder.py

--- plugins/t_decoder.py
+++ plugins/t_decoder.py
@@ -4,34 +4,37 @@ import requests
 import os
 import time
 
-@Client.on_message(filters.regex(r"^https://telegra.ph/file/.*\.(jpg|png|jpeg|mp4)$"))
+@Client.on_message(filters.regex(r"^https://telegra.ph/file/"))
 async def download(client, message):
  try:
     url = message.text
     response = requests.get(url, stream=True)
+    if message.text.endswith('.jpg'):
 
-    if response.status_code == 200:
-        with open('image.JPEG', 'wb') as f:
-            f.write(response.content)
+        if response.status_code == 200:
+            with open('image.JPEG', 'wb') as f:
+                f.write(response.content)
 
-        await message.reply_photo(photo='image.JPEG')
-        os.remove('image.JPEG')
-    elif response.status_code == 200:
-        with open('video.mp4', 'wb') as f:
-            f.write(response.content)
+            await message.reply_photo(photo='image.JPEG')
+            os.remove('image.JPEG')
+    elif message.text.endswith('.mp4'):
+        if response.status_code == 200:
+            with open('video.mp4', 'wb') as f:
+                f.write(response.content)
             await message.reply_video(video='video.mp4')
             os.remove('video.mp4')
-    else:      
-        await message.reply_text('Failed to decode the telegraph link.')
+        else:      
+            await message.reply_text('Failed to decode the telegraph link.')
+    else:
+        return
  except Exception as e:
         await message.reply_text(f"{e}")
+
 
 @Client.on_message(filters.command("imagine"))
 async def imagine(client, message):
  try:
-    width = 500
-    height = 500
-    url = f"https://picsum.photos/{width}/{height}"
+    url = "https://api.waifu.pics/sfw/waifu"
     response = requests.get(url, stream=True)
 
     if response.status_code == 200:
@@ -43,4 +46,3 @@ async def imagine(client, message):
         await message.reply_text('Failed to generate image.')
  except Exception as e:
         await message.reply_text(f"{e}")
-
