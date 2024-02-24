f7ce3bb9d54d70f1d3dce3c3df36715a68ec19be
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-24 12:25:50 +0530

    Create google.py

---
+++ plugins/google.py
@@ -0,0 +1,19 @@
+from pyrogram import Client,filters
+import requests
+
+API_URL = "https://api.safone.dev/google"
+
+@Client.on_message(filters.command("google"))
+async def google_search(client, message):
+        query = message.text.split(" ", maxsplit=1)[1]
+        response = requests.get(f"{API_URL}?q={query}&limit=1").json()
+
+        search_results = response["results"]
+
+        for result in search_results:
+            title = result["title"]
+            link = result["link"]
+            snippet = result["snippet"]
+
+            message.reply_text(f"**Title:** {title}\n**Link:** {link}\n**Snippet:** {snippet}")
+
