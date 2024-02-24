36f48a913e528a30716582f65915630b3b6ae65f
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-24 23:23:41 +0530

    Update printerest.py

--- plugins/printerest.py
+++ plugins/printerest.py
@@ -1,7 +1,7 @@
 import os
+import re
 import wget
 import asyncio
-import requests
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
@@ -10,23 +10,11 @@ async def pinterest(client, message: Message):
     try:
         pint_url = message.text
         SD_BOTS = await message.reply_text("Downloading...")
-        
-        response = requests.get(pint_url)
-        media_url_match = re.search(r'"og:image" content="(.*?)"', response.text)
-        
-        if media_url_match:
-            media_url = media_url_match.group(1)
-            media_filename = wget.download(media_url)
-            
-            await asyncio.sleep(5)
-            await SD_BOTS.edit("Uploading...")
-            
-            # Send the downloaded media as a document
-            await client.send_document(message.chat.id, media_filename)
-            
-            await SD_BOTS.delete()
-            os.remove(media_filename)
-        else:
-            await SD_BOTS.edit("No media found.")
+        filename = wget.download(pint_url)
+        await asyncio.sleep(5)
+        await SD_BOTS.edit("Uploading...")        
+        await client.send_document(message.chat.id, filename)
+        await SD_BOTS.delete()
+        os.remove(filename)
     except Exception as e:
         await message.reply_text(f"{e}")
