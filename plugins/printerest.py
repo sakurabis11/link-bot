5997689e3a9b649c7693117f6ff25282c3c643a7
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-24 22:22:54 +0530

    Update printerest.py

--- plugins/printerest.py
+++ plugins/printerest.py
@@ -12,7 +12,7 @@ async def pinterest(client, message: Message):
      SD_BOTS = await message.reply_text("Downloading...")
      await asyncio.sleep(5)
      await SD_BOTS.edit("Uploading...")
-     await client.send_document(message.chat.id, pint_url)
+     await client.send_photo(message.chat.id, pint_url)
      await SD.delete()
      os.remove(thumbnail)
  except Exception as e:
