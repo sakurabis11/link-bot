b1596ff2956eef6faea253879de3a208f4c3074a
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-24 22:02:49 +0530

    Create printerest.py

---
+++ plugins/printerest.py
@@ -0,0 +1,18 @@
+import os
+import re
+import wget
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@Client.on_message(filters.regex(r"^https?://pin\.it/([^/?]+)"))
+async def pinterest(client, message: Message):
+ try:
+     pint_url = message.text
+     SD_BOTS = await message.reply_text("Downloading...")
+     await asyncio.sleep(5)
+     await SD_BOTS.edit("Uploading...")
+     await client.send_document(message.chat.id, pint_url)
+     await SD.delete()
+     os.remove(thumbnail)
+ except Exception as e:
+     await message.reply_text(f"{e}")
