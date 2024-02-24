4f59d6402ec289e60edffa8bd5ad768ff811bafe
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-24 21:31:10 +0530

    Update printerest.py

--- plugins/printerest.py
+++ plugins/printerest.py
@@ -6,34 +6,13 @@ from pyrogram.types import Message
 
 @Client.on_message(filters.regex(r"^https?://pin.it/([^/?]+"))
 async def pinterest(client, message: Message):
-    try:
-        match = re.match(r'^https?://pin.it/([^/?]+)', message.text)
-        if match:
-            pint_url = f"https://i.pinimg.com/originals/{match.group(1)}"  
-        else:
-            raise ValueError("Invalid Pinterest URL format")
-
-        download_message = await message.reply_text("Downloading...")
-
-
-        filename = os.path.basename(pint_url)  
-        try:
-            wget.download(pint_url, filename)
-        except Exception as e:
-            raise Exception(f"Download failed: {e}") from e
-
-        upload_message = await download_message.edit("Uploading...")
-        try:
-            await client.send_document(message.chat.id, filename)
-        except Exception as e:
-            raise Exception(f"Upload failed: {e}") from e
-
-        try:
-            os.remove(filename)
-        except Exception as e:
-            print(f"Failed to remove downloaded file: {e}")
-
-    except Exception as e:
-        error_message = f"An error occurred: {e}"
-        await message.reply_text(error_message)
-
+ try:
+     pint_url = message.text
+     SD_BOTS = await message.reply_text("Downloading...")
+     await asyncio.sleep(5)
+     await SD_BOTS.edit("Uploading...")
+     await client.send_document(message.chat.id, pint_url)
+     await SD.delete()
+     os.remove(thumbnail)
+ except Exception as e:
+     await message.reply_text(f"{e}")
