696711e6faa21628e0f5db7f2a66547ba29b7aa1
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-24 22:27:52 +0530

    Update printerest.py

--- plugins/printerest.py
+++ plugins/printerest.py
@@ -1,19 +1,26 @@
 import os
-import re
 import wget
 import asyncio
+import requests
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 @Client.on_message(filters.regex(r"^https?://pin\.it/([^/?]+)"))
 async def pinterest(client, message: Message):
- try:
-     pint_url = message.text
-     SD_BOTS = await message.reply_text("Downloading...")
-     await asyncio.sleep(5)
-     await SD_BOTS.edit("Uploading...")
-     await client.send_photo(message.chat.id, pint_url)
-     await SD.delete()
-     os.remove(thumbnail)
- except Exception as e:
-     await message.reply_text(f"{e}")
+    try:
+        pint_url = message.text
+        SD_BOTS = await message.reply_text("Downloading...")
+
+        response = requests.get(pint_url)
+        media_url = re.search(r'"og:image" content="(.*?)"', response.text).group(1)
+        media_filename = wget.download(media_url)
+        
+        await asyncio.sleep(5)
+        await SD_BOTS.edit("Uploading...")        
+      
+        await client.send_document(message.chat.id, media_filename)
+        
+        await SD_BOTS.delete()
+        os.remove(media_filename)
+    except Exception as e:
+        await message.reply_text(f"{e}")
