3d1b97870e1d2381f667330d807fb9096e307157
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-24 21:08:34 +0530

    Update printerest.py

--- plugins/printerest.py
+++ plugins/printerest.py
@@ -2,12 +2,14 @@ from pyrogram import Client, filters
 import os, wget
 import re
 
-@Client.on_message(filters.regex(r"^https://pin.it"))
+@Client.on_message(filters.regex(r'^https?://pin.it'))
 async def pinterest(client, message):
+ try:
     pint_url = messsage.text
     SD=await message.reply_text("Downloading...")
     wget.download(pint_url)
     sd=await SD.edit("Uploading...")
-    await client.send_photo(message.chat.id, photo=f"{pint_url}.jpg")
-    
-    
+    await client.send_document(message.chat.id,pint_url)
+ except Exception as e:
+     await message.reply_text(f"error occured: {e}")
+
