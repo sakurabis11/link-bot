688b65254c28fba353f58afbc9823aa9697b9526
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-25 21:35:17 +0530

    Update Google.py

--- plugins/Google.py
+++ plugins/Google.py
@@ -7,18 +7,20 @@ def convert_text(query):
 def google_search(query):
     encoded_query = convert_text(query)
     url = f"https://api.safone.dev/google?query={encoded_query}&limit=1"
-    if response == 200:
-      try:
-          response = requests.get(url)
-          response.raise_for_status() 
 
-          data = response.json()
-          return data["results"][0]["description"]
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
 
-      except requests.exceptions.RequestException as e:
-          print(f"Error fetching search results: {e}")
-          return f"An error occurred while fetching data. Please try again later.{e}"
-    pass  
+        data = response.json()
+        return data["results"][2]["description"]
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching search results: {e}")
+        return "An error occurred while fetching data. Please try again later."
+
+    except KeyError:
+        return "No description found for this query."
 
 @Client.on_message(filters.command("google"))
 async def handle_google_command(client, message):
@@ -33,4 +35,3 @@ async def handle_google_command(client, message):
 
     except Exception as e:
         await message.reply_text(f"An error occurred: {e}")
-
