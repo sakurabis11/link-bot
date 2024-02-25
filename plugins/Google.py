2903369af2b7bd4f93c05db82e03e8fe596e2b02
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-25 19:58:06 +0530

    Update Google.py

--- plugins/Google.py
+++ plugins/Google.py
@@ -9,13 +9,15 @@ def google_search(query):
 
 @Client.on_message(filters.command("google"))
 async def handle_google_command(client, message):
- try:
-    query = message.text.split()[1:]
-    if not query:
-        await message.reply_text("Please provide a search query.")
-        return
-        gurl =  " ".join(query).replace(" ", "%")
+    try:
+        query = message.text.split()[1:]
+        if not query:
+            await message.reply_text("Please provide a search query.")
+            return
+
+
+        gurl = " ".join(query).replace(" ", "%")
         title = google_search(gurl)
         await message.reply_text(title)
- except Exception as e:
+    except Exception as e:
         await message.reply_text(f"Error: {e}")
