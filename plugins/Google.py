f6bef8a9400e5f2a609828078f36cd69cb57ceb4
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-25 14:20:23 +0530

    Update Google.py

--- plugins/Google.py
+++ plugins/Google.py
@@ -9,6 +9,6 @@ async def google_search(client, message):
     search_url = f"https://api.safone.dev/google?query={url}&limit=1"
     response = requests.get(search_url)
     results = response.json()
-    await message.reply_text(f"Title: {result['title']}\n", parse_mode="HTML")
+    await message.reply_text(f"Title: {results['title']}\n", parse_mode="HTML")
  except Exception as e:
     await message.reply_text(f"{e}")
