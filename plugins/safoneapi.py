f30d6879bf5992bd10629360213aba8827176d5b
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-26 23:27:18 +0530

    Update safoneapi.py

--- plugins/safoneapi.py
+++ plugins/safoneapi.py
@@ -1,14 +1,13 @@
 import asyncio
 from pyrogram import Client, filters
 import requests
-import wget  # Added import
+import wget 
 
-@Client.on_message(filters.command("google"))
+@Client.on_message(filters.command("pic"))
 async def google_text(client, message):
     try:
         user_query = ' '.join(message.command[1:])
         encoded_query = user_query.replace(" ", "%20")
-        await message.reply_text(f"Query: {encoded_query}")
 
         response = requests.get(f"https://api.safone.dev/image?query={encoded_query}&limit=1")
         if response.status_code == 200:
