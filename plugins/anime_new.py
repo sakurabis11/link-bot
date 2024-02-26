0b21fd08b29e2bba755803bee2412d035c85859c
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-27 01:27:20 +0530

    Create anime_new.py

---
+++ plugins/anime_new.py
@@ -0,0 +1,20 @@
+import asyncio
+from pyrogram import Client, filters
+import requests
+import wget 
+
+@Client.on_message(filters.command("anime_news"))
+async def google_text(client, message):
+    try: 
+        response = requests.get(f"https://api.safone.dev/anime/news?limit=1")
+        if response.status_code == 200:
+            data = response.json()
+            image_data = data['results'][0]
+            text_data = image_data['description']
+            title_data = image_data['title']
+            image_url = image_data['imageUrl']
+            downloaded_image = wget.download(image_url)
+            await client.send_photo(message.chat.id, downloaded_image, caption=f"Title: {title_data}\n\nNews: {text_data}")
+
+    except Exception as e:
+       await message.reply_text(f"An error occurred: {e}")
