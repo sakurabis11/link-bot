42f86dc75200f921f0d67b8a87ac92ec8b78d8d1
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-26 23:14:55 +0530

    Update safoneapi.py

--- plugins/safoneapi.py
+++ plugins/safoneapi.py
@@ -7,14 +7,16 @@ async def google_text(client, message):
  try:
     query = ' '.join(message.command[1:])
     query = query.replace(" ","%20")
-    response = requests.get(f"https://api.safone.dev/google?query={query}&limit=1")
+    await message.reply_text(f"query")
+  
+    response = requests.get(f"https://api.safone.dev/image?query={query}&limit=1")
     if response.status_code == 200:
         data = response.json()
-        if data['total_count'] > 0:
-          g_result = data['items'][0]
-          reply = f"**🔖 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:** <code>{g_result['description']}</code>\n"
-          await message.reply_text(reply)
+        g_result = data['items'][0]
+        reply = f"{g_result['imageUrl']}"
+        photo = wget.download(reply)
+        await client.send_message(message.chat.id, photo)
           
  except Exception as e:
-          await message.reply_text(f"{e}")
+        await message.reply_text(f"{e}")
   
