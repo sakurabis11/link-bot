6d6c9448a4132b5aa7a105bbd60cae91c0933b60
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-26 23:34:33 +0530

    Update cc_gen.py

--- plugins/cc_gen.py
+++ plugins/cc_gen.py
@@ -5,7 +5,7 @@ import requests
 @Client.on_message(filters.command("cc"))
 async def google_text(client, message):
     try:
-        user_query = ' '.join(message.command[1:])
+        user_query = message.text.split()[1:]
 
         response = requests.get(f"https://api.safone.dev/ccgen?bins={user_query}&limit=1")
         if response.status_code == 200:
