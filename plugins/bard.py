7608da0c8cfa82c55ca14ea6de97187097c68254
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-28 00:33:14 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -14,7 +14,7 @@ async def google_text(client, message):
         response = requests.get(f"https://api.safone.dev/bard?message={encoded_query}")
         if response.status_code == 200:
             data = response.json()
-            bard = data['candidates'][0]
+            bard = data['parts'][0]
             bar_d = bard['text']
             await client.send_message(message.chat.id, bar_d)
             await client.send_message(REQUESTED_CHANNEL, text=f"#ᴄᴄ_ɢᴇɴ\nʜᴇʏ {message.from_user.mention}\nʀᴇǫᴜᴇsᴛ ɪs {user_query}")
