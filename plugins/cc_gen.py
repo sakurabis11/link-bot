f2e4814e1477da237ac30b6a4380f89e8f37dd46
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-27 07:56:41 +0530

    Update cc_gen.py

--- plugins/cc_gen.py
+++ plugins/cc_gen.py
@@ -7,8 +7,8 @@ async def google_text(client, message):
     try:
         user_query = message.text.split()[1:]
         if not user_query:
-        await message.reply_text("Enter a four digit of bin to create the cc") 
-
+          await message.reply_text("Enter a four digit of bin to create the cc") 
+          return
         response = requests.get(f"https://api.safone.dev/ccgen?bins={user_query}&limit=1")
         if response.status_code == 200:
             data = response.json()
