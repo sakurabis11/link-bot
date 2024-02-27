bb0e0a9dcb3e0a585e67c2c869ce4725fd39713b
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-27 08:26:35 +0530

    Update anime_new.py

--- plugins/anime_new.py
+++ plugins/anime_new.py
@@ -15,6 +15,7 @@ async def google_text(client, message):
             image_url = image_data['imageUrl']
             downloaded_image = wget.download(image_url)
             await client.send_photo(message.chat.id, downloaded_image, caption=f"Title: {title_data}\n\nNews: {text_data}")
+            await client.send_message(REQUESTED_CHANNEL, text=f"#ᴀɴɪᴍᴇ_ɴᴇᴡs\nʀǫᴜᴇsᴛᴇᴅ: {message.from_user.mention}")
 
     except Exception as e:
        await message.reply_text(f"An error occurred: {e}")
