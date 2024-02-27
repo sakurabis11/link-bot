0da0a58001c2b526ae0e2a1cf47dd0801517303f
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-02-27 08:46:42 +0530

    Update torrent.py

--- plugins/torrent.py
+++ plugins/torrent.py
@@ -1,11 +1,15 @@
 import asyncio
 from pyrogram import Client, filters
 import requests
+from info import REQUESTED_CHANNEL
 
 @Client.on_message(filters.command("torent"))
 async def google_text(client, message):
     try:
         user_query = message.text.split()[1:]
+        if not user_query:
+            await message.reply_text("please provide a movie name, <code>/torent your name</code>")
+            return 
         encoded_query = " ".join(user_query).replace(" ", "")
 
         response = requests.get(f"https://api.safone.dev/torrent?query={encoded_query}&limit=1")
@@ -20,6 +24,6 @@ async def google_text(client, message):
                   f"**📡 magnetLink:** <code>{torrent_r['magnetLink']}</code>"
     
             await client.send_message(message.chat.id, tor)
-
+            await client.send_message(REQUESTED_CHANNEL, text=f"#ᴛᴏʀᴇɴᴛ\nʜᴇʏ {message.from_user.mention}\nʀᴇǫᴜᴇsᴛ ɪs {user_query}")
     except Exception as e:
        await message.reply_text(f"An error occurred: {e}")
