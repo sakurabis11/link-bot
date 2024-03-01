1a24bebcf0d6414255fa91eb0c2e5f42a55bc9ae
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-01 23:04:36 +0530

    Create google_search.py

---
+++ plugins/google_search.py
@@ -0,0 +1,23 @@
+import asyncio
+from pyrogram import Client, filters
+import requests
+from info import REQUESTED_CHANNEL
+
+@Client.on_message(filters.command("google"))
+async def google_text(client, message):
+    try:
+        user_query = message.text.split()[1:]
+        if not user_query:
+          await message.reply_text("provide a query") 
+          return
+        encoded_query = user_query.replace(" ", "%20")
+        response = requests.get(f"https://api.safone.dev/google?query={encoded_query}&limit=1")
+        if response.status_code == 200:
+            data = response.json()
+            google_re = data['results'][0]
+            res = google_re['description']
+            await client.send_message(message.chat.id, res)
+            await client.send_message(REQUESTED_CHANNEL, text=f"#ᴄᴄ_ɢᴇɴ\nʜᴇʏ {message.from_user.mention}\nʀᴇǫᴜᴇsᴛ ɪs {user_query}")
+
+    except Exception as e:
+       await message.reply_text(f"An error occurred: {e}")
