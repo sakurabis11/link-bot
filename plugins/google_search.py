3e85a00b52710911f7c14169141b4f32cf7d7a76
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-01 23:13:01 +0530

    Update google_search.py

--- plugins/google_search.py
+++ plugins/google_search.py
@@ -14,9 +14,8 @@ async def google_text(client, message):
         response = requests.get(f"https://api.safone.dev/google?query={encoded_query}&limit=1")
         if response.status_code == 200:
             data = response.json()
-            google_re = data['results'][1]
-            res = google_re['description']
-            await client.send_message(message.chat.id, res)
+            google_result = data["results"][0]["description"]
+            await client.send_message(message.chat.id, google_result)
             await client.send_message(REQUESTED_CHANNEL, text=f"#google_result\nʜᴇʏ {message.from_user.mention}\nʀᴇǫᴜᴇsᴛ ɪs {user_query}")
 
     except Exception as e:
