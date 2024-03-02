dc48e9863812c2d8e041259063abd8717e3b2b92
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-02 19:22:09 +0530

    Create ai_vision.py

---
+++ plugins/ai_vision.py
@@ -0,0 +1,50 @@
+rom pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+import requests
+from info import REQUESTED_CHANNEL
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyD214hhYJ-xf8rfaWX044_g1VEBQ0ua55Q")
+
+@Client.on_message(filters.command("vision") & filters.reply)
+async def vision(client, message):
+    pic = message.reply_to_photo
+    if not pic:
+      return await message.reply_text("Reply to a photo")
+    media = await pic.download()
+
+    generation_config = {
+        "temperature": 0.4,
+        "top_p": 1,
+        "top_k": 32,
+        "max_output_tokens": 4096,
+    }
+
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+    ]
+
+
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro-vision-latest",
+        generation_config=generation_config,
+        safety_settings=safety_settings
+    )
+
+    prompt_parts = [media,"what is the picture shows",]
+    response = model.generate_content(prompt_parts)
+    await message.reply_text(response.text)
