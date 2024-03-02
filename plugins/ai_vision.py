ed0754828fe5e179dc058dd6a30ffb4a1810f61b
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-02 20:32:30 +0530

    Update ai_vision.py

--- plugins/ai_vision.py
+++ plugins/ai_vision.py
@@ -1,22 +1,21 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import requests
+import os
 import google.generativeai as genai
+from pathlib import Path
 
 genai.configure(api_key="AIzaSyD214hhYJ-xf8rfaWX044_g1VEBQ0ua55Q")
 
-@Client.on_message(filters.command("vision") & filters.reply)
+@Client.on_message(filters.command("vision") & filters.photo)
 async def vision(client, message: Message):
     try:
-        pic = message.reply_to_message.photo
+        photo_path = await message.download_photo()
 
-        if not message.reply_to_message:
-            return await message.reply_text("Reply to a photo with the /vision command.")
-        if not message.reply_to_message.photo:
-            return await message.reply_text("Reply to a photo with the /vision command.")
-        media = await client.download_media(pic)
-        if not media:
-            return await message.reply_text("Failed to download the image. Please try again.")
+        if not os.path.exists(photo_path):
+            await message.reply_text("Failed to download the photo. Please try again.")
+            return
+
 
         generation_config = {
             "temperature": 0.4,
@@ -48,10 +47,13 @@ async def vision(client, message: Message):
                                      generation_config=generation_config,
                                      safety_settings=safety_settings
                                      )
-
-        prompt_parts = [media, "what is the picture shows"]
+        image_data = {
+            "mime_type": "image/jpeg",
+            "data": open(photo_path, "rb").read(),
+        }
+        prompt_parts = ["what is the picture shows", image_data]
         response = model.generate_content(prompt_parts)
         await message.reply_text(response.text)
-
+        os.remove(photo_path)
     except Exception as e:
         await message.reply_text(f"{e}")
