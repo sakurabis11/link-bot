df22b74df3afcc2f5c961099e6c2d66300fb2b1c
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-03 22:33:16 +0530

    Create chatbot.py

---
+++ plugins/chatbot.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, filters
+from utils import temp
+
+@Client.on_messag(filters.command("chatbot"))
+async def chatbot(client, message):
+  user_id = message.from_user.id
+    try:
+        user_query = message.text.split()[1:]
+        if not user_query:
+            await message.reply_text("please provide a movie name, <code>/torent your name</code>")
+            return 
+        encoded_query = " ".join(user_query).replace(" ", "")
+        response = requests.get(f"https://api.safone.dev/chatbot?query={encoded_query}&user_id={user_id}&bot_name={temp.U_NAME}&bot_master={}")
+        if response.status_code == 200:
+            data = response.json()
+            response = data["response"][0]
+    
+            await client.send_message(message.chat.id, response)
+    except Exception as e:
+            await message.reply_text(f"{e}")
+        
