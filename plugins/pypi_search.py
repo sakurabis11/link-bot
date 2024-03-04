cf83ecbde995c778cbd0b9354e84cdd59644e70e
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-04 22:43:46 +0530

    Create pypi_search.py

---
+++ plugins/pypi_search.py
@@ -0,0 +1,26 @@
+import asyncio
+from pyrogram import Client, filters
+import requests
+from info import REQUESTED_CHANNEL
+
+async def get_package(encoded_query):
+   response = requests.get(f"https://pypi.org/pypi/{user_query}/json")
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("info", {}).get("version")
+    else:
+        return None
+
+@Client.on_message(filters.command("pypi"))
+async def google_text(client, message):
+    try:
+        user_query = message.text.split()[1:]
+        if not user_query:
+          await message.reply_text("Enter a package name") 
+          return  
+        encoded_query = " ".join(user_query).replace(" ", "")
+        version = await get_package(encoded_query)
+        if version:
+          await message.reply_text(f"Package: {package_name}\nVersion: {version}")
+        else:
+          await message.reply_text(f"Package '{package_name}' not found on PyPI.")
