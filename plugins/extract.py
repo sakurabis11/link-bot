136f9c418246fdf4ea892aeae9ccd87cdaed76b0
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-11 00:11:11 +0530

    Update extract.py

--- plugins/extract.py
+++ plugins/extract.py
@@ -6,6 +6,7 @@ from typing import Tuple
 import asyncio
 from pyrogram import filters, Client
 from pyrogram.types import Message
+from info import REQUESTED_CHANNEL
 
 async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
     """run command in terminal"""
@@ -35,6 +36,7 @@ async def shazam_(client, message):
         if not message.reply_to_message or not message.reply_to_message.video:
             return await message.reply_text("Reply to a video...")
         thumbnail = wget.download("https://telegra.ph/file/f4f20a3a7b15d588fcc2a.jpg")
+        sd = await client.send_message(REQUESTED_CHANNEL, text=f"#ᴠɪᴅ_ᴛᴏ_ᴀᴜᴅ\n\nʀᴇǫᴜᴇsᴛᴇᴅ ғʀᴏᴍ {message.from_user.mention}\n\nᴀᴜᴅɪᴏ: ❌")
         stime = time.time()
         msg = await message.reply_text("Cᴏɴᴠᴇʀᴛɪɴɢ ᴠɪᴅᴇᴏ ᴛᴏ ᴀᴜᴅɪᴏ...\n\nIᴛ ᴍᴀʏ ᴄᴀᴜsᴇs sᴏᴍᴇ ᴛɪᴍᴇ ᴅᴜᴇ ᴛᴏ ᴠɪᴅᴇᴏ ᴅᴜʀᴀᴛɪᴏɴ, sᴏ ᴘʟᴇᴀsᴇ ᴡ𝟾")
         video_file = await message.reply_to_message.download()
@@ -44,6 +46,7 @@ async def shazam_(client, message):
         etime = time.time()
         t_k = round(etime - stime)
         await message.reply_audio(music_file, thumb=thumbnail)
+        await sd.edit("#ᴠɪᴅ_ᴛᴏ_ᴀᴜᴅ\n\nʀᴇǫᴜᴇsᴛᴇᴅ ғʀᴏᴍ {message.from_user.mention}\n\nᴀᴜᴅɪᴏ: ✅")
         t_taken = await message.reply_text(f"<code>{t_k} Sᴇᴄᴏɴᴅs ғᴏʀ ᴄᴏɴᴠᴇʀᴛɪɴɢ ᴛʜɪs ᴠɪᴅᴇᴏ ᴛᴏ ᴀᴜᴅɪᴏ...</code>")
         await asyncio.sleep(10)
         await t_taken.delete()
