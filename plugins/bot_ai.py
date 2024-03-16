ed3f72f2e0a37bd24d4cc2ed1d2efed78c8638ba
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-16 22:58:44 +0530

    Update bot_ai.py

--- plugins/bot_ai.py
+++ plugins/bot_ai.py
@@ -3,30 +3,38 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 import os
 from os import environ
+
 EVAL_ID = int(os.environ.get("EVAL_ID", "-1002002636126"))
 
 @Client.on_message(filters.text & filters.chat(int(EVAL_ID)))
 async def echo(client: Client, message: Message):
-  try:
-      msg = message.text
-      bot_id = 6645084082
-      user_id = message.reply_to_message.from_user.id
-      if user_id == bot_id:
-        if msg == "Hi":
-          await message.reply_text(f"Hello {message.from_user.mention}")
-        elif msg == "Bye":
-          await message.reply_text(f"Bye {message.from_user.mention}")
-        elif msg == "How are you":
-          await message.reply_text("I am fine, and you?")
-        elif msg == "I am fine":
-          await message.reply_text("Do u use my features")
-        elif msg == "yes":
-           await message.reply_text("Thanks for using my feature")
-        elif msg == "No":
-           await message.reply_text("use it click /help")
+    try:
+        msg = message.text
+        bot_id = 6645084082
+        if message.reply_to_message is not None:
+            user_id = message.reply_to_message.from_user.id
         else:
-          pass
-      else:
-          pass
-  except Exception as e:
-          await message.reply_text(f"{e}")
+            pass
+
+        # Option 2: Use effective_user (For Pyrogram v20 or later)
+        # user_id = message.effective_user.id  # Uncomment this for Pyrogram v20+
+
+        if user_id == bot_id:
+            if msg == "Hi":
+                await message.reply_text(f"Hello {message.from_user.mention}")
+            elif msg == "Bye":
+                await message.reply_text(f"Bye {message.from_user.mention}")
+            elif msg == "How are you":
+                await message.reply_text("I am fine, and you?")
+            elif msg == "I am fine":
+                await message.reply_text("Do u use my features")
+            elif msg == "yes":
+                await message.reply_text("Thanks for using my feature")
+            elif msg == "No":
+                await message.reply_text("use it click /help")
+            else:
+                pass
+        else:
+            pass
+    except Exception as e:
+        await message.reply_text(f"{e}")
