1a1acdaed7b22c161e2870a6260825f4e0b5704d
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-16 22:54:18 +0530

    Update bot_ai.py

--- plugins/bot_ai.py
+++ plugins/bot_ai.py
@@ -9,7 +9,7 @@ EVAL_ID = int(os.environ.get("EVAL_ID", "-1002002636126"))
 async def echo(client: Client, message: Message):
   try:
       msg = message.text
-      bot_id = "6645084082"
+      bot_id = 6645084082
       user_id = message.reply_to_message.from_user.id
       if user_id == bot_id:
         if msg == "Hi":
