a2c87f511337509db18ab0ae436099fd538f7178
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-16 23:22:14 +0530

    Update bot_ai.py

--- plugins/bot_ai.py
+++ plugins/bot_ai.py
@@ -6,29 +6,42 @@ from os import environ
 
 EVAL_ID = int(os.environ.get("EVAL_ID", "-1002002636126"))
 
-@Client.on_message(filters.text & filters.chat(int(EVAL_ID)))
+@Client.on_message(filters.text & filters.private)
 async def echo(client: Client, message: Message):
     try:
         msg = message.text
-        bot_id = 6645084082
-
-        user_id = message.effective_user.id  
-
-        if user_id == bot_id:
-            if msg == "Hi":
-                await message.reply_text(f"Hello {message.from_user.mention}")
-            elif msg == "Bye":
-                await message.reply_text(f"Bye {message.from_user.mention}")
-            elif msg == "How are you":
-                await message.reply_text("I am fine, and you?")
-            elif msg == "I am fine":
-                await message.reply_text("Do u use my features")
-            elif msg == "yes":
-                await message.reply_text("Thanks for using my feature")
-            elif msg == "No":
-                await message.reply_text("use it click /help")
-            else:
-                pass
+        if msg == "Hi":
+            await message.reply_text(f"Hello {message.from_user.mention}")
+        elif msg == "Bye":
+            await message.reply_text(f"Bye {message.from_user.mention}")
+        elif msg == "How are you":
+            await message.reply_text("I am fine, and you?")
+        elif msg == "I am fine":
+            await message.reply_text("Do u use my features")
+        elif msg == "yes":
+            await message.reply_text("Thanks for using my feature")
+        elif msg == "No":
+            await message.reply_text("use it click /help")
+        elif msg == "ok i will try":
+            await message.reply_text("mm")
+        elif msg == "who is your owner":
+            await message.reply_text(f"<a href='tg://user?id=1342641151><b> <^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> </b></a>")
+        elif msg == "who is ur owner":
+            await message.reply_text(f"<a href='tg://user?id=1342641151><b> <^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> </b></a>") 
+        elif msg == "which model are you using ?":
+            await message.reply_text("<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> is made me, i think you should ask him.")
+        elif msg == "which model are you using?":
+            await message.reply_text("<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> is made me, i think you should ask him.")
+        elif msg == "which ai are you using ?":
+            await message.reply_text("<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> is made me, i think you should ask him.")
+        elif msg == "which ai are you using?":
+            await message.reply_text("<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> is made me, i think you should ask him.")    
+        elif msg == "which ai model are you using ?":
+            await message.reply_text("<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> is made me, i think you should ask him.")
+        elif msg == "which ai model are you using?":
+            await message.reply_text("<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> is made me, i think you should ask him.")            
+        else:
+            pass
         else:
             pass
     except Exception as e:
