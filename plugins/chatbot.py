da11e37e182f0538e3ca3792a100e3e94e7a2158
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-03-19 00:07:20 +0530

    Update chatbot.py

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -29,41 +29,42 @@ async def echo(client, message: Message):
      if not msg:
        await message.reply_text("provide a input")
        return
+     msg = msg.replace(" ","_")
 
      if ("hi" or "hii" or "hiii" or "hello" or "hellu" or "helo") in msg:
        await message.reply_text(f"Hello {message.from_user.mention}")
-     elif ("good morning" or "morning" or "mrng") in msg:
+     elif ("good_morning" or "morning" or "mrng") in msg:
        await message.reply_text(f"good morning {message.from_user.mention}")
-     elif ("good evening" or "evening" or "evning") in msg:
+     elif ("good_evening" or "evening" or "evning") in msg:
        await message.reply_text(f"good morning {message.from_user.mention}")
-     elif ("how are you" or "how are u" or "how are you today") in msg:
+     elif ("how_are_you" or "how_are_u" or "how_are_you_today") in msg:
        await message.reply_text("I am just a telegram bot, i don't have any feelings. How are you?")    
-     elif ("i am fine" or "fine" or "good day") in msg:
+     elif ("i_am_fine" or "fine" or "good_day") in msg:
        await message.reply_text("Oh great\n\nDo u use my features?")
-     elif ("yes" or "yup" or "i used the features") in msg:
+     elif ("yes" or "yup" or "i_used_the_features") in msg:
        await message.reply_text("Ch nice, thank u for using me.")
      elif ("no" or "nope" or "nop") in msg:
        await message.reply_text("Click /help to use my features.")  
-     elif ("who is your owner" or "owner name") in msg:
+     elif ("who_is_your_owner" or "owner_name") in msg:
        await message.reply_text("@MrTG_Coder, He is the owner.")
-     elif ("which model are you using" or "which ai model are you using" or "which ai is used for chatbot") in msg:
+     elif ("which_model_are_you_using" or "which_ai_model_are_you_using" or "which_ai_is_used_for_chatbot") in msg:
        await message.reply_text("<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </> is made me, i think you should ask him.")
-     elif ("what is your name" or "ur name" or "your name") in msg:
+     elif ("what_is_your_name" or "ur_name" or "your_name") in msg:
        await message.reply_text("My name is ᴏʙᴀɴᴀɪ")  
-     elif ("how to deploy a telegram bot" or "how to deploy a bot" ) in msg:
+     elif ("how_to_deploy_a_telegram_bot" or "how_to_deploy_a_bot" ) in msg:
        await message.reply_text("Ask him: @MrTG_Coder")
-     elif ("can u send ur repo" or "can you send your repo") in msg:
+     elif ("can_u_send_ur_repo" or "can_you_send_your_repo") in msg:
        await message.reply_text("yup, why not repo:- https://github.com/MrTG-CodeBot/Obanai", disable_web_page_preview=True)
-     elif ("which server are you using" or "which platform is you were deployed") in msg:
+     elif ("which_server_are_you_using" or "which_platform_is_you_were_deployed") in msg:
        await message.reply_text("Idk")  
-     elif ("are you advance chatbot" or "you are a advance chatbot") in msg:
+     elif ("are_you_advance_chatbot" or "you_are_a_advance_chatbot") in msg:
        await message.reply_text("Nope, i upgrading all day by day")
-     elif ("can u send the uptime of urs" or "can u send the uptime of yours") in msg:
+     elif ("can_u_send_the_uptime_of_urs" or "can_u_send_the_uptime_of_yours") in msg:
        uptime = format_uptime(time.time() - psutil.boot_time())
        await message.reply_text(f"Uᴩᴛɪᴍᴇ: <code>{uptime}</code>")
-     elif ("can you generate images" or "do you generate images by text") in msg:
+     elif ("can_you_generate_images" or "do_you_generate_images_by_text") in msg:
        await message.reply_text("Nope")  
-     elif ("how many users are using this bot" or "how many users do you have" or "group") in msg:
+     elif ("how_many_users_are_using_this_bot" or "how_many_users_do_you_have" or "group") in msg:
        users = await db.total_users_count()
        chats = await db.total_chat_count()
        await message.reply_text(text=script.STATUS_TXT.format(users, chats))
