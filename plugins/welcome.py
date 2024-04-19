ecbae9ae4101559c755cd8ffca3aa61d070ff99a
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-04-19 20:34:46 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,4 +1,4 @@
-import pyrogram 
+import pyrogram
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from info import EVAL_ID, ADMINS
@@ -6,29 +6,37 @@ from info import EVAL_ID, ADMINS
 welcome_msg = []
 
 @Client.on_message(filters.command("set_welcome") & filters.user(EVAL_ID))
-async def setw_elcome(client, message):
- user_id = message.from_user.id
- if user_id in ADMINS:
-   welcome_msg = message.text.split()[1:]
-   await message.reply_text("set welcome message ✅")
- else:
-   await message.reply_text(f"You are not admin")
+async def set_welcome(client, message):
+    user_id = message.from_user.id
+    try:
+        if user_id in ADMINS:
+            welcome_msg = message.text.split()[1:]
+            await message.reply_text("Set welcome message ✅")
+        else:
+            await message.reply_text(f"You are not an admin")
+    except IndexError:
+        await message.reply_text("Invalid command format. Usage: /set_welcome <message>")
+
 
 @Client.on_message(filters.command("see_welcome"))
-async def seew_elcome(client, message):
-  if welcome_msg is None:
-    await message.reply_text("The welcome message is none")
-  else:
-    await message.reply_text(welcome_msg)
+async def see_welcome(client, message):
+    try:
+        if welcome_msg is None:
+            await message.reply_text("The welcome message is not set.")
+        else:
+            await message.reply_text(welcome_msg)
+    except Exception as e:  
+        await message.reply_text(f"An error occurred: {e}")
+
 
 @Client.on_message(filters.command("delete_welcome") & filters.user(EVAL_ID))
-async def setw_elcome(client, message):
- user_id = message.from_user.id
- if user_id in ADMINS:
-   if welcome_msg is None:
-     await message.reply_text("welcome message is None")
-   else:
-     welcome_msg = None
-     await message.reply_text("delete welcome message ✅")
- else:
-   await message.reply_text(f"You are not admin")
+async def delete_welcome(client, message):
+    user_id = message.from_user.id
+    try:
+        if user_id in ADMINS:
+            welcome_msg = None
+            await message.reply_text("Welcome message deleted ✅")
+        else:
+            await message.reply_text(f"You are not an admin")
+    except Exception as e:  
+        await message.reply_text(f"An error occurred: {e}")
