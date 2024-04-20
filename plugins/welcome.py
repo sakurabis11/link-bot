12b331cc7034dcc0a9e2faf522fb90501066faf4
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-04-20 10:47:42 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -20,14 +20,13 @@ async def set_welcome(client, message):
 @Client.on_message(filters.command("see_welcome") & filters.chat(int(EVAL_ID)))
 async def see_welcome(client, message):
   try:
-    if welcome_msg is None:
-      await message.reply_text("The welcome message is not set.")
-    else:
+    if welcome_msg and welcome_msg.strip():
       user_input = " ".join(welcome_msg)
       await client.send_message(message.chat.id, text=f"{user_input}")
+    else:
+      await message.reply_text("The welcome message is not set.")
   except Exception as e:
     await message.reply_text(f"An error occurred: {e}")
-
 
 @Client.on_message(filters.command("delete_welcome") & filters.chat(int(EVAL_ID)))
 async def delete_welcome(client, message):
