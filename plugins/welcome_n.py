aa684539722d05871804211ce7d053ef26c2c707
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-04-20 23:27:19 +0530

    Update welcome_n.py

--- plugins/welcome_n.py
+++ plugins/welcome_n.py
@@ -1,12 +1,12 @@
-
+from info import DATABASE_URI, ADMINS
 import asyncio
 from pyrogram import Client, filters
 from pymongo import MongoClient
 
-mongo_client = MongoClient("mongodb://localhost:27017")
+mongo_client = MongoClient(DATABASE_URI)
 db = mongo_client.welcome_bot
 
-@Client.on_message(filters.command("set_welcome"))
+@Client.on_message(filters.command("set_welcome") & filters.user(ADMINS))
 async def set_welcome(client, message):
     try:
         welcome_message = message.text.split(" ", maxsplit=1)[1]
@@ -15,7 +15,7 @@ async def set_welcome(client, message):
     except IndexError:
         await message.reply_text("Usage: /set_welcome ")
 
-@Client.on_message(filters.command("view_welcome"))
+@Client.on_message(filters.command("view_welcome") & filters.user(ADMINS))
 async def view_welcome(client, message):
     welcome_message = db.welcome_messages.find_one({"chat_id": message.chat.id})
     if welcome_message:
@@ -23,7 +23,7 @@ async def view_welcome(client, message):
     else:
         await message.reply_text("No welcome message set for this chat.")
 
-@Client.on_message(filters.command("delete_welcome"))
+@Client.on_message(filters.command("delete_welcome") & filters.user(ADMINS))
 async def delete_welcome(client, message):
     db.welcome_messages.delete_one({"chat_id": message.chat.id})
     await message.reply_text("Welcome message deleted successfully!")
