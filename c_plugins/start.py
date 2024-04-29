a066d192c2189ffa03f1d967d1e42b68ca4a65c9
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-04-30 00:07:40 +0530

    Update start.py

--- c_plugins/start.py
+++ c_plugins/start.py
@@ -1,16 +1,37 @@
 from pyrogram import enums, filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message
+from info import DATABASE_URI, DATABASE_NAME
+from pymongo import MongoClient
+
+client = MongoClient(DATABASE_URI)
+db = client[DATABASE_NAME]
+collection = db["clone_bots"]
 
 @Client.on_message(filters.command("start"))
 async def start(client, message: Message):
-    mine = await client.get_me()
     button = [[ InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/sd_bots"),
                 ],[
                 InlineKeyboardButton("ʜᴇʟᴘ" , callback_data='help') ,
                 InlineKeyboardButton("ᴀʙᴏᴜᴛ" , callback_data='about')
             ]]
-    await message.reply_text(f"**__Hello {message.from_user.mention}__**", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+    await message.reply_text(f"**__ʜɪ {message.from_user.mention}__**\n**__ᴄʟɪᴄᴋ ʜᴇʟᴘ ᴛᴏ ɢᴇᴛ ᴛʜᴇ ʙᴜᴛᴛᴏɴs__**", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
+@Client.on_message(filters.command("help"))
+async def start(client, message: Message):
+    buttons = [[
+         InlineKeyboardButton('ɢᴏᴏɢʟᴇ ᴀɪ', callback_data='google'),
+         ],[
+         InlineKeyboardButton('sᴏɴɢ', callback_data='song'),
+         InlineKeyboardButton('ɪɴsᴛᴀ', callback_data='insta'),
+         InlineKeyboardButton('ᴠɪᴅᴇᴏ ᴛᴏ ᴀᴜᴅɪᴏ', callback_data='convert')
+         ],[
+         InlineKeyboardButton('ᴅᴏɴᴀᴛᴇ', callback_data='donate')
+         ],[
+         InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+         InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data='close')
+        ]]
+    await message.reply_text("ᴛʜᴇsᴇ ᴀʀᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴs", reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
+  
 @Client.on_callback_query()
 async def callback_handle(client, query):
     if query.data == 'start':
@@ -78,12 +99,17 @@ async def callback_handle(client, query):
         
    
     elif query.data == 'about':
+        bot_infos = collection.find({}) 
+        for bot_info in bot_infos:
+            user_finame = bot_info.get("user_fname", "N/A")
+            user_id = bot_info.get("user_id", "N/A")
+            username = bot_info.get("username", "N/A")
         buttons = buttons = [[
             InlineKeyboardButton('Home', callback_data='start'),
             InlineKeyboardButton('close', callback_data='close')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text("✯ Dᴇᴠᴇʟᴏᴩᴇʀ: <a href='https://t.me/MrTG_Coder'>ᴍʀ.ʙᴏᴛ ᴛɢ</a>\n✯ Lɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a>\n✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a>", reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
+        await query.message.edit_text(f"✯ ᴏᴡɴᴇʀ: <a href='tg://user?id={user_id}'><b>{user_finame}</b></a>\n✯ Lɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a>\n✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a>\n\n✯ ᴄʟᴏɴᴇᴅ ʙʏ: @mrtgcoderbot", reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
     
     elif query.data == 'close':
         await query.message.delete()
