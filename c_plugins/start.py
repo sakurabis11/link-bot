ab74d04dd7d9e8e2df9747230383bd04e8e84500
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-04-30 20:20:10 +0530

    Update start.py

--- c_plugins/start.py
+++ c_plugins/start.py
@@ -27,8 +27,8 @@ async def start(client, message: Message):
             InlineKeyboardButton("ʜᴇʟᴘ" , callback_data='help') ,
             InlineKeyboardButton("ᴀʙᴏᴜᴛ" , callback_data='about')
             ]]
-            reply_markup = InlineKeyboardMarkup(button)
-            await message.reply_text(f"ʜɪ {message.from_user.mention}\nᴄʟɪᴄᴋ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴs", reply_markup=reply_markup)
+        reply_markup = InlineKeyboardMarkup(button)
+        await message.reply_text(f"ʜɪ {message.from_user.mention}\nᴄʟɪᴄᴋ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴs", reply_markup=reply_markup)
   except Exception as e:
             await message.reply_text(e)
 
