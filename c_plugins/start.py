a9cd347d9082804990c2651e2ea2b30cb3f3b683
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-01 08:44:05 +0530

    Update start.py

--- c_plugins/start.py
+++ c_plugins/start.py
@@ -41,7 +41,7 @@ async def help(client, message: Message):
             InlineKeyboardButton('ᴄʟᴏsᴇ' , callback_data='close')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text("ᴛʜᴇsᴇ ᴀʀᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴs" , reply_markup=reply_markup ,
+        await message.reply_text("ᴛʜᴇsᴇ ᴀʀᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴs" , reply_markup=reply_markup ,
                                       parse_mode=enums.ParseMode.HTML) 
     except Exception as e:
         print(e)
@@ -57,7 +57,7 @@ async def about(client, message: Message):
                     InlineKeyboardButton('close' , callback_data='close')
                 ]]
                 reply_markup = InlineKeyboardMarkup(buttons)
-                await query.message.edit_text(
+                await message.reply_text(
                     f"✯ᴍʏ ɴᴀᴍᴇ: {me.first_name}\n\n✯ Lɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a>\n✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a>\n\n✯ ᴄʟᴏɴᴇᴅ ғʀᴏᴍ: @mrtgcoderbot" ,
                     reply_markup=reply_markup , parse_mode=enums.ParseMode.HTML , disable_web_page_preview=True)
 
@@ -140,18 +140,24 @@ async def callback_handle(client , query):
 
     elif query.data == 'about':
         try:
-                me = await client.get_me()
-                bot_username = me.username
-                bot_info = collection.find_one({"username": bot_username.strip("@")})
-                buttons = [[
-                    InlineKeyboardButton('Home' , callback_data='start') ,
-                    InlineKeyboardButton('close' , callback_data='close')
-                ]]
-                reply_markup = InlineKeyboardMarkup(buttons)
-                await query.message.edit_text(
-                    f"✯ᴍʏ ɴᴀᴍᴇ: {me.first_name}\n\n✯ Lɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a>\n✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a>\n\n✯ ᴄʟᴏɴᴇᴅ ғʀᴏᴍ: @mrtgcoderbot" ,
-                    reply_markup=reply_markup , parse_mode=enums.ParseMode.HTML , disable_web_page_preview=True)
-
+            me = await client.get_me()
+            bot_username = me.username
+            bot_info = collection.find_one({"username": bot_username.strip("@")})
+            me = client.get_me()
+            user_fname = bot_info.get("user_fname") ,
+            user_id = bot_info.get("user_id")
+            user_f_name = user_fname[0]
+            user_id_str = str(user_id)
+            user = user_id_str.replace("(" , "").replace("," , "").replace(")" , "")
+            buttons = [[
+                InlineKeyboardButton('Home' , callback_data='start') ,
+                InlineKeyboardButton('close' , callback_data='close')
+            ]]
+            reply_markup = InlineKeyboardMarkup(buttons)
+            await query.message.edit_text(
+                f"✯ ᴏᴡɴᴇʀ: <a href='tg://user?id={user}'><b>{user_f_name}</b></a>\n✯ Lɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a>\n✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a>\n\n✯ ᴄʟᴏɴᴇᴅ ғʀᴏᴍ: @mrtgcoderbot" ,
+                reply_markup=reply_markup , parse_mode=enums.ParseMode.HTML , disable_web_page_preview=True)
+            
         except Exception as e:
             print(f"Error processing about query: {e}")
 
