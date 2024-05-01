182732501f5e85f2ba4466b83ec9e25f877f93a6
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-01 13:33:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ from PIL import Image
 from io import BytesIO
 import requests
 from pymongo import MongoClient
-from info import DATABASE_URI, DATABASE_NAME, API_ID, API_HASH
+from info import DATABASE_URI, DATABASE_NAME, API_ID, API_HASH, LOG_CHANNEL_INFORM, LOG_CHANNEL_ERROR
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
