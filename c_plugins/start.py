f682894cf6a473bd25b59a1389c91190924f8a7f
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-01 08:34:22 +0530

    Update start.py

--- c_plugins/start.py
+++ c_plugins/start.py
@@ -1,6 +1,6 @@
 from pyrogram import enums , filters , Client
 from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup , CallbackQuery , Message
-from info import DATABASE_URI, 
+from info import DATABASE_URI, DATABASE_NAME
 from pymongo import MongoClient, DATABASE_NAME
 
 
