5eeb5b9c853426e64046f8a23c8550791ec16aef
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-01 19:14:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -89,7 +89,6 @@ async def restart_all_bots():
                 print(f"Error: Bot token missing in document: {e}")
             except Exception as e:
                 print(f"Error restarting bot {bot_token}: {e}")
-                await client.send_message(LOG_CHANNEL_ERROR, text=f"{e}")
         else:
             pass
 
