d699d901eb4b2e1567d0727707430e8e2b40ec06
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-10 06:54:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from plugins import web_server
 import pytz
 import aiohttp
 from aiohttp import web
-from utils import temp, restart_all_bots
+from utils import temp
 
 from pyrogram.raw.all import layer
 from pyrogram import types
@@ -48,7 +48,6 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", 8080).start()
         logger.info("Running...")
         print(f"{me.first_name} | @{me.username} started...")
-        await restart_all_bots()
         await self.send_message(chat_id=LOG_CHANNEL_INFORM, text=f"ᴀʟʟ ᴄʟᴏɴᴇᴅ ʙᴏᴛs ᴀʀᴇ ʀᴇsᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ")
         tz = pytz.timezone('Asia/Kolkata')
         today = date.today()
