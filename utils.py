1b82a144486e25989638432ae82b444f9eb6c9ff
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-15 00:08:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -124,6 +124,28 @@ async def broadcast_messages(user_id, message):
     except Exception as e:
         return False, "Error"
 
+async def broadcast_messager(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages(user_id, message)
+    except InputUserDeactivated:
+        await sd.delete_user(int(user_id))
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
+    except UserIsBlocked:
+        logging.info(f"{user_id} -Blocked the bot.")
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await sd.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
+    except Exception as e:
+        return False, "Error"
+
+
 
 def extract_user(message: Message) -> Union[int, str]:
     """extracts the user from a message"""
