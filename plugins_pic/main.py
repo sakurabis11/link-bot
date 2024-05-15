c9453bb632c694ae60e7a2f09e7ecadb30355446
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-15 23:21:42 +0530

    Update main.py

--- plugins_pic/main.py
+++ plugins_pic/main.py
@@ -7,7 +7,8 @@ import os
 from utils import get_size
 from Script import script
 from pyrogram.errors import PeerIdInvalid
-from info import DATABASE_URI_2, DATABASE_NAME_2, LOG_CHANNEL, PIC_LOG_CHANNEL
+from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong
+from info import DATABASE_URI_2, DATABASE_NAME_2, LOG_CHANNEL, PIC_LOG_CHANNEL, ADMINS
 from database_pic.pic_users_db import sd
 from os import environ
 
@@ -37,6 +38,23 @@ async def get_stats(bot, message):
     await rju.edit(script.STATS_TXT.format(total_users, size, free))
  except Exception as e:
     print(e)
+
+@Client.on_message(filters.command('users') & filters.user(ADMINS))
+async def list_users(bot, message):
+    raju = await message.reply('Getting List Of Users')
+    users = await sd.get_all_users()
+    out = "Users Saved In DB Are:\n\n"
+    async for user in users:
+        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
+        if user['ban_status']['is_banned']:
+            out += '( Banned User )'
+        out += '\n'
+    try:
+        await raju.edit_text(out)
+    except MessageTooLong:
+        with open('users.txt', 'w+') as outfile:
+            outfile.write(out)
+        await message.reply_document('users.txt', caption="List Of Users")
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
