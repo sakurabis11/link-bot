927b1f0d9aa7f11a1b674d7cdad4a4a84ad97299
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-15 08:01:21 +0530

    Update main.py

--- plugins_pic/main.py
+++ plugins_pic/main.py
@@ -7,7 +7,7 @@ import os
 from utils import get_size
 from Script import script
 from pyrogram.errors import PeerIdInvalid
-from info import DATABASE_URI_2, DATABASE_NAME_2, LOG_CHANNEL
+from info import DATABASE_URI_2, DATABASE_NAME_2, LOG_CHANNEL, PIC_LOG_CHANNEL
 from database_pic.pic_users_db import sd
 from os import environ
 
@@ -67,9 +67,9 @@ async def photo(client, message):
     x = collection.insert_one({"user_id": user_id, "file_id": file_ids})
     await message.reply_text(f"Photo saved successfully\n\n {x}")
     if message.from_user.username!= None:
-        await client.send_cached_media(chat_id=LOG_CHANNEL, file_id=file_ids, caption=f"Photo from @{message.from_user.username}")
+        await client.send_cached_media(chat_id=PIC_LOG_CHANNEL, file_id=file_ids, caption=f"Photo from @{message.from_user.username}")
     else:
-        await client.send_cached_media(chat_id=LOG_CHANNEL , file_id=file_ids , caption=f"Photo from {message.from_user.mention} {message.from_user.first_name}")
+        await client.send_cached_media(chat_id=PIC_LOG_CHANNEL , file_id=file_ids , caption=f"Photo from {message.from_user.mention} {message.from_user.first_name}")
   except Exception as e:
     await message.reply_text(e)
 
