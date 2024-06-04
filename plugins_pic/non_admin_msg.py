7f38f8b00ddd9d70fb944ffbca71769e5d49ff18
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-05 00:15:50 +0530

    Delete plugins_pic/non_admin_msg.py

--- plugins_pic/non_admin_msg.py
+++
@@ -1,8 +0,0 @@
-import pyrogram 
-from pyrogram import Client, filters
-from info import ADMINS
-
-@Client.on_message(filters.text | filters.photo & filters.private & ~filters.user(ADMINS))
-async def start_non(client, message):
-  await message.reply_text("Sorry the bot is under maintenance work")
-  
