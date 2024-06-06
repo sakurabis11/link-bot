218600df597a2253502b3fa44a75ab4e8b65df9b
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-06 16:08:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -82,7 +82,22 @@ async def start_bot():
     print(f"{mine.first_name} | @{mine.username}")
   except Exception as e:
     print(e)
-
+      
+async def start_new_bot():
+  try:
+    pic_client = Client(
+        name = "pic",
+        api_id = API_ID,
+        api_hash = API_HASH,
+        bot_token = BOT_TOKEN_3,
+        plugins={"root": "core"}
+    )
+    await pic_client.start()
+    mine = await pic_client.get_me()
+    print(f"{mine.first_name} | @{mine.username}")
+  except Exception as e:
+    print(e)
+      
 def get_size(size):
     """Get size in readable format"""
 
