b2d60267d8fb2ddf5e306515b3e89d5e1f93feb2
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-06 15:52:00 +0530

    Update pm_mani.py

--- core/pm_mani.py
+++ core/pm_mani.py
@@ -1,5 +1,34 @@
 import pyrogram 
 from pyrogram import Client, filters
+import asyncio
 from pyrogram.types import *
 
 @Client.on_message(filters.command("start"))
+async def start_session(client, message):
+    
+    await message.reply_text("Welcome to our bot...\n\nThis bot is intended to chat with people on Telegram")
+
+@Client.on_message(filters.command("search"))
+async def sart_session(client, message):
+    
+    v=await message.reply_text("searching partner...")
+    await asyncio.sleep(5)
+    z=await v.edit("Partner found")
+
+@Client.on_message(filters.command("next"))
+async def strt_session(client, message):
+    
+    a=await message.reply_text("searching partner...")
+    await asyncio.sleep(5)
+    b=await v.edit("Partner found")
+
+@Client.on_message(filters.command("stop"))
+async def stat_session(client, message):
+    await message.reply_text("Stopped")
+
+@Client.on_message(filters.text & ~filters.command("start","search","next","stop")
+async def v_session(client, message):
+   txt = message.text
+   await client.send_message(
+   
+  
