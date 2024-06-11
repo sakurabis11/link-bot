e0c910bfcf17fb6d9de3a1f3ea8aa628567d935f
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-11 08:01:29 +0530

    Update help_fn.py

--- plugins/help_fn.py
+++ plugins/help_fn.py
@@ -3,5 +3,8 @@ from pyrogram.types import *
 
 @Client.on_message(filters.command("def"))
 async def help_fn(client, message:Message):
+ try:
    txt = message.text.split()[1::]
    await message.reply_text(help(txt))
+ except Exception as e:
+   await message.reply_text(e)
