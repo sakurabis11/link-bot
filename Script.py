191dcd74541310c127097f83a2a560af2856208f
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-17 21:00:35 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,19 +1,12 @@
 class script(object):  
-    LOG_TEXT_PI = """#ɴᴇᴡ_ᴜꜱᴇʀ_pic_ᴜᴘᴅᴀᴛᴇᴅ
+    LOG_TEXT = """#ɴᴇᴡ_ᴜꜱᴇʀ
     
 ◉ ᴜꜱᴇʀ-ɪᴅ: <code>{}</code>
 ◉ ᴀᴄᴄ-ɴᴀᴍᴇ: {}
 ◉ ᴜꜱᴇʀ ʟɪɴᴋ: <a href='tg://user?id={}'><b>ᴜsᴇʀ</b></a>"""
     
-    LOG_TEXT_P = """#ɴᴇᴡ_ᴜꜱᴇʀ
-
-◉ ᴜꜱᴇʀ-ɪᴅ: <code>{}</code>
-◉ ᴀᴄᴄ-ɴᴀᴍᴇ: {}
-◉ ᴜꜱᴇʀ ʟɪɴᴋ: <a href='tg://user?id={}'><b>ᴜsᴇʀ</b></a>"""
 
     STATS_TXT =  """<b>◉ ᴛᴏᴛᴀʟ ᴜꜱᴇʀꜱ: <code>{}</code></b>
-◉ ᴛᴏᴛᴀʟ ᴘʜᴏᴛᴏs: <code>{}</code>
-◉ ᴜʀ ᴘʜᴏᴛᴏ ᴄᴏᴜɴᴛ :<code>{}</code>
 ◉ sɪᴢᴇ: <code>{}</code>
 ◉ ғʀᴇᴇ: <code>{}</code>"""
 
