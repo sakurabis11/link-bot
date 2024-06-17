9b6b1ce35ffd4f11f829fd16248166c4811605ed
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-17 21:07:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,6 +17,11 @@ API_HASH = os.environ.get('API_HASH', '')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 PORT = os.environ.get("PORT", "8080")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', "").split()]
+
+
 LOG_CHANNEL = int(os.environ.get("PIC_LOG_CHANNEL", ""))
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "")
 DATABASE_URI = os.environ.get("DATABASE_URI", "")
+
+# ur channel link
+F_SUB = os.environ.get("F_SUB", "")
